5a0e1080847359d6a5990ad1cf24f3e916544886
Fabian Müntefering <fabian.muentefering@mail.de>
2019-03-10 03:43:43 -0500

    Configuration crash protection

--- gabac_config/generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for filename in ./*.0; do
-    echo "********s${filename}*********"
-    /home/fabian/Documents/dev/gabac/build/bin/gabacify analyze -i ${filename} -c ${filename}.json
-done
