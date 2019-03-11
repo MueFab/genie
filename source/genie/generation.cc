6069f81be3e723d6254400455acd11b2a9ccece4
Fabian Müntefering <fabian.muentefering@mail.de>
2019-03-11 13:30:43 -0500

    Debug copy

--- source/genie/generation.cc
+++ source/genie/generation.cc
@@ -44,6 +44,84 @@ void decompress(const std::string& temp_dir,
 
 namespace dsg {
 
+bool copyDir(
+        boost::filesystem::path const & source,
+        boost::filesystem::path const & destination
+)
+{
+    namespace fs = boost::filesystem;
+    try
+    {
+        // Check whether the function call is valid
+        if(
+                !fs::exists(source) ||
+                !fs::is_directory(source)
+                )
+        {
+            std::cerr << "Source directory " << source.string()
+                      << " does not exist or is not a directory." << '\n'
+                    ;
+            return false;
+        }
+        if(fs::exists(destination))
+        {
+            std::cerr << "Destination directory " << destination.string()
+                      << " already exists." << '\n'
+                    ;
+            return false;
+        }
+        // Create the destination directory
+        if(!fs::create_directory(destination))
+        {
+            std::cerr << "Unable to create destination directory"
+                      << destination.string() << '\n'
+                    ;
+            return false;
+        }
+    }
+    catch(fs::filesystem_error const & e)
+    {
+        std::cerr << e.what() << '\n';
+        return false;
+    }
+    // Iterate through the source directory
+    for(
+            fs::directory_iterator file(source);
+            file != fs::directory_iterator(); ++file
+            )
+    {
+        try
+        {
+            fs::path current(file->path());
+            if(fs::is_directory(current))
+            {
+                // Found directory: Recursion
+                if(
+                        !copyDir(
+                                current,
+                                destination / current.filename()
+                        )
+                        )
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                // Found file: Copy
+                fs::copy_file(
+                        current,
+                        destination / current.filename()
+                );
+            }
+        }
+        catch(fs::filesystem_error const & e)
+        {
+            std:: cerr << e.what() << '\n';
+        }
+    }
+    return true;
+}
 
 static void generationFromFasta(
         const ProgramOptions& programOptions
@@ -252,8 +330,12 @@ void decompression(
             unpackFiles(temp_dir, in);
     fclose(in);
 
+    copyDir(temp_dir,temp_dir + "/../genie_comp");
+
     // Decompress
     run_gabac(flist, programOptions.configPath, true, programOptions.numThreads);
+
+    copyDir(temp_dir,temp_dir + "/../genie_uncomp");
 
     // Extract spring parameters
     spring::compression_params cp;
@@ -322,8 +404,11 @@ void generation(
                 }
             }
 
+            copyDir(path,path + "/../genie_orig");
+
             // Compress
             run_gabac(filelist, programOptions.configPath, false, programOptions.numThreads);
+
 
             // Pack
             std::string outfile = (programOptions.inputFilePath.substr(0, programOptions.inputFilePath.find_last_of('.')) + ".genie");
