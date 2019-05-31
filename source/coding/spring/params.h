7840f7ef508de4ac99462d17ccd39efc824d9391
Brian Bliss <bbliss@illinois.edu>
2019-05-31 12:15:34 -0500

    Reduce lock array sizes to 0x10000 (performance opt).

--- source/coding/spring/params.h
+++ source/coding/spring/params.h
@@ -11,8 +11,8 @@ const uint32_t MAX_NUM_READS = 4294967290;
 const int NUM_DICT_REORDER = 2;
 const int MAX_SEARCH_REORDER = 1000;
 const int THRESH_REORDER = 4;
-const int NUM_LOCKS_REORDER =
-    0x1000000;  // limits on number of locks (power of 2 for fast mod)
+const int NUM_LOCKS_REORDER = 0x10000;  // # of locks (power of 2 for fast mod)
+const int LOCKS_REORDER_MASK = 0xffff;
 const float STOP_CRITERIA_REORDER = 0.5;
 // fraction of unmatched reads in last 1M for thread to give up on searching
 const int NUM_DICT_ENCODER = 2;
