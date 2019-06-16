0517b9edfd68e6e0eb6ffe3c96191dde6cf16203
Fabian Müntefering <fabian.muentefering@mail.de>
2019-06-16 14:03:11 +0200

    Remove boost program_options

--- source/genie/ProgramOptions.cc
+++ source/genie/ProgramOptions.cc
@@ -11,13 +11,11 @@
 
 #include "genie/ProgramOptions.h"
 
-#include <boost/program_options.hpp>
-#include <boost/tokenizer.hpp>
-#include <boost/token_functions.hpp>
-
 #include <iostream>
 #include <set>
 #include <map>
+
+#include <cli11/CLI11.hpp>
 
 #include "genie/exceptions.h"
 #include "genie/utilities.h"
@@ -25,43 +23,6 @@
 
 
 namespace dsg {
-
-
-static void parseConfigurationFile(
-    const std::string& path,
-    std::vector<std::string> * const args)
-{
-    namespace po = boost::program_options;
-
-    std::cout << "Parsing configurration file" << std::endl;
-
-    dsg::input::FileReader fileReader(path);
-    std::string options("");
-
-    // Read configuration file contents.
-    while (true) {
-        // Try to read a line.
-        std::string line("");
-        fileReader.readLine(&line);
-        if (line.empty() == true) {
-            break;
-        }
-
-        // Append this line plus an extra space (for tokenization) to our
-        // programOptionsString.
-        options += " " + line;
-    }
-
-    // At this point the programOptionsString may not be empty.
-    if (options.empty() == true) {
-        throwRuntimeError("failed to parse configuration file");
-    }
-
-    // Tokenize the configuration file contents.
-    boost::char_separator<char> separators(" ");
-    boost::tokenizer<boost::char_separator<char>> tokenizer(options, separators);
-    std::copy(tokenizer.begin(), tokenizer.end(), std::back_inserter(*args));
-}
 
 
 ProgramOptions::ProgramOptions(
@@ -84,27 +45,9 @@ ProgramOptions::ProgramOptions(
 }
 
 
-ProgramOptions::~ProgramOptions(void)
+ProgramOptions::~ProgramOptions()
 {
     // Nothing to do here.
-}
-
-
-void ProgramOptions::print(void)
-{
-    // LOG_S(INFORMATIONAL) << "Program options:";
-    // LOG_S(INFORMATIONAL) << "  generic:";
-    // LOG_S(INFORMATIONAL) << "    force                : " << ((force == true) ? "true" : "false");
-    // LOG_S(INFORMATIONAL) << "    verbose              : " << (verbose ? "true" : "false");
-    // LOG_S(INFORMATIONAL) << "    number of threads    : " << numThreads;
-    // LOG_S(INFORMATIONAL) << "  input:";
-    // LOG_S(INFORMATIONAL) << "    input file path      : " << inputFilePath;
-    // LOG_S(INFORMATIONAL) << "    input file pair path : " << inputFilePairPath;
-    // LOG_S(INFORMATIONAL) << "    input file type      : " << inputFileType;
-    // LOG_S(INFORMATIONAL) << "  algorithm:";
-    // LOG_S(INFORMATIONAL) << "    read algorithm       : " << readAlgorithm;
-    // LOG_S(INFORMATIONAL) << "    ID algorithm         : " << idAlgorithm;
-    // LOG_S(INFORMATIONAL) << "    QV algorithm         : " << qvAlgorithm;
 }
 
 
@@ -112,104 +55,50 @@ void ProgramOptions::processCommandLine(
     int argc,
     char *argv[])
 {
-    namespace po = boost::program_options;
+    CLI::App app("Genie MPEG-G reference encoder\n\n"
+    "Usage fastq encoding: \n genie [fastq] [optional: paired fastq] -c [Path to gabac config] [Optional: -t number of threads]\n"
+    "Usage fastq decoding: \n genie [genie file] -c [Path to gabac config] [Optional: -t number of threads]\n"
+    "Usage sam encoding: \n genie [sam] [fasta reference] -c [Path to gabac config] [Optional: -t number of threads]\n"
+    "Usage sam decoding: \n genie [sgenie file] -c [Path to gabac config] [Optional: -t number of threads]\n");
 
-    // Declare the supported option groups.
-    po::options_description allOptions("All");
-    po::positional_options_description p;
+    std::string filename = "default";
+    app.add_option("input-file", inputFilePath, "First input file - fastq in fastq mode, sam in sam mode, genie/sgenie file in decompression mode. This argument is positional (first argument) and mandatory")->mandatory(true);
+    app.add_option("input-file-pair-path", inputFilePairPath, "Second input file - paired fastq in fastq mode (optional), reference fasta file in sam mode (mandatory). This argument is also positional.");
+    app.add_option("-c,--config-file-path", configPath, "Path to directory with gabac configurations. Missing configuratons will be regenerated, which may take a while")->mandatory(true);
 
-    p.add("input-file", 1);
-    p.add("input-file-pair-path", 1);
+    numThreads = 1;
+    app.add_option("-t,--numThreads", numThreads, "How many threads to launch for parallel execution of GABAC. Default is 1.");
 
-    std::set<std::string> allowedOptionGroupNames = {
-        "basic",
-        "generic",
-        "input",
-        "algorithm"
-    };
-
-    // Add the input options.
-    allOptions.add_options()
-        ("input-file",
-            po::value<std::string>(&inputFilePath),
-            "First input file - fastq in fastq mode, sam in sam mode, genie/sgenie file in decompression mode. This argument is positional (first argument) and mandatory")
-        ("input-file-pair-path,p",
-            po::value<std::string>(&inputFilePairPath),
-            "Second input file - paired fastq in fastq mode (optional), reference fasta file in sam mode (mandatory). This argument is also positional.")
-            ("config-file-path,c",
-                    po::value<std::string>(&configPath),
-                            "Path to directory with gabac configurations. Missing configuratons will be regenerated, which may take a while")
-            ("numThreads,t",
-             po::value<int>(&numThreads)->default_value(1),
-             "How many threads to launch for parallel execution of GABAC. Default is 1.")
-            ("help", "Print usage");
-
-    // Declare an options_description instance which will include all the
-    // options except for the basic options.
-
-
-    // Declare to maps for the options.
-    po::variables_map allOptionsMap;
-
-    po::store(po::command_line_parser(argc, argv).
-            options(allOptions).positional(p).run(), allOptionsMap);
-
-    // First thing to do is to print the help.
-    if (allOptionsMap.count("help") || allOptionsMap.count("h")) {
-        std::cout
-                << "Usage fastq encoding: \n genie [fastq] [optional: paired fastq] -c [Path to gabac config] [Optional: -t number of threads]"
-                << std::endl;
-        std::cout
-                << "Usage fastq decoding: \n genie [genie file] -c [Path to gabac config] [Optional: -t number of threads]"
-                << std::endl;
-        std::cout
-                << "Usage sam encoding: \n genie [sam] [fasta reference] -c [Path to gabac config] [Optional: -t number of threads]"
-                << std::endl;
-        std::cout
-                << "Usage sam decoding: \n genie [sgenie file] -c [Path to gabac config] [Optional: -t number of threads]"
-                << std::endl << std::endl;
-        std::cout << allOptions << std::endl;
-        help = true;
-        return;
+    try {
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError &e) {
+        throw std::runtime_error("Command line parsing failed:" + std::to_string(app.exit(e)));
     }
 
-    // This will throw on erroneous program options. Thus we call notify()
-    // after printing the help.
-    po::notify(allOptionsMap);
-
-    print();
     validate();
-    validateDependencies();
 }
 
 
-void ProgramOptions::validate(void)
+void ProgramOptions::validate()
 {
     std::set<std::string>::iterator it;
 
-    //
-    // force
-    //
-
-    if (force == true) {
-        // Nothing to do here.
-    }
-
-    //
-    // inputFilePath
-    //
-
-    if (inputFilePath.empty() == true) {
+    if (inputFilePath.empty()) {
         throwRuntimeError("no input file path provided");
     }
 
-    if (common::fileExists(inputFilePath) == false) {
+    if (!common::fileExists(inputFilePath)) {
         throwRuntimeError("input file does not exist");
     }
 
+    if (!common::dirExists(configPath)) {
+        throwRuntimeError("config dir does not exist");
+    }
 
-    if (inputFilePairPath.empty() == false) {
-        if (common::fileExists(inputFilePairPath) == false) {
+
+
+    if (!inputFilePairPath.empty()) {
+        if (!common::fileExists(inputFilePairPath)) {
             throwRuntimeError("input pair file does not exist");
         }
         if (inputFilePairPath == inputFilePath) {
@@ -237,120 +126,8 @@ void ProgramOptions::validate(void)
 
     std::transform(inputFileType.begin(), inputFileType.end(),inputFileType.begin(), ::toupper);
 
-    it = allowedInputFileTypes.find(inputFileType);
-
-    if (it == allowedInputFileTypes.end()) {
-        // LOG_S(ERROR) << "Input file type '" << inputFileType << "' is invalid";
-        // LOG_S(ERROR) << "Allowed input file types are:";
-        for (const auto& allowedInputFileType : allowedInputFileTypes) {
-            // LOG_S(ERROR) << "  " << allowedInputFileType;
-        }
+    if (allowedInputFileTypes.find(inputFileType) == allowedInputFileTypes.end()) {
         throwRuntimeError("input file type is invalid");
-    }
-
-
-    //
-    // verbose
-    //
-
-    // Nothing to do here.
-
-
-    //
-    // idAlgorithm
-    //
-
-    std::set<std::string> allowedIdAlgorithms = {
-        "TOK",
-    };
-
-    // Check if the user input string for idAlgorithm is in the
-    // set of allowed ID algorithms.
-    idAlgorithm = "TOK";
-    it = allowedIdAlgorithms.find(idAlgorithm);
-
-    if (it == allowedIdAlgorithms.end()) {
-        // LOG_S(ERROR) << "ID algorithm '" << idAlgorithm << "' is invalid";
-        // LOG_S(ERROR) << "Allowed ID algorithms are:";
-        for (const auto& allowedIdAlgorithm : allowedIdAlgorithms) {
-            // LOG_S(ERROR) << "  " << allowedIdAlgorithm;
-        }
-        throwRuntimeError("ID algorithm is invalid");
-    }
-
-
-    //
-    // qvAlgorithm
-    //
-
-    std::set<std::string> allowedQvAlgorithms = {
-        "CALQ",
-        "QVZ"
-    };
-
-    qvAlgorithm = "QVZ";
-
-    // Check if the user input string for qvAlgorithm is in the
-    // set of allowed QV algorithms.
-    it = allowedQvAlgorithms.find(qvAlgorithm);
-
-    if (it == allowedQvAlgorithms.end()) {
-        // LOG_S(ERROR) << "QV algorithm '" << qvAlgorithm << "' is invalid";
-        // LOG_S(ERROR) << "Allowed QV algorithms are:";
-        for (const auto& allowedQvAlgorithm : allowedQvAlgorithms) {
-            // LOG_S(ERROR) << "  " << allowedQvAlgorithm;
-        }
-        throwRuntimeError("QV algorithm is invalid");
-    }
-
-
-    //
-    // readCompressionAlgorithm
-    //
-
-    std::set<std::string> allowedReadAlgorithms = {
-        "HARC",
-        "TSC",
-    };
-
-    readAlgorithm = "HARC";
-    // Check if the user input string for readAlgorithm is in the
-    // set of allowed read algorithms.
-    it = allowedReadAlgorithms.find(readAlgorithm);
-
-    if (it == allowedReadAlgorithms.end()) {
-        // LOG_S(ERROR) << "Read algorithm '" << readAlgorithm << "' is invalid";
-        // LOG_S(ERROR) << "Allowed read algorithms are:";
-        for (const auto& allowedReadAlgorithm : allowedReadAlgorithms) {
-            // LOG_S(ERROR) << "  " << allowedReadAlgorithm;
-        }
-        throwRuntimeError("read algorithm is invalid");
-    }
-}
-
-
-void ProgramOptions::validateDependencies(void)
-{
-        if ((inputFileType != "SGENIE") && (inputFileType != "GENIE") && (inputFileType != "FASTQ") && (inputFileType != "SAM")) {
-            // LOG_S(ERROR) << "Read algorithm 'HARC' requires a FASTA file "
-                         // "or a FASTQ file as input";
-            throwRuntimeError("invalid dependency");
-        }
-
-    if (idAlgorithm == "TOK") {
-        // Nothing to do here.
-    }
-
-    if (qvAlgorithm == "CALQ") {
-        if ((inputFileType != "SAM")) {
-            // LOG_S(ERROR) << "QV algorithm 'CALQ' requires a SAM file "
-                         // "as input";
-            throwRuntimeError("invalid dependency");
-        }
-    }
-
-    if (qvAlgorithm == "QVZ") {
-        // Nothing to do here.
     }
 }
 
