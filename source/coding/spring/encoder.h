8d5a87e101231ce42f556c34a8b8d86b58843a31
Shubham Chandak <schandak@stanford.edu>
2019-07-01 04:16:25 -0700

    turn on parallelization, remove warnings

--- source/coding/spring/encoder.h
+++ source/coding/spring/encoder.h
@@ -625,8 +625,12 @@ void encoder_main(const std::string &temp_dir, const compression_params &cp) {
   }
 
   constructdictionary<bitset_size>(read, dict, read_lengths_s, eg.numdict_s,
-                                   eg.numreads_s + eg.numreads_N, 3, eg.basedir,
-                                   eg.num_thr);
+                                   eg.numreads_s + eg.numreads_N, 3, eg.basedir
+#ifdef GENIE_USE_OPENMP
+                                   , eg.num_thr);
+#else
+                                   );
+#endif
   encode<bitset_size>(read, dict, order_s, read_lengths_s, eg, egb);
   remove(eg.infile_N.c_str());
   delete[] read;
