e2f2fe3d6bf5d7cda2d944b78e9c6f2dd5570077
Fabian Müntefering <fabian.muentefering@mail.de>
2019-07-03 01:53:10 +0200

    Better approach

--- source/genie/gabac_integration.h
+++ source/genie/gabac_integration.h
@@ -16,41 +16,5 @@ void compress_one_file(const std::string& file, const std::string& config, bool 
 // Decompress a list of files in parallel
 void run_gabac(const std::vector<std::string>& files, const std::string& config, bool decompress, size_t threads);
 
-struct gabac_stream_params {
-    uint64_t maxval;
-    uint8_t wordsize;
-};
-
-inline const std::map<std::string, gabac_stream_params>& getSPRINGParams() {
-    static const std::map<std::string, gabac_stream_params> mapping = {
-            {"S0.0", {1022, 8}},
-            {"S1.0", {1, 8}},
-            {"S3.0", {1, 8}},
-            {"S3.1", {511, 8}},
-            {"S4.0", {1, 8}},
-            {"S4.1", {4, 8}},
-            {"S6.0", {4, 8}},
-            {"S7.0", {std::numeric_limits<uint32_t >::max(), 8}},
-            {"S8.0", {8, 8}},
-            {"S8.1", {65535, 8}},
-            {"S8.2", {512000, 8}},
-            {"S8.3", {512000, 8}},
-            {"S8.4", {std::numeric_limits<uint32_t >::max(), 8}},
-            {"S8.5", {std::numeric_limits<uint32_t >::max(), 8}},
-            {"S8.6", {512000, 8}},
-            {"S8.7", {512000, 8}},
-            {"S12.0", {5, 8}},
-            {"Q", {127, 1}},
-            {"T0", {9, 8}},
-            {"T1", {1, 8}},
-            {"T2", {127, 8}},
-            {"T3", {127, 8}},
-            {"T4", {std::numeric_limits<uint32_t >::max(), 8}},
-            {"T5", {255, 8}}
-
-    };
-    return mapping;
-}
-
 }
 #endif //GENIE_GABAC_INTEGRATION_H
