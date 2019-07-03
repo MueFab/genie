8e261e5032644e073749812aaa4e47eb165450c7
Jan Voges <janvoges@me.com>
2019-07-03 16:58:12 +0200

    2nd part of big code and CMake build system cleanup

--- ci/cmake-build.sh
+++ ci/cmake-build.sh
@@ -27,7 +27,7 @@ run git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 
 readonly cmake_build_dir="${git_root_dir}/cmake-build"
-if [ ! -d "${cmake_build_dir}" ]; then
+if [[ ! -d "${cmake_build_dir}" ]]; then
     info "Creating directory for CMake build: ${cmake_build_dir}"
     run mkdir -p "${cmake_build_dir}"
 fi
