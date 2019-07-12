1892f811a119c2e8d6cd87c7bf7bbc3e6cec9f9f
Jan Voges <janvoges@me.com>
2019-07-12 21:18:16 +0200

    More concise CI scripts, now using set -euxo pipefail

--- ci/test-genie-roundtrip.sh
+++ ci/test-genie-roundtrip.sh
@@ -1,48 +1,24 @@
 #!/usr/bin/env bash
 
-self="${0}"
-self_name="${self##*/}"
+set -euxo pipefail
 
-datetime=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-
-trace() { echo "[${self_name}] [${datetime}] [trace] ${*}"; }
-debug() { echo "[${self_name}] [${datetime}] [debug] ${*}"; }
-info() { echo "[${self_name}] [${datetime}] [info] ${*}"; }
-warning() { echo "[${self_name}] [${datetime}] [warning] ${*}"; }
-error() { echo "[${self_name}] [${datetime}] [error] ${*}" 1>&2; }
-fatal() { echo "[${self_name}] [${datetime}] [fatal] ${*}" 1>&2; }
-
-run() {
-    local rc
-    info "Running: ${*}"
-    "${@}"
-    rc="${?}"
-    if [[ ${rc} != 0 ]]; then
-        fatal "Command failed (RC=${rc}): ${*}"
-        exit ${rc}
-    fi
-}
-
-run git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
+git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 
 readonly genie="${git_root_dir}/cmake-build/bin/genie"
-if [[ ! -x "${genie}" ]]; then
-    fatal "Genie application '${genie}' is not executable"
-    exit 1
-fi
+if [[ ! -x "${genie}" ]]; then exit 1; fi
 
-run "${genie}" \
+"${genie}" \
     resources/test-files/fastq/fourteen-gattaca-records.fastq \
     --config-file-path resources/gabac-configs/
 
-run "${genie}" \
+"${genie}" \
     resources/test-files/fastq/fourteen-gattaca-records.genie \
     --config-file-path resources/gabac-configs/
 
-run "${git_root_dir}/scripts/cmp-reordered-fastq.sh" \
+"${git_root_dir}/scripts/cmp-reordered-fastq.sh" \
     -1 resources/test-files/fastq/fourteen-gattaca-records.fastq \
     -2 resources/test-files/fastq/fourteen-gattaca-records.genie-decompressed.fastq
 
-run rm resources/test-files/fastq/fourteen-gattaca-records.genie
-run rm resources/test-files/fastq/fourteen-gattaca-records.genie-decompressed.fastq
+rm resources/test-files/fastq/fourteen-gattaca-records.genie
+rm resources/test-files/fastq/fourteen-gattaca-records.genie-decompressed.fastq
