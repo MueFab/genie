48ecbbde66716122c8162482ce52943b77ecc31e
Jan Voges <janvoges@me.com>
2019-07-25 10:35:06 +0200

    Fix ci/test-genie-roundtrip.sh

--- ci/test-genie-roundtrip.sh
+++ ci/test-genie-roundtrip.sh
@@ -9,16 +9,19 @@ readonly genie="${git_root_dir}/cmake-build/bin/genie"
 if [[ ! -x "${genie}" ]]; then exit 1; fi
 
 "${genie}" \
-    resources/test-files/fastq/fourteen-gattaca-records.fastq \
-    --config-file-path resources/gabac-configs/
+    "${git_root_dir}/resources/test-files/fastq/fourteen-gattaca-records.fastq" \
+    --config-file-path "${git_root_dir}/resources/gabac-configs/" \
+    --output-file-path "${git_root_dir}/resources/test-files/fastq/fourteen-gattaca-records.genie"
 
 "${genie}" \
-    resources/test-files/fastq/fourteen-gattaca-records.genie \
-    --config-file-path resources/gabac-configs/
+    "${git_root_dir}/resources/test-files/fastq/fourteen-gattaca-records.genie" \
+    --config-file-path "${git_root_dir}/resources/gabac-configs/" \
+    --output-file-path "${git_root_dir}/resources/test-files/fastq/fourteen-gattaca-records.genie-decompressed.fastq"
 
 "${git_root_dir}/scripts/cmp-reordered-fastq.sh" \
-    -1 resources/test-files/fastq/fourteen-gattaca-records.fastq \
-    -2 resources/test-files/fastq/fourteen-gattaca-records.genie-decompressed.fastq
+    -1 "${git_root_dir}/resources/test-files/fastq/fourteen-gattaca-records.fastq" \
+    -2 "${git_root_dir}/resources/test-files/fastq/fourteen-gattaca-records.genie-decompressed.fastq"
 
-rm resources/test-files/fastq/fourteen-gattaca-records.genie
-rm resources/test-files/fastq/fourteen-gattaca-records.genie-decompressed.fastq
+rm "${git_root_dir}/resources/test-files/fastq/fourteen-gattaca-records.genie"
+rm "${git_root_dir}/resources/test-files/fastq/fourteen-gattaca-records.genie-decompressed.fastq"
+rm "${git_root_dir}/genie.log"
