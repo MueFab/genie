48c7101a7570c2cb78e62c7c4e42788d78855a15
Shubham Chandak <schandak@stanford.edu>
2019-08-04 12:29:08 -0700

    add combine_pairs mode

--- source/libs/spring/decompress.h
+++ source/libs/spring/decompress.h
@@ -16,10 +16,11 @@ namespace spring {
         std::vector<int64_t> tokens[128][8];
     };
 
-    std::pair<std::vector<utils::FastqRecord>, std::vector<bool>>
-    decode_streams(decoded_desc_t &desc, const std::vector<std::array<uint8_t, 2>> &subseq_indices, bool paired_end, bool preserve_quality, bool preserve_id);
+    void decode_streams(decoded_desc_t &dec, const std::vector<std::array<uint8_t, 2>> &subseq_indices, bool paired_end, bool preserve_quality, bool preserve_id, bool combine_pairs, std::vector<utils::FastqRecord> matched_records[2], std::vector<utils::FastqRecord> unmatched_records[2], std::vector<uint32_t> &mate_au_id, std::vector<uint32_t> &mate_record_index);
 
     bool decompress(const std::string &temp_dir, dsg::StreamSaver *ld, bool combine_pairs);
+
+    void write_fastq_record_to_ostream(std::ostream &out, utils::FastqRecord &fastqRecord, bool preserve_quality);
 
 }  // namespace spring
 
