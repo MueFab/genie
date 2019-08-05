ee02274fb7e9abef4fec1e7418d5bbf26f9b26f9
Shubham Chandak <schandak@stanford.edu>
2019-08-04 20:39:23 -0400

    cleanup (towards ureads) - not compiling yet

--- source/libs/spring/spring.cc
+++ source/libs/spring/spring.cc
@@ -13,6 +13,7 @@
 #include "encoder.h"
 #include "params.h"
 #include "preprocess.h"
+#include "compress-ureads.h"
 #include "reorder.h"
 #include "reorder-compress-quality-id.h"
 #include "generate-read-streams.h"
@@ -26,10 +27,12 @@
 
 namespace spring {
 
-    generated_aus generate_streams_SPRING(
-            utils::FastqFileReader *fastqFileReader1,
-            utils::FastqFileReader *fastqFileReader2, int num_thr,
-            bool paired_end, const std::string &working_dir, bool analyze, dsg::StreamSaver &st) {
+  generated_aus generate_streams_SPRING(
+          utils::FastqFileReader *fastqFileReader1,
+          utils::FastqFileReader *fastqFileReader2, int num_thr,
+          bool paired_end, const std::string &working_dir, bool analyze,
+          dsg::StreamSaver &st, bool ureads_flag, bool preserve_quality,
+          bool preserve_id) {
         // generate random temp directory in the working directory
         std::string temp_dir;
         while (true) {
@@ -46,7 +49,6 @@ namespace spring {
         auto compression_start = std::chrono::steady_clock::now();
 
         std::string infile_1, infile_2, outfile;
-        bool preserve_quality = true, preserve_id = true;
         compression_params *cp_ptr = new compression_params;
         memset(cp_ptr, 0, sizeof(compression_params));
         compression_params &cp = *cp_ptr;
@@ -55,6 +57,7 @@ namespace spring {
         cp.preserve_quality = preserve_quality;
         cp.num_reads_per_block = NUM_READS_PER_BLOCK;
         cp.num_thr = num_thr;
+        cp.ureads_flag = ureads_flag;
 
 #if GENIE_USE_OPENMP
        //
@@ -65,81 +68,88 @@ namespace spring {
        omp_set_dynamic(0);
 #endif
 
-        std::cout << "Preprocessing ...\n";
-        auto preprocess_start = std::chrono::steady_clock::now();
-        preprocess(fastqFileReader1, fastqFileReader2, temp_dir, cp);
-        auto preprocess_end = std::chrono::steady_clock::now();
-        std::cout << "Preprocessing done!\n";
-        std::cout << "Time for this step: "
-                  << std::chrono::duration_cast<std::chrono::seconds>(
-                          preprocess_end - preprocess_start)
-                          .count()
-                  << " s\n";
-
-        std::cout << "Reordering ...\n";
-        auto reorder_start = std::chrono::steady_clock::now();
-        call_reorder(temp_dir, cp);
-        auto reorder_end = std::chrono::steady_clock::now();
-        std::cout << "Reordering done!\n";
-        std::cout << "Time for this step: "
-                  << std::chrono::duration_cast<std::chrono::seconds>(reorder_end -
-                                                                      reorder_start)
-                          .count()
-                  << " s\n";
-
-        std::cout << "Encoding ...\n";
-        auto encoder_start = std::chrono::steady_clock::now();
-        call_encoder(temp_dir, cp);
-        auto encoder_end = std::chrono::steady_clock::now();
-        std::cout << "Encoding done!\n";
-        std::cout << "Time for this step: "
-                  << std::chrono::duration_cast<std::chrono::seconds>(encoder_end -
-                                                                      encoder_start)
-                          .count()
-                  << " s\n";
-
-        std::cout << "Generating read streams ...\n";
-        auto grs_start = std::chrono::steady_clock::now();
-        auto descriptorFilesPerAUs = generate_read_streams(temp_dir, cp, analyze, st);
-        auto grs_end = std::chrono::steady_clock::now();
-        std::cout << "Generating read streams done!\n";
-        std::cout << "Time for this step: "
-                  << std::chrono::duration_cast<std::chrono::seconds>(grs_end -
-                                                                      grs_start)
-                          .count()
-                  << " s\n";
-
-        cp.num_blocks = descriptorFilesPerAUs.size();
-
-        /* std::cout << "Generating new FASTQ\n";
-         auto new_fq_start = std::chrono::steady_clock::now();
-         if (!cp.paired_end) {
-             generate_new_fastq_se(fastqFileReader1, temp_dir, cp);
-         } else {
-             generate_new_fastq_pe(fastqFileReader1, fastqFileReader2, temp_dir, cp);
-         }
-         auto new_fq_end = std::chrono::steady_clock::now();
-         std::cout << "Generating new FASTQ done (for testing)\n";
-         std::cout << "Time for this step: "
-                   << std::chrono::duration_cast<std::chrono::seconds>(new_fq_end -
-                                                                       new_fq_start)
-                          .count()
-                   << " s\n";*/
-
-        generated_aus result(descriptorFilesPerAUs);
-        if (preserve_quality || preserve_id) {
-            std::cout << "Reordering and compressing quality and/or ids ...\n";
-            auto rcqi_start = std::chrono::steady_clock::now();
-            reorder_compress_quality_id(temp_dir, cp, analyze, st);
-            auto rcqi_end = std::chrono::steady_clock::now();
-            std::cout << "Reordering and compressing quality and/or ids done!\n";
+        std::vector<std::map<uint8_t, std::map<uint8_t, std::string>>> descriptorFilesPerAUs;
+        if (cp.ureads_flag) {
+            std::cout << "ureads_flag detected.\n";
+            // TODO: add support for analyze
+            descriptorFilesPerAUs =
+                    compress_ureads(fastqFileReader1, fastqFileReader2, temp_dir, cp, st);
+        } else {
+            std::cout << "Preprocessing ...\n";
+            auto preprocess_start = std::chrono::steady_clock::now();
+            preprocess(fastqFileReader1, fastqFileReader2, temp_dir, cp);
+            auto preprocess_end = std::chrono::steady_clock::now();
+            std::cout << "Preprocessing done!\n";
             std::cout << "Time for this step: "
-                      << std::chrono::duration_cast<std::chrono::seconds>(rcqi_end -
-                                                                          rcqi_start)
+                      << std::chrono::duration_cast<std::chrono::seconds>(
+                              preprocess_end - preprocess_start)
                               .count()
                       << " s\n";
-        }
 
+            std::cout << "Reordering ...\n";
+            auto reorder_start = std::chrono::steady_clock::now();
+            call_reorder(temp_dir, cp);
+            auto reorder_end = std::chrono::steady_clock::now();
+            std::cout << "Reordering done!\n";
+            std::cout << "Time for this step: "
+                      << std::chrono::duration_cast<std::chrono::seconds>(reorder_end -
+                                                                          reorder_start)
+                              .count()
+                      << " s\n";
+
+            std::cout << "Encoding ...\n";
+            auto encoder_start = std::chrono::steady_clock::now();
+            call_encoder(temp_dir, cp);
+            auto encoder_end = std::chrono::steady_clock::now();
+            std::cout << "Encoding done!\n";
+            std::cout << "Time for this step: "
+                      << std::chrono::duration_cast<std::chrono::seconds>(encoder_end -
+                                                                          encoder_start)
+                              .count()
+                      << " s\n";
+
+            std::cout << "Generating read streams ...\n";
+            auto grs_start = std::chrono::steady_clock::now();
+            descriptorFilesPerAUs = generate_read_streams(temp_dir, cp, analyze, st);
+            auto grs_end = std::chrono::steady_clock::now();
+            std::cout << "Generating read streams done!\n";
+            std::cout << "Time for this step: "
+                      << std::chrono::duration_cast<std::chrono::seconds>(grs_end -
+                                                                          grs_start)
+                              .count()
+                      << " s\n";
+
+            cp.num_blocks = descriptorFilesPerAUs.size();
+
+            /* std::cout << "Generating new FASTQ\n";
+             auto new_fq_start = std::chrono::steady_clock::now();
+             if (!cp.paired_end) {
+                 generate_new_fastq_se(fastqFileReader1, temp_dir, cp);
+             } else {
+                 generate_new_fastq_pe(fastqFileReader1, fastqFileReader2, temp_dir, cp);
+             }
+             auto new_fq_end = std::chrono::steady_clock::now();
+             std::cout << "Generating new FASTQ done (for testing)\n";
+             std::cout << "Time for this step: "
+                       << std::chrono::duration_cast<std::chrono::seconds>(new_fq_end -
+                                                                           new_fq_start)
+                              .count()
+                       << " s\n";*/
+
+            if (preserve_quality || preserve_id) {
+                std::cout << "Reordering and compressing quality and/or ids ...\n";
+                auto rcqi_start = std::chrono::steady_clock::now();
+                reorder_compress_quality_id(temp_dir, cp, analyze, st);
+                auto rcqi_end = std::chrono::steady_clock::now();
+                std::cout << "Reordering and compressing quality and/or ids done!\n";
+                std::cout << "Time for this step: "
+                          << std::chrono::duration_cast<std::chrono::seconds>(rcqi_end -
+                                                                              rcqi_start)
+                                  .count()
+                          << " s\n";
+            }
+        }
+        generated_aus result(descriptorFilesPerAUs);
         // Write compression params to a file
         std::string compression_params_file = "cp.bin";
         gabac::DataBlock d((uint8_t *) &cp, sizeof(compression_params), 1);
@@ -147,9 +157,6 @@ namespace spring {
         st.pack(d, compression_params_file);
 
         delete cp_ptr;
-
-        // TODO: remove temporary files
-        remove((temp_dir + "/read_order.bin").c_str());
 
         auto compression_end = std::chrono::steady_clock::now();
         std::cout << "Compression done!\n";
