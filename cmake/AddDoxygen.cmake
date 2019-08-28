80dfdda19786dda33fd06f9a81411dc2f545dced
Jan Voges <janvoges@me.com>
2019-08-28 15:02:27 +0200

    Add Doxygen documentation build (not tested)

---
+++ cmake/AddDoxygen.cmake
@@ -0,0 +1,28 @@
+find_package(Doxygen)
+
+if(DOXYGEN_FOUND)
+    message(STATUS "Doxygen is available")
+    message(STATUS "Issue 'make doc' to build documentation")
+
+    if(NOT DOXYGEN_DOT_FOUND)
+        message(WARNING
+            "'dot' tool from graphviz not found; "
+            "Doxygen documentation will not include graphs"
+        )
+    endif()
+
+    set(DOCUMENTATION_DIRECTORY "${CMAKE_BINARY_DIR}/doc")
+    set(DOXYFILE_IN "${CMAKE_SOURCE_DIR}/Doxyfile.in")
+    set(DOXYFILE_OUT "${DOCUMENTATION_DIRECTORY}/Doxyfile")
+
+    configure_file(${DOXYFILE_IN} ${DOXYFILE_OUT} @ONLY)
+
+    add_custom_target(doc
+        COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYFILE_OUT}
+        WORKING_DIRECTORY ${DOCUMENTATION_DIRECTORY}
+        COMMENT "Generating API documentation with Doxygen"
+        VERBATIM
+    )
+else()
+    message(WARNING "Doxygen not found; target 'doc' not available")
+endif()
