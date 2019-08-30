af325b244938a926badaca778d5d901a7597ea90
frankjannis <frank.jannis@gmail.com>
2019-08-30 16:49:39 +0200

    Added apply-clang-format.sh

--- scripts/apply-clang-format.sh
+++ scripts/apply-clang-format.sh
@@ -3,3 +3,16 @@
 set -euo pipefail
 
 git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
+
+echo running apply-clang-format.sh...
+
+GIT_REPO_PATH=$(git rev-parse --show-toplevel)
+cd $GIT_REPO_PATH
+
+echo checking ./source...
+find ./source -iname *.h -o -iname *.c -o -iname *.cc | xargs clang-format -style=file -i -fallback-style=none
+
+echo checking ./tests...
+find ./tests -iname *.h -o -iname *.c -o -iname *.cc | xargs clang-format -style=file -i -fallback-style=none
+
+echo Done!
