c14b838d58d30ed21da8c17173356970dd9d2984
Fabian Müntefering <fabian.muentefering@mail.de>
2019-09-03 16:34:23 +0200

    Cleanup 1

--- source/apps/ureads-encoder/part2_format_data_unit.h
+++ source/apps/ureads-encoder/part2_format_data_unit.h
@@ -6,12 +6,32 @@
 
 #include "part2_format_bitwriter.h"
 
-struct Data_unit {
+/**
+ * ISO 23092-2 Section 3.1 table 3
+ *
+ * base trunc containing only data unit size. See parameter_set, raw_reference, access_unit for specialisations
+ */
+class Data_unit {
+public:
+    /**
+     * ISO 23092-2 Section 3.1 table 4
+     * ------------------------------------------------------------------------------------------------------------- */
 
+    enum class Data_unit_type : uint8_t {
+        raw_reference = 0, //!< Line 1
+        parameter_set = 1, //!< Line 2
+        access_unit = 2 //!< Line 3
+    };
+
+    /** ------------------------------------------------------------------------------------------------------------ */
+
+    /**
+     * ISO 23092-2 Section 3.3.2
+     */
     enum class Dataset_type : uint8_t {
-        non_aligned = 0,
-        aligned = 1,
-        reference = 2
+        non_aligned = 0, //!< Line 3
+        aligned = 1, //!< Line 3
+        reference = 2 //!< Line 3
     };
 
     enum class AU_type : uint8_t {
@@ -24,17 +44,12 @@ struct Data_unit {
         U_TYPE_AU = 6
     };
 
-    enum class Data_unit_type : uint8_t {
-        raw_reference = 0,
-        parameter_set = 1,
-        access_unit = 2,
-        skip = 3
-    };
+private:
+    Data_unit_type data_unit_type;  //!< : 8; Line 2
 
-    Data_unit_type data_unit_type; // : 8
+public:
 
     explicit Data_unit (const Data_unit_type& t);
-
     virtual void write(BitWriter* write);
 };
 
