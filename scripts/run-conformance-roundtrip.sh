c91d8bf6e83a11af1118183eaac85459bf4dcc62
Jan Voges <janvoges@me.com>
2019-09-12 16:53:27 +0200

    Add comment to scripts/run-conformance-roundtrip.sh

--- scripts/run-conformance-roundtrip.sh
+++ scripts/run-conformance-roundtrip.sh
@@ -6,6 +6,9 @@ git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 
 readonly ureads_encoder="${git_root_dir}/cmake-build/bin/ureads-encoder"
+
+# Adjust this path to point to your mpegg-decoder-p2 executable!
+#        vvvvvvvvvvvvvvvv
 readonly mpegg_decoder_p2="/home/voges/repos/mpegg-reference-sw/build-debug/bin/decoder/mpegg-decoder-p2"
 
 readonly fastq="${git_root_dir}/resources/test-files/fastq/simplest.fastq"
