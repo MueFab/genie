b568f1adbc3c1b525fda9e54fb081256e2048974
Jan Voges <janvoges@me.com>
2019-09-16 22:48:57 +0200

    Better generate-authors-file.sh

--- util/generate-authors-file.sh
+++ util/generate-authors-file.sh
@@ -7,8 +7,13 @@
 set -euo pipefail
 
 git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
+readonly git_root_dir="$(git rev-parse --show-toplevel)"
+
+readonly authors_file="${git_root_dir}/AUTHORS"
 
 git shortlog --summary --email \
   | perl -spe 's/^\s+\d+\s+//' \
   | sed -e '/^CommitSyncScript.*$/d' \
-  > AUTHORS
+  > "${authors_file}"
+
+echo "created/updated authors file: ${authors_file}"
