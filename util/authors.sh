cc25066973df44d1955a178c0b2b67a08b92b7ca
Jan Voges <janvoges@me.com>
2019-09-17 22:15:46 +0200

    Update util/authors.sh

--- util/authors.sh
+++ util/authors.sh
@@ -9,6 +9,19 @@ set -euo pipefail
 git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 
+cmds=()
+cmds+=("git")
+cmds+=("perl")
+cmds+=("sed")
+
+for i in "${!cmds[@]}"; do
+    cmd=${cmds[${i}]}
+    if not command -v "${cmd}" &>/dev/null; then
+        error "command does not exist: ${cmd}"
+        exit 1
+    fi
+done
+
 readonly authors_file="${git_root_dir}/AUTHORS"
 
 git shortlog --summary --email \
