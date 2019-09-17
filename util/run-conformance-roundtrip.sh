68888a89993c348d753166321fec4a9683ef0e90
Jan Voges <janvoges@me.com>
2019-09-17 09:51:46 +0200

    Update run-conformance-roundtrip.sh

--- util/run-conformance-roundtrip.sh
+++ util/run-conformance-roundtrip.sh
@@ -5,18 +5,32 @@ set -euo pipefail
 git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 
-readonly ureads_encoder="${git_root_dir}/cmake-build/bin/ureads-encoder"
+readonly build_dir="${git_root_dir}/cmake-build-release"
+readonly ureads_encoder="${build_dir}/bin/ureads-encoder"
+if [[ ! -x "${ureads_encoder}" ]]; then
+    echo "error: ureads-encoder application does not exist: ${ureads_encoder}"
+    exit 1
+fi
 
 # Adjust this path to point to your mpegg-decoder-p2 executable!
 #        vvvvvvvvvvvvvvvv
-readonly mpegg_decoder_p2="/home/voges/repos/mpegg-reference-sw/build-debug/bin/decoder/mpegg-decoder-p2"
+readonly mpegg_decoder_p2="/Users/janvoges/Repositories/mpegg-reference-sw/build-debug/bin/decoder/mpegg-decoder-p2"
+if [[ ! -x "${mpegg_decoder_p2}" ]]; then
+    echo "error: mpegg-decoder-p2 application does not exist: ${mpegg_decoder_p2}"
+    exit 1
+fi
 
-readonly fastq="${git_root_dir}/resources/test-files/fastq/simplest.fastq"
-readonly bitstream="${fastq}.bitstream"
-readonly decoded="${bitstream}.decoded"
+readonly fastq_file="${git_root_dir}/resources/test-files/fastq/simplest.fastq"
+readonly bitstream_file="${fastq_file}.bitstream"
+readonly decoded_file="${bitstream_file}.decoded"
 
-"${ureads_encoder}" -i "${fastq}" -o "${bitstream}"
-"${mpegg_decoder_p2}" -i "${bitstream}" -o "${decoded}"
+"${ureads_encoder}" \
+    --input-file "${fastq_file}" \
+    --output-file "${bitstream_file}"
+"${mpegg_decoder_p2}" \
+    --verbose debug \
+    --bitstream "${bitstream_file}" \
+    --output "${decoded_file}"
 
-rm "${bitstream}"
-rm "${decoded}"
+rm "${bitstream_file}"
+rm "${decoded_file}"
