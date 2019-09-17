a4771d7ff6795e603a9503fc44c56226c3017b1d
Jan Voges <janvoges@me.com>
2019-09-17 22:33:34 +0200

    Update util/shellcheck.sh

--- util/shellcheck.sh
+++ util/shellcheck.sh
@@ -5,22 +5,40 @@ set -euo pipefail
 git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 
+cmds=()
+cmds+=("shellcheck")
+
+for i in "${!cmds[@]}"; do
+    cmd=${cmds[${i}]}
+    if not command -v "${cmd}" &>/dev/null; then
+        echo "error: command does not exist: ${cmd}"
+        exit 1
+    fi
+done
+
 dirs=()
 dirs+=("${git_root_dir}/ci")
 dirs+=("${git_root_dir}/source")
 dirs+=("${git_root_dir}/tests")
 dirs+=("${git_root_dir}/util")
 
+extensions=()
+extensions+=("*.sh")
+
 files=()
+
 for i in "${!dirs[@]}"; do
     dir=${dirs[${i}]}
-    while IFS='' read -r line; do
-        files+=("${line}");
-    done < <(find "${dir}" -name '*.sh')
+    for j in "${!extensions[@]}"; do
+        extension=${extensions[${j}]}
+        while IFS='' read -r line; do
+            files+=("${line}");
+        done < <(find "${dir}" -name "${extension}")
+    done
 done
 
 for i in "${!files[@]}"; do
     file=${files[${i}]}
-    echo "processing: ${file}"
+    echo "running shellcheck on: ${file}"
     shellcheck "${file}"
 done
