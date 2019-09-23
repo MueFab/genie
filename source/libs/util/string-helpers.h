b04f36e85c27be205f47960153b733fd43fb2f5e
Jan Voges <janvoges@me.com>
2019-09-23 18:30:24 +0200

    Fix linker error

--- source/libs/util/string-helpers.h
+++ source/libs/util/string-helpers.h
@@ -5,19 +5,13 @@
 
 namespace util {
 
-const char *ws = " \t\n\r\f\v";
+static const char *ws = " \t\n\r\f\v";
 
-std::string &rtrim(std::string &s, const char *t = ws) {
-    s.erase(s.find_last_not_of(t) + 1);
-    return s;
-}
+std::string &rtrim(std::string &s, const char *t = ws);
 
-std::string &ltrim(std::string &s, const char *t = ws) {
-    s.erase(0, s.find_first_not_of(t));
-    return s;
-}
+std::string &ltrim(std::string &s, const char *t = ws);
 
-inline std::string &trim(std::string &s, const char *t = ws) { return ltrim(rtrim(s, t), t); }
+std::string &trim(std::string &s, const char *t = ws);
 
 }  // namespace util
 
