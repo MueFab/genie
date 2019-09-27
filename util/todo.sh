54819f9fd36e1a31c32184826a9db5ab39dae199
Jan Voges <voges@tnt.uni-hannover.de>
2019-09-27 18:16:33 +0200

    Add todo.sh

---
+++ util/todo.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+self="${0}"
+self_name="${self##*/}"
+
+git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
+readonly git_root_dir="$(git rev-parse --show-toplevel)"
+
+cmds=()
+cmds+=("grep")
+
+for i in "${!cmds[@]}"; do
+    cmd=${cmds[${i}]}
+    if not command -v "${cmd}" &>/dev/null; then
+        echo "[${self_name}] error: command does not exist: ${cmd}"
+        exit 1
+    fi
+done
+
+dirs=()
+dirs+=("${git_root_dir}/ci")
+dirs+=("${git_root_dir}/src")
+dirs+=("${git_root_dir}/test")
+dirs+=("${git_root_dir}/util")
+
+for i in "${!dirs[@]}"; do
+    dir=${dirs[${i}]}
+    echo "[${self_name}] listing TODOs and FIXMEs in tree: ${dir}"
+    set +e
+    grep --color --recursive --line-number --word-regexp 'TODO' "${dir}"
+    grep --color --recursive --line-number --word-regexp 'FIXME' "${dir}"
+    set -e
+done
