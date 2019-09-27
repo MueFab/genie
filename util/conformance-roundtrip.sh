cad473d2323baaa116be4d7ac28d700267b8f25d
Jan Voges <voges@tnt.uni-hannover.de>
2019-09-27 18:25:21 +0200

    Update conformance-roundtrip.sh

--- util/conformance-roundtrip.sh
+++ util/conformance-roundtrip.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+self="${0}"
+self_name="${self##*/}"
+
 git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 
@@ -11,7 +14,7 @@ cmds+=("rm")
 for i in "${!cmds[@]}"; do
     cmd=${cmds[${i}]}
     if not command -v "${cmd}" &>/dev/null; then
-        echo "error: command does not exist: ${cmd}"
+        echo "[${self_name}] error: command does not exist: ${cmd}"
         exit 1
     fi
 done
@@ -19,15 +22,15 @@ done
 readonly build_dir="${git_root_dir}/cmake-build-release"
 readonly ureads_encoder="${build_dir}/bin/ureads-encoder"
 if [[ ! -x "${ureads_encoder}" ]]; then
-    echo "error: ureads-encoder application does not exist: ${ureads_encoder}"
+    echo "[${self_name}] error: ureads-encoder application does not exist: ${calq_tests_app}"
     exit 1
 fi
 
 # Adjust this path to point to your mpegg-decoder-p2 executable!
 #        vvvvvvvvvvvvvvvv
-readonly mpegg_decoder_p2="/Users/janvoges/Repositories/mpegg-reference-sw/build-debug/bin/decoder/mpegg-decoder-p2"
+readonly mpegg_decoder_p2="/path/to/mpegg-reference-sw/build-debug/bin/decoder/mpegg-decoder-p2"
 if [[ ! -x "${mpegg_decoder_p2}" ]]; then
-    echo "error: mpegg-decoder-p2 application does not exist: ${mpegg_decoder_p2}"
+    echo "[${self_name}] error: mpegg-decoder-p2 application does not exist: ${mpegg_decoder_p2}"
     exit 1
 fi
 
