49aa5bea6b82b640c8fafc9244732d72a739c612
Jan Voges <voges@tnt.uni-hannover.de>
2019-09-30 13:46:06 +0200

    Update cmake-build-release.sh

--- ci/cmake-build-release.sh
+++ ci/cmake-build-release.sh
@@ -7,10 +7,8 @@ git rev-parse --git-dir 1>/dev/null # Exit if not inside Git repo
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 
 readonly build_dir="${git_root_dir}/cmake-build-release"
-if [[ ! -d "${build_dir}" ]]; then
-    mkdir -p "${build_dir}"
-fi
 
+mkdir --parents "${build_dir}"
 cd "${build_dir}"
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make --jobs
