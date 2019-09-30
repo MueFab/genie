7075aab30256d8a8a0645995d3a901d59166d660
Jan Voges <voges@tnt.uni-hannover.de>
2019-09-30 09:43:48 +0200

    Update util scripts

--- util/before-commit.sh
+++ util/before-commit.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 self="${0}"
 self_name="${self##*/}"
 
+# Get Git root directory
 git rev-parse --git-dir 1>/dev/null # Exit if not inside Git repo
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 
@@ -14,7 +15,7 @@ run() {
 }
 
 run "${git_root_dir}/util/authors.sh"
-run "${git_root_dir}/util/autopep8-pycodestyle.sh"
+# run "${git_root_dir}/util/autopep8-pycodestyle.sh"
 run "${git_root_dir}/util/clang-format.sh"
 run "${git_root_dir}/util/shellcheck.sh"
 run "${git_root_dir}/util/todo.sh"
--- util/conformance-roundtrip.sh
+++ util/conformance-roundtrip.sh
@@ -5,12 +5,11 @@ set -euo pipefail
 self="${0}"
 self_name="${self##*/}"
 
-git rev-parse --git-dir 1>/dev/null # Exit if not inside Git repo
-readonly git_root_dir="$(git rev-parse --show-toplevel)"
-
+# Check whether all required commands are available
 cmds=()
+cmds+=("git")
 cmds+=("rm")
-
+#
 for i in "${!cmds[@]}"; do
     cmd=${cmds[${i}]}
     if not command -v "${cmd}" &>/dev/null; then
@@ -19,6 +18,11 @@ for i in "${!cmds[@]}"; do
     fi
 done
 
+# Get Git root directory
+git rev-parse --git-dir 1>/dev/null # Exit if not inside Git repo
+readonly git_root_dir="$(git rev-parse --show-toplevel)"
+
+# Get the Genie encoder
 readonly build_dir="${git_root_dir}/cmake-build-release"
 readonly ureads_encoder="${build_dir}/bin/ureads-encoder"
 if [[ ! -x "${ureads_encoder}" ]]; then
@@ -26,6 +30,7 @@ if [[ ! -x "${ureads_encoder}" ]]; then
     exit 1
 fi
 
+# Get the MPEG-G reference software decoder
 # Adjust this path to point to your mpegg-decoder-p2 executable!
 #        vvvvvvvvvvvvvvvv
 readonly mpegg_decoder_p2="/path/to/mpegg-reference-sw/build-debug/bin/decoder/mpegg-decoder-p2"
@@ -34,18 +39,22 @@ if [[ ! -x "${mpegg_decoder_p2}" ]]; then
     exit 1
 fi
 
+# Test data
 readonly fastq_file="${git_root_dir}/data/fastq/simplest.fastq"
 readonly bitstream_file="${fastq_file}.bitstream"
 readonly decoded_file="${bitstream_file}.decoded"
 
+# Run the encoder
 "${ureads_encoder}" \
     --input-file "${fastq_file}" \
     --output-file "${bitstream_file}"
 
+# Run the decoder
 "${mpegg_decoder_p2}" \
     --verbose debug \
     --bitstream "${bitstream_file}" \
     --output "${decoded_file}"
 
+# Cleanup
 rm "${bitstream_file}"
 rm "${decoded_file}"
