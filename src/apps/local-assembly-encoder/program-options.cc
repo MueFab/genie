b874baaf225d87b9126894dcf263c9ded955b55e
Fabian Müntefering <fabian.muentefering@mail.de>
2019-11-21 12:15:26 +0100

    Add script to generate conformance streams

--- src/apps/local-assembly-encoder/program-options.cc
+++ src/apps/local-assembly-encoder/program-options.cc
@@ -17,7 +17,13 @@ void ProgramOptions::processCommandLine(int argc, char *argv[]) {
 
     app.add_option("-i,--input-file", inputFilePath, "Input file")->mandatory(true);
     app.add_option("-o,--output-file", outputFilePath, "Output file");
-    app.add_option("-t,--AU-type", typeString, "AU Type[U, P, N, M, I]");
+    app.add_option("-t,--au-type", typeString, "AU Type[U, P, N, M, I]");
+
+    try {
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError &e) {
+        DIE("Program options error: " + std::to_string(app.exit(e)));
+    }
 
     if(typeString == "U") {
         type = format::DataUnit::AuType::U_TYPE_AU;
@@ -31,12 +37,6 @@ void ProgramOptions::processCommandLine(int argc, char *argv[]) {
         type = format::DataUnit::AuType::I_TYPE_AU;
     } else {
         DIE("Invalid AU Type");
-    }
-
-    try {
-        app.parse(argc, argv);
-    } catch (const CLI::ParseError &e) {
-        DIE("Program options error: " + std::to_string(app.exit(e)));
     }
 
     if (!ghc::filesystem::exists(ghc::filesystem::path(inputFilePath))) {
