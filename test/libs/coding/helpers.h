4c322c49f65f737338fab14f735a72b988fd1cef
joshfactorial <jallen17@illinois.edu>
2019-12-17 14:56:05 -0600

    Trying to set up some new unit tests

---
+++ test/libs/coding/helpers.h
@@ -0,0 +1,12 @@
+#ifndef UTIL_TESTS_HELPERS_H_
+#define UTIL_TESTS_HELPERS_H_
+
+#include <string>
+
+namespace util_tests {
+
+std::string exec(const std::string &cmd);
+
+}  // namespace util_tests
+
+#endif  // UTIL_TESTS_HELPERS_H_
