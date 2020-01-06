a8850eacf937d08f5e8cbe18429efdb390725f4e
Fabian Müntefering <fabian.muentefering@mail.de>
2020-01-06 20:51:00 +0100

    Add LAE application

--- src/apps/local-assembly-encoder/encoding.cc
+++ src/apps/local-assembly-encoder/encoding.cc
@@ -3,29 +3,61 @@
 #include <util/log.h>
 
 #include <coding/gabac-compressor.h>
+#include <coding/gabac-decompressor.h>
+#include <coding/local-assembly-decoder.h>
 #include <coding/local-assembly-encoder.h>
 #include <format/mpegg_p2/mpegg-p-2-exporter.h>
+#include <format/mpegg_p2/mpegg-p-2-importer.h>
+#include <format/sam/sam-exporter.h>
+#include <format/sam/sam-importer.h>
 #include <util/thread-manager.h>
 #include <fstream>
 
 namespace lae {
 
 void encode(const ProgramOptions &programOptions) {
- /*   std::ifstream infile("test.sam");
-    SamImporter samimporter(10000, &infile);
-    LocalAssemblyEncoder laencoder(1000, false);
-    GabacCompressor gabacCompressor;
+    if (!programOptions.decompression) {
+        std::ifstream infile(programOptions.inputFilePath);
+        const size_t RECORDS_PER_BLOCK = 10000;
+        format::sam::SamImporter importer(RECORDS_PER_BLOCK, infile);
 
-    std::ofstream file("test.mpgb");
-    MpeggP2Exporter exporter(&file);
+        const size_t LOCAL_ASSEMBLY_BUFFER_SIZE = 2000;
+        LocalAssemblyEncoder encoder(LOCAL_ASSEMBLY_BUFFER_SIZE, false);
 
-    samimporter.setDrain(&laencoder);
-    laencoder.setDrain(&gabacCompressor);
-    gabacCompressor.setDrain(&exporter);
+        GabacCompressor compressor;
 
-    ThreadManager threads(1, &samimporter);
+        std::ofstream outfile(programOptions.outputFilePath);
+        MpeggP2Exporter exporter(&outfile);
 
-    threads.run();*/
+        importer.setDrain(&encoder);
+        encoder.setDrain(&compressor);
+        compressor.setDrain(&exporter);
+
+        const size_t NUM_THREADS = 8;
+        ThreadManager threads(NUM_THREADS, &importer);
+
+        threads.run();
+    } else {
+        std::ifstream infile(programOptions.inputFilePath);
+        MpeggP2Importer importer(infile);
+
+        LocalAssemblyDecoder decoder;
+
+        GabacDecompressor decompressor;
+
+        std::ofstream outfile(programOptions.outputFilePath);
+        SamExporter exporter(format::sam::SamFileHeader::createDefaultHeader(), outfile);
+
+        importer.setDrain(&decompressor);
+        decompressor.setDrain(&decoder);
+        decoder.setDrain(&exporter);
+
+        ThreadManager threads(programOptions.num_threads, &importer);
+
+        threads.run();
+    }
+
+    std::cout << "Finished successfully!" << std::endl;
 }
 
 }  // namespace lae
--- src/apps/local-assembly-encoder/program-options.cc
+++ src/apps/local-assembly-encoder/program-options.cc
@@ -6,7 +6,7 @@
 
 namespace lae {
 
-ProgramOptions::ProgramOptions(int argc, char *argv[]) : inputFilePath(), outputFilePath(), typeString("I") {
+ProgramOptions::ProgramOptions(int argc, char *argv[]) : inputFilePath(), outputFilePath(), decompression(false), num_threads(1), forceOverride(false) {
     processCommandLine(argc, argv);
 }
 
@@ -16,8 +16,10 @@ void ProgramOptions::processCommandLine(int argc, char *argv[]) {
     CLI::App app("Local assembly encoder");
 
     app.add_option("-i,--input-file", inputFilePath, "Input file")->mandatory(true);
-    app.add_option("-o,--output-file", outputFilePath, "Output file");
-    app.add_option("-t,--au-type", typeString, "AU Type[U, P, N, M, I]");
+    app.add_option("-o,--output-file", outputFilePath, "Output file")->mandatory(true);
+    num_threads = 1;
+    app.add_option("-t,--threads", num_threads, "Number of threads");
+    app.add_flag("-f,--force-override", forceOverride, "Override output file if already existing");
 
     try {
         app.parse(argc, argv);
@@ -25,27 +27,38 @@ void ProgramOptions::processCommandLine(int argc, char *argv[]) {
         DIE("Program options error: " + std::to_string(app.exit(e)));
     }
 
-    if(typeString == "U") {
-        type = format::mpegg_rec::ClassType::CLASS_U;
-    } else if(typeString == "P") {
-        type = format::mpegg_rec::ClassType::CLASS_P;
-    } else if(typeString == "N") {
-        type = format::mpegg_rec::ClassType::CLASS_N;
-    } else if(typeString == "M") {
-        type = format::mpegg_rec::ClassType::CLASS_M;
-    } else if(typeString == "I") {
-        type = format::mpegg_rec::ClassType::CLASS_I;
-    } else {
-        DIE("Invalid AU Type");
-    }
-
     if (!ghc::filesystem::exists(ghc::filesystem::path(inputFilePath))) {
         DIE("Input file does not exist: " + inputFilePath);
     }
 
-    if (ghc::filesystem::exists(ghc::filesystem::path(outputFilePath))) {
-   //     DIE("Output file exists already: " + outputFilePath);
+    if (!forceOverride && ghc::filesystem::exists(ghc::filesystem::path(outputFilePath))) {
+        DIE("Output already existing: " + outputFilePath);
     }
+
+    auto inDot = inputFilePath.find_last_of('.');
+    if (inDot == std::string::npos) {
+        DIE("Input file has no extension");
+    }
+    std::string inExtension = inputFilePath.substr(inDot + 1);
+
+    auto outDot = outputFilePath.find_last_of('.');
+    if (outDot == std::string::npos) {
+        DIE("Output file has no extension");
+    }
+    std::string outExtension = outputFilePath.substr(outDot + 1);
+
+    if (inExtension == "sam" && outExtension == "mgb") {
+        std::cout << "Detected sam input + mgb output! Compressing..." << std::endl;
+        decompression = false;
+    } else if (inExtension == "mgb" && outExtension == "sam") {
+        std::cout << "Detected mgb input + sam output! Decompressing..." << std::endl;
+        decompression = true;
+    } else {
+        DIE("Could not infer task from input/output file extensions. Make sure to use sam input + mgb output (for "
+            "compression) or mgb input + sam output (for decompression).");
+    }
+
+    std::cout << "Number of threads: " << uint32_t(num_threads) << std::endl;
 }
 
 }  // namespace lae
