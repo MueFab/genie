1655339debc604f5a395ae6614a60393f44f9600
frankjannis <frank.jannis@gmail.com>
2020-01-29 15:59:40 +0100

    (encapsulator) made Dataset iterate over ParamSets

--- src/genie/format/mpegg_p1/dataset.cc
+++ src/genie/format/mpegg_p1/dataset.cc
@@ -12,10 +12,16 @@ namespace mpegg_p1 {
 
 Dataset::Dataset(const genie::format::mgb::DataUnitFactory& dataUnitFactory,
                  std::vector<genie::format::mgb::AccessUnit>& accessUnits_p2) {
-    //(void)dataUnitFactory;  // silence compiler warning
 
     // TODO multiple params?
-    dataset_parameter_sets.emplace_back(dataUnitFactory.getParams(0));
+    for (unsigned int i = 0;; ++i) {
+        try {
+            dataset_parameter_sets.emplace_back(dataUnitFactory.getParams(i));
+        } catch (const std::out_of_range&) {
+            std::cout << "Got " << i << " ParameterSet/s from DataUnitFactory" << std::endl;
+            break;
+        }
+    }
 
     for (auto& au : accessUnits_p2) {
         access_units.emplace_back(std::move(au));
