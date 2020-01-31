6844d8f7ce172a0dd7ee7749999ec3c3ae44b48c
Jannis Frank <frank.jannis@gmail.com>
2020-01-31 11:57:11 +0100

    (encapsulator) added dataset_group_ID setter

--- src/genie/format/mpegg_p1/dataset_parameter_set.h
+++ src/genie/format/mpegg_p1/dataset_parameter_set.h
@@ -24,6 +24,8 @@ class DatasetParameterSet {
     uint16_t getDatasetID() { return dataset_ID; }
     uint8_t getDatasetGroupID() { return dataset_group_ID; }
 
+    void setDatasetGroupID(uint8_t x_dataset_group_ID) { dataset_group_ID = x_dataset_group_ID; }
+
    private:
     /**
      * ISO 23092-1 Section 6.5.2 table 23
