ef1369f77d5d98d64ca561c2e6b241c1caca0e84
Junaid Ahmad <junaid@jnomics.com>
2020-03-12 15:31:57 +0000

    (entropy/paramcabac) Continue with state_vars.cc/h (2)

--- src/genie/entropy/paramcabac/state_vars.cc
+++ src/genie/entropy/paramcabac/state_vars.cc
@@ -30,9 +30,6 @@ void StateVars::populate(const SupportValues::TransformIdSubsym transform_ID_sub
                          const SupportValues support_values,
                          const Binarization cabac_binarization) {
 
-    const uint8_t codingSubsymSize = support_values.getCodingSubsymSize();
-    const uint8_t outputSymbolSize = support_values.getOutputSymbolSize();
-    const uint8_t codingOrder = support_values.getCodingOrder();
     const BinarizationParameters::BinarizationId binarization_ID = cabac_binarization.getBinarizationID();
     const BinarizationParameters& cabacBinazParams = cabac_binarization.getCabacBinarizationParameters();
     const Context& cabacContextParams = cabac_binarization.getCabacContextParameters();
@@ -41,14 +38,21 @@ void StateVars::populate(const SupportValues::TransformIdSubsym transform_ID_sub
     core::GenSubIndex subsequence_ID = core::GenSub::POS_MAPPING_FIRST; // need subsequence_ID to be supplied to this function TODO
     core::AlphabetID alphabet_ID = core::AlphabetID::ACGTN;             // need alphabet_ID to be supplied to this function TODO
 
-    // numSubSymbols
-    if(codingSubsymSize > 0) { // TODO: should do assert or return error?
+    const uint8_t codingSubsymSize = support_values.getCodingSubsymSize();
+    const uint8_t outputSymbolSize = support_values.getOutputSymbolSize();
+    const uint8_t codingOrder = support_values.getCodingOrder();
+    const bool shareSubSymCtxFlag = cabacContextParams.getShareSubsymCtxFlag();
+    const uint16_t numContexts = cabacContextParams.getNumContexts();
+
+
+    // numSubsyms
+    if(codingSubsymSize > 0) {
         numSubsyms = outputSymbolSize / codingSubsymSize;
     } else {
         UTILS_THROW_RUNTIME_EXCEPTION("coding_subsym_size = "+std::to_string(codingSubsymSize)+" not supported");
     }
 
-    // numAlphabetSymbols
+    // numAlphaSubsym
     // Check for special cases for numAlphabetSymbols
     if(descriptor_ID == core::GenDesc::MMTYPE || descriptor_ID == core::GenDesc::RFTT) {
         if(subsequence_ID == core::GenSub::MMTYPE_TYPE || subsequence_ID == core::GenSub::RFTT_TYPE) { // subseq 0
@@ -74,7 +78,7 @@ void StateVars::populate(const SupportValues::TransformIdSubsym transform_ID_sub
         numAlphaSubsym = (1<<codingSubsymSize);
     }
 
-    /* numContextsSymbol */
+    // numCtxSubsym
     switch(binarization_ID) {
         case BinarizationParameters::BinarizationId::BINARY_CODING:
             numCtxSubsym = codingSubsymSize;
@@ -133,6 +137,53 @@ void StateVars::populate(const SupportValues::TransformIdSubsym transform_ID_sub
                          + 1;
         }
         break;
+    }
+
+    // codingOrderCtxOffset[]
+    codingOrderCtxOffset[0] = 0;
+    if(codingOrder >= 1)
+        codingOrderCtxOffset[1] = numCtxSubsym;
+    if(codingOrder == 2)
+        codingOrderCtxOffset[2] = numCtxSubsym * numAlphaSubsym;
+
+    // codingSizeCtxOffset
+    if(shareSubSymCtxFlag) {
+        codingSizeCtxOffset = 0;
+    } else if(codingOrder == 0) {
+        codingSizeCtxOffset = numCtxSubsym;
+    } else {
+        codingSizeCtxOffset = codingOrderCtxOffset[codingOrder] *  numAlphaSubsym;
+    }
+
+    // numCtxLuts
+    numCtxLuts = 0;
+    if(transform_ID_subsym == SupportValues::TransformIdSubsym::LUT_TRANSFORM) {
+        if((binarization_ID >= BinarizationParameters::BinarizationId::SPLIT_UNITWISE_TRUNCATED_UNARY &&
+                binarization_ID <= BinarizationParameters::BinarizationId::SIGNED_DOUBLE_TRUNCATED_UNARY) ||
+                codingOrder == 0 ||
+                (1u<<codingSubsymSize) > MAX_LUT_SIZE) {
+            UTILS_THROW_RUNTIME_EXCEPTION("LUT_TRANSFORM not supported with given configuration: coding_order = 0\
+                                         , binarization_ID = "+std::to_string((uint8_t)binarization_ID)+"\
+                                         , coding_subsym_size = "+std::to_string(codingSubsymSize));
+        } else {
+            numCtxLuts = (codingSubsymSize/2)
+                       * ((1<<2)-1)
+                       + ((1<<(codingSubsymSize%2))-1);
+        }
+    }
+
+   // numCtxTotal
+    if(numContexts != 0) {
+        numCtxTotal = numContexts;
+    } else {
+        numCtxTotal = numCtxLuts;
+        numCtxTotal += (shareSubSymCtxFlag) ?
+                        1 :
+                        numSubsyms *
+                        ((codingOrder == 0) ?
+                                numCtxSubsym :
+                                codingOrderCtxOffset[codingOrder] *
+                                numAlphaSubsym);
     }
 }
 
