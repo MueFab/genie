07e59893b99c2a03b257b7314dc750e8bfeef289
Junaid Ahmad <junaid@jnomics.com>
2020-03-17 14:50:21 +0000

    (gabac/gabac) paramcabac integration with encode*cc/h

--- src/genie/entropy/gabac/encode-cabac.cc
+++ src/genie/entropy/gabac/encode-cabac.cc
@@ -18,49 +18,75 @@ namespace genie {
 namespace entropy {
 namespace gabac {
 
-void encode_cabac(const BinarizationId& binarizationId, const std::vector<unsigned int>& binarizationParameters,
-                  const ContextSelectionId& contextSelectionId, util::DataBlock* const symbols, size_t maxSize) {
+void encode_cabac(const paramcabac::TransformedSeq &conf,
+                  util::DataBlock* const symbols,
+                  size_t maxSize) {
     util::DataBlock block(0, 1);
+
     assert(symbols != nullptr);
 #ifndef NDEBUG
-    const unsigned int paramSize[unsigned(BinarizationId::STEG) + 1u] = {1, 1, 0, 0, 1, 1}; // FIXME what is this??? TBC
+    //const unsigned int paramSize[unsigned(BinarizationId::STEG) + 1u] = {1, 1, 0, 0, 1, 1}; // FIXME what is this??? TBC
 #endif
-    assert(binarizationParameters.size() >= paramSize[static_cast<int>(binarizationId)]);
+    //assert(binarzation.getCabacBinarizationParameters().size() >= paramSize[static_cast<int>(binarzation.binarization_ID)]);
 
     size_t numSymbols = symbols->size();
     if (numSymbols <= 0) return;
+
+    const paramcabac::SupportValues &supportVals = conf.getSupportValues();
+    const paramcabac::Binarization &binarzation = conf.getBinarization();
+    const paramcabac::BinarizationParameters &binarzationParams = binarzation.getCabacBinarizationParameters();
+    const paramcabac::StateVars &stateVars = conf.getStateVars();
 
     OBufferStream bitstream(&block);
     Writer writer(&bitstream);
     writer.start(numSymbols);
 
     unsigned int binarizationParameter = 0;
-    if (!binarizationParameters.empty()) {
-        binarizationParameter = binarizationParameters[0];
-    }
 
     util::BlockStepper r = symbols->getReader();
 
-    if (contextSelectionId == ContextSelectionId::bypass) {
+    if (binarzation.getBypassFlag()) { // bypass mode
         void (Writer::*func)(uint64_t, unsigned int);
-        switch (binarizationId) {
-            case BinarizationId::BI:
+        switch (binarzation.getBinarizationID()) {
+            case paramcabac::BinarizationParameters::BinarizationId::BINARY_CODING:
                 func = &Writer::writeAsBIbypass;
+                binarizationParameter = stateVars.getCLengthBI();
                 break;
-            case BinarizationId::TU:
+            case paramcabac::BinarizationParameters::BinarizationId::TRUNCATED_UNARY:
                 func = &Writer::writeAsTUbypass;
+                binarizationParameter = binarzationParams.getCMax();
                 break;
-            case BinarizationId::EG:
+            case paramcabac::BinarizationParameters::BinarizationId::EXPONENTIAL_GOLOMB:
                 func = &Writer::writeAsEGbypass;
                 break;
-            case BinarizationId::SEG:
+            case paramcabac::BinarizationParameters::BinarizationId::SIGNED_EXPONENTIAL_GOMB:
                 func = &Writer::writeAsSEGbypass;
                 break;
-            case BinarizationId::TEG:
+            case paramcabac::BinarizationParameters::BinarizationId::TRUNCATED_EXPONENTIAL_GOLOMB:
                 func = &Writer::writeAsTEGbypass;
+                binarizationParameter = binarzationParams.getCMaxTeg();
                 break;
-            case BinarizationId::STEG:
+            case paramcabac::BinarizationParameters::BinarizationId::SIGNED_TRUNCATED_EXPONENTIAL_GOLOMB:
                 func = &Writer::writeAsSTEGbypass;
+                binarizationParameter = binarzationParams.getCMaxTeg();
+                break;
+            case paramcabac::BinarizationParameters::BinarizationId::SPLIT_UNITWISE_TRUNCATED_UNARY:
+                //func = &Writer::writeAsSUTUbypass; TODO
+                binarizationParameter = binarzationParams.getSplitUnitSize();
+                break;
+            case paramcabac::BinarizationParameters::BinarizationId::SIGNED_SPLIT_UNITWISE_TRUNCATED_UNARY:
+                //func = &Writer::writeAsSSUTUbypass; TODO
+                binarizationParameter = binarzationParams.getSplitUnitSize();
+                break;
+            case paramcabac::BinarizationParameters::BinarizationId::DOUBLE_TRUNCATED_UNARY:
+                //func = &Writer::writeAsDTUbypass; TODO
+                //binarizationParameter = binarzationParams.getCMaxDtu(); // TODO make binarizationParameter a vector
+                binarizationParameter = binarzationParams.getSplitUnitSize();
+                break;
+            case paramcabac::BinarizationParameters::BinarizationId::SIGNED_DOUBLE_TRUNCATED_UNARY:
+                //func = &Writer::writeAsSDTUbypass; TODO
+                //binarizationParameter = binarzationParams.getCMaxDtu(); // TODO make binarizationParameter a vector
+                binarizationParameter = binarzationParams.getSplitUnitSize();
                 break;
             default:
                 GABAC_DIE("Unknown Binarization");
@@ -81,30 +107,53 @@ void encode_cabac(const BinarizationId& binarizationId, const std::vector<unsign
     }
 
     void (Writer::*func)(uint64_t, unsigned int, unsigned int);
-    switch (binarizationId) {
-        case BinarizationId::BI:
+    switch (binarzation.getBinarizationID()) {
+        case paramcabac::BinarizationParameters::BinarizationId::BINARY_CODING:
             func = &Writer::writeAsBIcabac;
+            binarizationParameter = stateVars.getCLengthBI();
             break;
-        case BinarizationId::TU:
+        case paramcabac::BinarizationParameters::BinarizationId::TRUNCATED_UNARY:
             func = &Writer::writeAsTUcabac;
+            binarizationParameter = binarzationParams.getCMax();
             break;
-        case BinarizationId::EG:
+        case paramcabac::BinarizationParameters::BinarizationId::EXPONENTIAL_GOLOMB:
             func = &Writer::writeAsEGcabac;
             break;
-        case BinarizationId::SEG:
+        case paramcabac::BinarizationParameters::BinarizationId::SIGNED_EXPONENTIAL_GOMB:
             func = &Writer::writeAsSEGcabac;
             break;
-        case BinarizationId::TEG:
+        case paramcabac::BinarizationParameters::BinarizationId::TRUNCATED_EXPONENTIAL_GOLOMB:
             func = &Writer::writeAsTEGcabac;
+            binarizationParameter = binarzationParams.getCMaxTeg();
             break;
-        case BinarizationId::STEG:
+        case paramcabac::BinarizationParameters::BinarizationId::SIGNED_TRUNCATED_EXPONENTIAL_GOLOMB:
             func = &Writer::writeAsSTEGcabac;
+            binarizationParameter = binarzationParams.getCMaxTeg();
+            break;
+        case paramcabac::BinarizationParameters::BinarizationId::SPLIT_UNITWISE_TRUNCATED_UNARY:
+            //func = &Writer::writeAsSUTUcabac; TODO
+            binarizationParameter = binarzationParams.getSplitUnitSize();
+            break;
+        case paramcabac::BinarizationParameters::BinarizationId::SIGNED_SPLIT_UNITWISE_TRUNCATED_UNARY:
+            //func = &Writer::writeAsSSUTUcabac; TODO
+            binarizationParameter = binarzationParams.getSplitUnitSize();
+            break;
+        case paramcabac::BinarizationParameters::BinarizationId::DOUBLE_TRUNCATED_UNARY:
+            //func = &Writer::writeAsDTUcabac; TODO
+            //binarizationParameter = binarzationParams.getCMaxDtu(); // TODO make binarizationParameter a vector
+            binarizationParameter = binarzationParams.getSplitUnitSize();
+            break;
+        case paramcabac::BinarizationParameters::BinarizationId::SIGNED_DOUBLE_TRUNCATED_UNARY:
+            //func = &Writer::writeAsSDTUcabac; TODO
+            //binarizationParameter = binarzationParams.getCMaxDtu(); // TODO make binarizationParameter a vector
+            binarizationParameter = binarzationParams.getSplitUnitSize();
             break;
         default:
             GABAC_DIE("Unknown Binarization");
     }
 
-    if (contextSelectionId == ContextSelectionId::adaptive_coding_order_0) {
+    uint8_t codingOrder = supportVals.getCodingOrder();
+    if (codingOrder == 0) {
         while (r.isValid()) {
             if (maxSize <= bitstream.size()) {
                 break;
@@ -112,7 +161,7 @@ void encode_cabac(const BinarizationId& binarizationId, const std::vector<unsign
             (writer.*func)(r.get(), binarizationParameter, 0);
             r.inc();
         }
-    } else if (contextSelectionId == ContextSelectionId::adaptive_coding_order_1) {
+    } else if (codingOrder == 1) {
         unsigned int previousSymbol = 0;
         while (r.isValid()) {
             if (maxSize <= bitstream.size()) {
@@ -131,7 +180,7 @@ void encode_cabac(const BinarizationId& binarizationId, const std::vector<unsign
             }
             r.inc();
         }
-    } else if (contextSelectionId == ContextSelectionId::adaptive_coding_order_2) {
+    } else if (codingOrder == 2) {
         unsigned int previousSymbol = 0;
         unsigned int previousPreviousSymbol = 0;
         while (r.isValid()) {
