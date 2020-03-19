ee556a8fc1c22b6bab3d91015b2338e6c120f71f
Junaid Ahmad <junaid@jnomics.com>
2020-03-19 16:22:49 +0000

    (gabac) tweaks related to context model, context table and selector.

--- src/genie/entropy/gabac/context-tables.cc
+++ src/genie/entropy/gabac/context-tables.cc
@@ -12,41 +12,8 @@ namespace entropy {
 namespace gabac {
 namespace contexttables {
 
-std::vector<ContextModel> buildContextTable() {
-    std::vector<ContextModel> contextModels;
-
-    for (const auto& contextSet : INIT_TRUNCATED_UNARY_CTX) {
-        for (const auto j : contextSet) {
-            ContextModel contextModel(j);
-            contextModels.push_back(contextModel);
-        }
-    }
-
-    for (const auto& contextSet : INIT_EXPONENTIAL_GOLOMB_CTX) {
-        for (const auto j : contextSet) {
-            ContextModel contextModel(j);
-            contextModels.push_back(contextModel);
-        }
-    }
-
-    for (const auto& contextSet : INIT_BINARY_CTX) {
-        for (const auto j : contextSet) {
-            ContextModel contextModel(j);
-            contextModels.push_back(contextModel);
-        }
-    }
-
-    std::cout << "Size of contextModels: " << contextModels.size() << std::endl;
-    return contextModels;
-}
-
-
 std::vector<ContextModel> buildContextTable(unsigned int numContexts) {
-    std::vector<ContextModel> contextModels;
-    
-    // TODO 
-    
-    return contextModels;
+    return std::vector<ContextModel>(numContexts);
 }
 
 }  // namespace contexttables
--- src/genie/entropy/gabac/context-tables.h
+++ src/genie/entropy/gabac/context-tables.h
@@ -16,32 +16,6 @@ namespace entropy {
 namespace gabac {
 namespace contexttables {
 
-const int CONTEXT_SET_LENGTH = 32;
-
-// These variables point to the location of the first context set for
-// each type of binarization.
-//
-// Note: TEG/STEG are combinations of these "rudimentary" binarizations
-//       (i.e., BI, TU and EG) and do not use dedicated context sets but
-//       instead use a context set for each "sub"binarization.
-
-const int OFFSET_TRUNCATED_UNARY_0 = 0;
-
-const int OFFSET_EXPONENTIAL_GOLOMB_0 = OFFSET_TRUNCATED_UNARY_0 + (64 * CONTEXT_SET_LENGTH);
-
-const int OFFSET_BINARY_0 = OFFSET_EXPONENTIAL_GOLOMB_0 + (16 * CONTEXT_SET_LENGTH);
-
-// 64 rows, 32 columns, filled with 64
-const std::vector<std::vector<unsigned char>> INIT_TRUNCATED_UNARY_CTX(64, std::vector<unsigned char>(CONTEXT_SET_LENGTH, 64));
-
-// 16 rows, 32 columns, filled with 64
-const std::vector<std::vector<unsigned char>> INIT_EXPONENTIAL_GOLOMB_CTX(16, std::vector<unsigned char>(CONTEXT_SET_LENGTH, 64));
-
-// 16 rows, 32 columns, filled with 64
-const std::vector<std::vector<unsigned char>> INIT_BINARY_CTX(16, std::vector<unsigned char>(CONTEXT_SET_LENGTH, 64));
-
-std::vector<ContextModel> buildContextTable();
-
 std::vector<ContextModel> buildContextTable(unsigned int numContexts);
 
 }  // namespace contexttables
