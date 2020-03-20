ae8fd56ffa0c3921236c07ae06d45355819ac3fb
frankjannis <frank.jannis@gmail.com>
2020-03-20 12:39:57 +0100

    (encapsulator) FileHeader writeout

--- src/genie/format/mpegg_p1/file_header.cc
+++ src/genie/format/mpegg_p1/file_header.cc
@@ -8,8 +8,18 @@ FileHeader::FileHeader(std::vector<std::string>* const x_compatible_brand) {
     compatible_brand = std::move(*x_compatible_brand);
 }
 
-void FileHeader::writeToFile(genie::util::BitWriter&) const {
-    // TODO
+void FileHeader::writeToFile(genie::util::BitWriter& bitWriter) const {
+    bitWriter.write("flhd");
+
+    uint64_t length = 12 + 10 + compatible_brand.size() * 4;
+    bitWriter.write(length, 64);
+
+    bitWriter.write(major_brand);
+    bitWriter.write(minor_brand);
+    for (auto const& it: compatible_brand) {
+        bitWriter.write(it);
+    }
+    bitWriter.flush();
 }
 
 }  // namespace mpegg_p1
