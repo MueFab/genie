80f1ee7242a28d05293b6a61c0cd4b87bdaa81b7
Fabian Müntefering <fabian.muentefering@mail.de>
2020-04-09 02:52:00 +0200

    Spring output stage

--- src/genie/read/spring/spring-encoder.cc
+++ src/genie/read/spring/spring-encoder.cc
@@ -5,6 +5,7 @@
  */
 
 #include "spring-encoder.h"
+#include "reorder-compress-quality-id.h"
 
 namespace genie {
 namespace read {
@@ -16,6 +17,8 @@ void SpringEncoder::flowIn(core::record::Chunk &&t, size_t id) {
 
 void SpringEncoder::dryIn() {
     preprocessor.finish();
+
+    util::FastqStats stats;
 
     std::cout << "Reordering ...\n";
     auto reorder_start = std::chrono::steady_clock::now();
@@ -32,6 +35,18 @@ void SpringEncoder::dryIn() {
     std::cout << "Encoding done!\n";
     std::cout << "Time for this step: "
               << std::chrono::duration_cast<std::chrono::seconds>(encoder_end - encoder_start).count() << " s\n";
+
+    if (preprocessor.cp.preserve_quality || preprocessor.cp.preserve_id) {
+        std::cout << "Reordering and compressing quality and/or ids ...\n";
+        auto rcqi_start = std::chrono::steady_clock::now();
+        reorder_compress_quality_id(preprocessor.temp_dir, preprocessor.cp, &stats);
+        auto rcqi_end = std::chrono::steady_clock::now();
+        std::cout << "Reordering and compressing quality and/or ids done!\n";
+        std::cout << "Time for this step: "
+                  <<
+                  std::chrono::duration_cast<std::chrono::seconds>(rcqi_end -
+                                                                   rcqi_start).count() << " s\n";
+    }
 
 
     dryOut();
