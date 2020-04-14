533acc4547d82af8f1ad4551aeecb93cff6d5cf6
Junaid Ahmad <junaid@jnomics.com>
2020-04-14 14:36:22 +0100

    (gabac) further tweaks to merge coding

--- src/genie/entropy/gabac/merge-subseq-transform.cc
+++ src/genie/entropy/gabac/merge-subseq-transform.cc
@@ -17,9 +17,8 @@ namespace genie {
 namespace entropy {
 namespace gabac {
 
-void transformMergeCoding(const paramcabac::Subsequence& subseqCfg,
-                          util::DataBlock *symbols, std::vector<util::DataBlock *> &transformedSubseqs) {
-    assert(symbols != nullptr);
+void transformMergeCoding(const paramcabac::Subsequence& subseqCfg, std::vector<util::DataBlock> *const transformedSubseqs) {
+    assert(transformedSubseqs != nullptr);
     const paramcabac::TransformedParameters& trnsfSubseqParams = subseqCfg.getTransformParameters();
     const uint16_t subseqCount = trnsfSubseqParams.getParam();
     const std::vector<uint8_t> subseqShiftSizes = trnsfSubseqParams.getMergeCodingShiftSize();
@@ -30,52 +29,58 @@ void transformMergeCoding(const paramcabac::Subsequence& subseqCfg,
     }
 
     // Prepare internal and output data structures
-    transformedSubseqs.resize(subseqCount);
+    util::DataBlock symbols(0, 1);
+    symbols.swap(&(*transformedSubseqs)[0]);
+    transformedSubseqs->resize(subseqCount);
 
-    const uint64_t symbolsCount = symbols->size();
+    const uint64_t symbolsCount = symbols.size();
     std::vector<uint64_t> trnsSubseqMasks(0, subseqCount);
     for (uint64_t ts = 0; ts < subseqCount; ts++) {
         const uint8_t codingSubsymSize = trnsfCfgs[ts].getSupportValues().getCodingSubsymSize();
         trnsSubseqMasks[ts] = (1u<<codingSubsymSize)-1;
-        transformedSubseqs[ts]->setWordSize(paramcabac::StateVars::getLgWordSize(trnsfCfgs[ts].getSupportValues().getOutputSymbolSize()));
-        transformedSubseqs[ts]->resize(symbolsCount);
+        (*transformedSubseqs)[ts].setWordSize(paramcabac::StateVars::getLgWordSize(trnsfCfgs[ts].getSupportValues().getOutputSymbolSize()));
+        (*transformedSubseqs)[ts].resize(symbolsCount);
     }
 
+    // split
     for (uint64_t i = 0; i < symbolsCount; i++) {
-        uint64_t symbolValue = symbols->get(i);
+        uint64_t symbolValue = symbols.get(i);
         for (uint64_t ts = 0; ts < subseqCount; ts++) {
-            transformedSubseqs[ts]->set(i, (symbolValue>>subseqShiftSizes[ts]) & trnsSubseqMasks[ts]);
+            (*transformedSubseqs)[ts].set(i, (symbolValue>>subseqShiftSizes[ts]) & trnsSubseqMasks[ts]);
         }
     }
 
-    //symbols.clear(); FIXME needed ?
+    symbols.clear();
 }
 
-void inverseTransformMergeCoding(const paramcabac::Subsequence& subseqCfg,
-                                 util::DataBlock *symbols, std::vector<util::DataBlock *> &transformedSubseqs) {
+void inverseTransformMergeCoding(const paramcabac::Subsequence& subseqCfg, std::vector<util::DataBlock> *const transformedSubseqs) {
     const paramcabac::TransformedParameters& trnsfSubseqParams = subseqCfg.getTransformParameters();
     const uint16_t subseqCount = trnsfSubseqParams.getParam();
     const std::vector<uint8_t> subseqShiftSizes = trnsfSubseqParams.getMergeCodingShiftSize();
 
-    if (subseqCount <= 0 || subseqCount != transformedSubseqs.size()) {
+    if (subseqCount <= 0 || subseqCount != (*transformedSubseqs).size()) {
         GABAC_DIE("invalid subseq_count for merge coding");
     }
 
     // Prepare the output data structure
-    const uint64_t symbolsCount = symbols->size();
-    symbols->resize(symbolsCount);
-    symbols->setWordSize(paramcabac::StateVars::getLgWordSize(subseqShiftSizes[subseqCount-1] +
+    util::DataBlock symbols(0, 1);
+    const uint64_t symbolsCount = (*transformedSubseqs)[0].size();
+    symbols.resize(symbolsCount);
+    symbols.setWordSize(paramcabac::StateVars::getLgWordSize(subseqShiftSizes[subseqCount-1] +
                                                               subseqCfg.getTransformSubseqCfg(subseqCount-1).getSupportValues().getOutputSymbolSize()));
 
     for (uint64_t i = 0; i < symbolsCount; i++) {
         uint64_t symbolValue = 0;
         for (uint64_t ts = 0; ts < subseqCount; ts++) {
-            symbolValue = (symbolValue << subseqShiftSizes[ts]) | transformedSubseqs[ts]->get(i);
+            symbolValue = (symbolValue << subseqShiftSizes[ts]) | (*transformedSubseqs)[ts].get(i);
         }
-        symbols->set(i, symbolValue);
+        symbols.set(i, symbolValue);
     }
 
-    //symbols.clear(); FIXME needed ?
+    (*transformedSubseqs).resize(1);
+    (*transformedSubseqs)[0].clear();
+
+    symbols.swap(&(*transformedSubseqs)[0]);
 }
 }  // namespace gabac
 }  // namespace entropy
