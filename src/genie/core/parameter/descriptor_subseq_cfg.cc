6176f4b786a7360498e2e33e8f3e65661fb68fa2
Fabian Müntefering <fabian.muentefering@mail.de>
2020-04-26 21:36:05 +0200

    Renaming of DescriptorBox to DescriptorSubseqCfg

--- src/genie/core/parameter/descriptor_subseq_cfg.cc
+++ src/genie/core/parameter/descriptor_subseq_cfg.cc
@@ -4,7 +4,7 @@
  * https://github.com/mitogen/genie for more details.
  */
 
-#include "descriptor_box.h"
+#include "descriptor_subseq_cfg.h"
 #include <genie/util/bitwriter.h>
 #include <genie/util/exceptions.h>
 #include <genie/util/make-unique.h>
@@ -18,23 +18,23 @@ namespace parameter {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-DescriptorBox::DescriptorBox() : class_specific_dec_cfg_flag(false), descriptor_configurations(0) {
+DescriptorSubseqCfg::DescriptorSubseqCfg() : class_specific_dec_cfg_flag(false), descriptor_configurations(0) {
     descriptor_configurations.push_back(util::make_unique<desc_pres::DescriptorPresent>());
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-DescriptorBox::DescriptorBox(const DescriptorBox& box) : class_specific_dec_cfg_flag(false) { *this = box; }
+DescriptorSubseqCfg::DescriptorSubseqCfg(const DescriptorSubseqCfg& box) : class_specific_dec_cfg_flag(false) { *this = box; }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-DescriptorBox::DescriptorBox(DescriptorBox&& box) noexcept : class_specific_dec_cfg_flag(false) {
+DescriptorSubseqCfg::DescriptorSubseqCfg(DescriptorSubseqCfg&& box) noexcept : class_specific_dec_cfg_flag(false) {
     *this = std::move(box);
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-DescriptorBox& DescriptorBox::operator=(const DescriptorBox& box) {
+DescriptorSubseqCfg& DescriptorSubseqCfg::operator=(const DescriptorSubseqCfg& box) {
     if (this == &box) {
         return *this;
     }
@@ -48,7 +48,7 @@ DescriptorBox& DescriptorBox::operator=(const DescriptorBox& box) {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-DescriptorBox& DescriptorBox::operator=(DescriptorBox&& box) noexcept {
+DescriptorSubseqCfg& DescriptorSubseqCfg::operator=(DescriptorSubseqCfg&& box) noexcept {
     class_specific_dec_cfg_flag = box.class_specific_dec_cfg_flag;
     descriptor_configurations = std::move(box.descriptor_configurations);
     return *this;
@@ -56,7 +56,7 @@ DescriptorBox& DescriptorBox::operator=(DescriptorBox&& box) noexcept {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-DescriptorBox::DescriptorBox(size_t num_classes, GenDesc desc, util::BitReader& reader) {
+DescriptorSubseqCfg::DescriptorSubseqCfg(size_t num_classes, GenDesc desc, util::BitReader& reader) {
     class_specific_dec_cfg_flag = reader.read<bool>(1);
     if (class_specific_dec_cfg_flag == 0) {
         descriptor_configurations.emplace_back(Descriptor::factory(desc, reader));
@@ -69,7 +69,7 @@ DescriptorBox::DescriptorBox(size_t num_classes, GenDesc desc, util::BitReader& 
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-void DescriptorBox::setClassSpecific(uint8_t index, std::unique_ptr<Descriptor> conf) {
+void DescriptorSubseqCfg::setClassSpecific(uint8_t index, std::unique_ptr<Descriptor> conf) {
     if (index > descriptor_configurations.size()) {
         UTILS_THROW_RUNTIME_EXCEPTION("Config index out of bounds.");
     }
@@ -78,7 +78,7 @@ void DescriptorBox::setClassSpecific(uint8_t index, std::unique_ptr<Descriptor> 
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-void DescriptorBox::set(std::unique_ptr<Descriptor> conf) {
+void DescriptorSubseqCfg::set(std::unique_ptr<Descriptor> conf) {
     if (class_specific_dec_cfg_flag) {
         UTILS_THROW_RUNTIME_EXCEPTION("Invalid setConfig() for non class-specific descriptor config.");
     }
@@ -87,15 +87,15 @@ void DescriptorBox::set(std::unique_ptr<Descriptor> conf) {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-const Descriptor& DescriptorBox::get() const { return *descriptor_configurations.front(); }
+const Descriptor& DescriptorSubseqCfg::get() const { return *descriptor_configurations.front(); }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-const Descriptor& DescriptorBox::getClassSpecific(uint8_t index) const { return *descriptor_configurations[index]; }
+const Descriptor& DescriptorSubseqCfg::getClassSpecific(uint8_t index) const { return *descriptor_configurations[index]; }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-void DescriptorBox::enableClassSpecific(uint8_t numClasses) {
+void DescriptorSubseqCfg::enableClassSpecific(uint8_t numClasses) {
     if (class_specific_dec_cfg_flag) {
         return;
     }
@@ -108,11 +108,11 @@ void DescriptorBox::enableClassSpecific(uint8_t numClasses) {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-bool DescriptorBox::isClassSpecific() const { return class_specific_dec_cfg_flag; }
+bool DescriptorSubseqCfg::isClassSpecific() const { return class_specific_dec_cfg_flag; }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-void DescriptorBox::write(util::BitWriter& writer) const {
+void DescriptorSubseqCfg::write(util::BitWriter& writer) const {
     writer.write(static_cast<uint8_t>(class_specific_dec_cfg_flag), 1);
     for (auto& i : descriptor_configurations) {
         i->write(writer);
