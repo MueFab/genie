06464f8a900a6f33368082b02322a874e6cd0dd6
Junaid Ahmad <junaid@jnomics.com>
2020-05-05 17:00:04 +0100

    (gabac, paramcabac) Refactorings to use short binarization names

--- src/genie/entropy/paramcabac/binarization_parameters.h
+++ src/genie/entropy/paramcabac/binarization_parameters.h
@@ -27,16 +27,16 @@ namespace paramcabac {
 class BinarizationParameters {
    public:
     enum class BinarizationId : uint8_t {
-        BINARY_CODING = 0,
-        TRUNCATED_UNARY = 1,
-        EXPONENTIAL_GOLOMB = 2,
-        SIGNED_EXPONENTIAL_GOMB = 3,
-        TRUNCATED_EXPONENTIAL_GOLOMB = 4,
-        SIGNED_TRUNCATED_EXPONENTIAL_GOLOMB = 5,
-        SPLIT_UNITWISE_TRUNCATED_UNARY = 6,
-        SIGNED_SPLIT_UNITWISE_TRUNCATED_UNARY = 7,
-        DOUBLE_TRUNCATED_UNARY = 8,
-        SIGNED_DOUBLE_TRUNCATED_UNARY = 9,
+        BI = 0,
+        TU = 1,
+        EG = 2,
+        SEG = 3,
+        TEG = 4,
+        STEG = 5,
+        SUTU = 6,
+        SSUTU = 7,
+        DTU = 8,
+        SDTU = 9,
     };
 
     BinarizationParameters();
@@ -65,7 +65,7 @@ class BinarizationParameters {
     uint8_t cmax_dtu;         //!< : 8; Line 7
     uint8_t split_unit_size;  //!< : 4; Line 10
     
-    static uint8_t numParams[unsigned(BinarizationId::SIGNED_DOUBLE_TRUNCATED_UNARY) + 1u];
+    static uint8_t numParams[unsigned(BinarizationId::SDTU) + 1u];
 
 };
 
