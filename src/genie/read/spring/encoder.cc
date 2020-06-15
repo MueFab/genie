7815e2a231aa59659135da318ebddd6e1072d2eb
Fabian Müntefering <fabian.muentefering@mail.de>
2020-06-15 19:37:37 +0200

    Fix openMP issue

--- src/genie/read/spring/encoder.cc
+++ src/genie/read/spring/encoder.cc
@@ -103,11 +103,16 @@ class SpringSource : public util::OriginalSource, public util::Source<core::Acce
 void Encoder::flushIn(size_t& pos) {
     preprocessor.finish(pos);
 
+
     std::vector<core::parameter::ParameterSet> params;
+    auto loc_cp = preprocessor.cp;
+#ifndef GENIE_USE_OPENMP
+    loc_cp.num_thr = 1;
+#endif
 
     std::cout << "Reordering ...\n";
     auto reorder_start = std::chrono::steady_clock::now();
-    call_reorder(preprocessor.temp_dir, preprocessor.cp);
+    call_reorder(preprocessor.temp_dir, loc_cp);
     auto reorder_end = std::chrono::steady_clock::now();
     std::cout << "Reordering done!\n";
     std::cout << "Time for this step: "
@@ -115,7 +120,7 @@ void Encoder::flushIn(size_t& pos) {
 
     std::cout << "Encoding ...\n";
     auto encoder_start = std::chrono::steady_clock::now();
-    call_encoder(preprocessor.temp_dir, preprocessor.cp);
+    call_encoder(preprocessor.temp_dir, loc_cp);
     auto encoder_end = std::chrono::steady_clock::now();
     std::cout << "Encoding done!\n";
     std::cout << "Time for this step: "
@@ -123,7 +128,7 @@ void Encoder::flushIn(size_t& pos) {
 
     std::cout << "Generating read streams ...\n";
     auto grs_start = std::chrono::steady_clock::now();
-    generate_read_streams(preprocessor.temp_dir, preprocessor.cp, entropycoder, params);
+    generate_read_streams(preprocessor.temp_dir, loc_cp, entropycoder, params);
     auto grs_end = std::chrono::steady_clock::now();
     std::cout << "Generating read streams done!\n";
     std::cout << "Time for this step: " << std::chrono::duration_cast<std::chrono::seconds>(grs_end - grs_start).count()
@@ -132,7 +137,7 @@ void Encoder::flushIn(size_t& pos) {
     if (preprocessor.cp.preserve_quality || preprocessor.cp.preserve_id) {
         std::cout << "Reordering and compressing quality and/or ids ...\n";
         auto rcqi_start = std::chrono::steady_clock::now();
-        reorder_compress_quality_id(preprocessor.temp_dir, preprocessor.cp, qvcoder, namecoder, entropycoder, params);
+        reorder_compress_quality_id(preprocessor.temp_dir, loc_cp, qvcoder, namecoder, entropycoder, params);
         auto rcqi_end = std::chrono::steady_clock::now();
         std::cout << "Reordering and compressing quality and/or ids done!\n";
         std::cout << "Time for this step: "
