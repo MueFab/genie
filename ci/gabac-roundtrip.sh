d8da0762354875b13198d0b896ce180bd96e5847
Jan Voges <voges@tnt.uni-hannover.de>
2020-08-17 15:38:08 +0200

    Add GABAC roundtrips to CI

--- ci/gabac-roundtrip.sh
+++ ci/gabac-roundtrip.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 git_root_dir="$(git rev-parse --show-toplevel)"
-$git_root_dir/cmake-build-release/gabac-app -i $git_root_dir/data/gabac/test-files/genNeg.pos -o genNeg.out -t encode -d 0 -s 1
-$git_root_dir/cmake-build-release/gabac-app -i genNeg.out -o genNeg.decoded -t decode -d 0 -s 1
+${git_root_dir}/cmake-build-release/bin/gabac-app -i $git_root_dir/data/gabac/test-files/genNeg.pos -o genNeg.out -t encode -d 0 -s 1
+${git_root_dir}/cmake-build-release/bin/gabac-app -i genNeg.out -o genNeg.decoded -t decode -d 0 -s 1
 diff --report-identical-files $git_root_dir/data/gabac/test-files/genNeg.pos genNeg.decoded
+
