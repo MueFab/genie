bd68fc2d84596a23cf795f0cd17e8339cd5d66b7
Junaid Ahmad <junaid@jnomics.com>
2020-09-07 16:44:20 +0100

    (dependency) gabac: make mismatch-decoder to use symbol's decoder

--- src/genie/entropy/gabac/mismatch-decoder.cc
+++ src/genie/entropy/gabac/mismatch-decoder.cc
@@ -8,6 +8,7 @@
 
 #include "mismatch-decoder.h"
 #include <genie/util/make-unique.h>
+#include "stream-handler.h"
 
 // ---------------------------------------------------------------------------------------------------------------------
 
@@ -17,18 +18,84 @@ namespace gabac {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-MismatchDecoder::MismatchDecoder(util::DataBlock &&d, const EncodingConfiguration &c) : data(std::move(d)), enConf(c) {
+MismatchDecoder::MismatchDecoder(util::DataBlock &&d, const EncodingConfiguration &c)
+    : data(std::move(d)), enConf(c), numSubseqSymbolsTotal(0), numSubseqSymbolsDecoded(0), numTrnsfSubseqs(0) {
+    const paramcabac::Subsequence &subseqCfg = enConf.getSubseqConfig();
+    uint64_t subseqPayloadSizeUsed = 0;
 
+    gabac::IBufferStream inputStream(&data, 0);
+    const uint64_t subseqPayloadSize = gabac::StreamHandler::readStreamSize(inputStream);
+    if (subseqPayloadSize <= 0) return; // TODO throw exception?
+
+
+    // read number of symbols in descriptor subsequence
+    if (subseqCfg.getTokentypeFlag()) {
+        subseqPayloadSizeUsed += gabac::StreamHandler::readU7(inputStream, numSubseqSymbolsTotal);
+    } else {
+        subseqPayloadSizeUsed += gabac::StreamHandler::readUInt(inputStream, numSubseqSymbolsTotal, 4);
+    }
+
+    if (numSubseqSymbolsTotal > 0) {
+        if (inputStream.peek() != EOF) {
+            // Set up for the inverse sequence transformation
+            numTrnsfSubseqs = subseqCfg.getNumTransformSubseqCfgs();
+            if (numTrnsfSubseqs > 1) return; // TODO allow symbols decoding with only 1 transformed subseqs for now.
+
+            // Loop through the transformed sequences
+            std::vector<util::DataBlock> transformedSubseqs(numTrnsfSubseqs);
+            for (size_t i = 0; i < numTrnsfSubseqs; i++) {
+                util::DataBlock currTrnsfSubseqData;
+                uint64_t currNumtrnsfSymbols = 0;
+                uint64_t trnsfSubseqPayloadSizeRemain = 0;
+
+                if (i < (numTrnsfSubseqs - 1)) {
+                    subseqPayloadSizeUsed +=
+                        gabac::StreamHandler::readUInt(inputStream, trnsfSubseqPayloadSizeRemain, 4);
+                } else {
+                    trnsfSubseqPayloadSizeRemain = subseqPayloadSize - subseqPayloadSizeUsed;
+                }
+
+                if (trnsfSubseqPayloadSizeRemain > 0) {
+                    if (numTrnsfSubseqs > 1) {
+                        subseqPayloadSizeUsed += gabac::StreamHandler::readUInt(inputStream,
+                                                                                currNumtrnsfSymbols,
+                                                                                4);
+                        trnsfSubseqPayloadSizeRemain -= 4;
+                    } else {
+                        currNumtrnsfSymbols = numSubseqSymbolsTotal;
+                    }
+
+                    if (currNumtrnsfSymbols > 0) {
+                        subseqPayloadSizeUsed += gabac::StreamHandler::readBytes(inputStream,
+                                                                                 trnsfSubseqPayloadSizeRemain,
+                                                                                 &currTrnsfSubseqData);
+                    }
+
+                    trnsfSubseqDecoder.emplace_back(&currTrnsfSubseqData,
+                                                    subseqCfg.getTransformSubseqCfg(i),
+                                                    currNumtrnsfSymbols);
+                }
+            }
+        }
+    }
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-uint64_t MismatchDecoder::decodeMismatch(uint64_t ref) { return 0; }
+uint64_t MismatchDecoder::decodeMismatch(uint64_t ref) {
+    std::vector<uint64_t> decodedTrnsfSymbols;
+    for (size_t i = 0; i < numTrnsfSubseqs; i++) {
+        decodedTrnsfSymbols[i] = trnsfSubseqDecoder[i].decodeNextSymbol(&ref);
+    }
+
+    numSubseqSymbolsDecoded++;
+    return decodedTrnsfSymbols[0]; // TODO implement inverse subseq transformations (based on buffer etc.)
+}
 
 // ---------------------------------------------------------------------------------------------------------------------
 
 bool MismatchDecoder::dataLeft() const {
-    return position < data.size();
+    return numSubseqSymbolsDecoded < numSubseqSymbolsTotal;
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -36,7 +103,6 @@ bool MismatchDecoder::dataLeft() const {
 std::unique_ptr<core::MismatchDecoder> MismatchDecoder::copy() const {
     util::DataBlock d = data;
     auto ret = util::make_unique<MismatchDecoder>(std::move(d), enConf); // FIXME it will start cabac decoding process
-    ret->position = position;
     return ret;
 }
 
