4f112ca8560c3b19d8b5b05bc6304932ad327967
Junaid Ahmad <junaid@jnomics.com>
2020-10-07 13:49:19 +0100

    (dependency) More follow up tweaks.

--- src/genie/core/access-unit.cc
+++ src/genie/core/access-unit.cc
@@ -23,7 +23,7 @@ AccessUnit::Subsequence &AccessUnit::Subsequence::operator=(const Subsequence &s
     id = sub.id;
     numSymbols = sub.numSymbols;
     dependency = sub.dependency;
-    if (sub.mmDecoder != nullptr) mmDecoder = sub.mmDecoder->copy(); // FIXME it will start cabac decoding process
+    if (sub.mmDecoder != nullptr) mmDecoder = sub.mmDecoder->copy();
     return *this;
 }
 
@@ -67,7 +67,10 @@ util::DataBlock* AccessUnit::Subsequence::getDependency() {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-void AccessUnit::Subsequence::attachMismatchDecoder(std::unique_ptr<MismatchDecoder> mm) { mmDecoder = std::move(mm); }
+core::AccessUnit::Subsequence AccessUnit::Subsequence::attachMismatchDecoder(std::unique_ptr<MismatchDecoder> mm) {
+    mmDecoder = std::move(mm);
+    return *this;
+}
 
 // ---------------------------------------------------------------------------------------------------------------------
 
