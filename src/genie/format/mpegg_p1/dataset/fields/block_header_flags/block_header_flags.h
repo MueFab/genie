7784b5626c6c662833af4041cf1c1a742ed25a76
Abderraouf Ayadi <abderraouf7@hotmail.com>
2020-12-21 02:07:09 +0100

    add method getClassInfos()

--- src/genie/format/mpegg_p1/dataset/fields/block_header_flags/block_header_flags.h
+++ src/genie/format/mpegg_p1/dataset/fields/block_header_flags/block_header_flags.h
@@ -18,31 +18,64 @@ class BlockConfig {
 
     BlockConfig(bool _block_header_flag, bool _mit_flag);
 
-    /*
+    /**
      *
+     * @return
      */
     bool getBlockHeaderFlag() const;
 
-    /*
+    /**
      *
+     * @return
      */
     bool getMITFlag() const;
 
-    /*
+    /**
      *
+     * @return
      */
     uint8_t getNumClasses() const;
 
+    /**
+     *
+     * @return
+     */
+    const std::vector<ClassInfo>& getClassInfos() const;
+
+    /**
+     *
+     * @param _cls_info
+     */
     void addClassInfo(ClassInfo&& _cls_info);
 
+    /**
+     *
+     * @param _cls_infos
+     */
     void addClassInfos(std::vector<ClassInfo>& _cls_infos);
 
+    /**
+     *
+     * @param _cls_infos
+     */
     void setClassInfos(std::vector<ClassInfo>&& _cls_infos);
 
+    /**
+     *
+     * @return
+     */
     virtual uint64_t getBitLength() const;
 
+    /**
+     *
+     * @param bit_writer
+     */
     virtual void write(genie::util::BitWriter& bit_writer) const;
 
+    /**
+     *
+     * @param bit_writer
+     */
     void writeClassInfos(genie::util::BitWriter& bit_writer) const;
 
    protected:
