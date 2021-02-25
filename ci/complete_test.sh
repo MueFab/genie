af66a4ebad06f323617f1db8c0fb0c18a70f0535
Fabian Müntefering <fabian.muentefering@mail.de>
2021-02-25 20:41:37 +0100

    Local CI scripts
    Local CI scripts
    
    Local CI scripts
    
    Local CI scripts
    
    Local CI scripts
    
    Local CI scripts

---
+++ ci/complete_test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Do a full test of genie
+set -e
+
+############ Check prerequisites ###############
+
+if ! command -v git &> /dev/null
+then
+    echo "Git could not be found. Please install git (debian: sudo apt-get install git)."
+    exit 1
+fi
+
+if ! command -v cpplint &> /dev/null
+then
+    echo "Cpplint could not be found. Please install cpplint (debian: sudo pip3 install cpplint)."
+    exit 1
+fi
+
+if ! command -v cppcheck &> /dev/null
+then
+    echo "Cppcheck could not be found. Please install cppcheck (debian: sudo apt-get install cppcheck)."
+    exit 1
+fi
+
+if ! command -v cmake &> /dev/null
+then
+    echo "Cmake could not be found. Please install cmake (debian: sudo apt-get install cmake)."
+    exit 1
+fi
+
+if ! command -v make &> /dev/null
+then
+    echo "Make could not be found. Please install make."
+    exit 1
+fi
+
+############ Run ###############
+
+git_root_dir="$(git rev-parse --show-toplevel)"
+
+cd $git_root_dir/ci
+./src_test.sh || { echo 'Src test failed!' ; exit 1; }
+./build.sh debug || { echo 'Build debug failed!' ; exit 1; }
+./unit_test.sh || { echo 'Unit tests failed!' ; exit 1; }
+./memory_test.sh || { echo 'Memory tests failed!' ; exit 1; }
+./roundtrip_test.sh || { echo 'Roundtrip tests failed!' ; exit 1; }
+./conformance_test.sh || { echo 'Conformance tests failed!' ; exit 1; }
+
---
+++ ci/conformance_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Do a round trip with the MPEG-G reference decoder
+set -e
+
+############ Check prerequisites ###############
+
+if ! command -v git &> /dev/null
+then
+    echo "Git could not be found. Please install git (debian: sudo apt-get install git)."
+    exit 1
+fi
+
+if [[ "$OSTYPE" == "win32" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+    fileExt=".exe"
+else
+    fileExt=""
+fi
+
+
+git_root_dir="$(git rev-parse --show-toplevel)"
+
+
+############ Run ###############
