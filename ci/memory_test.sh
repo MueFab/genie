513b6e1af199e0534a6af73a8016071a64039a64
Fabian Müntefering <fabian.muentefering@mail.de>
2021-02-25 20:41:37 +0100

    Local CI scripts
    Local CI scripts
    
    Local CI scripts
    
    Local CI scripts
    
    Local CI scripts
    
    Local CI scripts

---
+++ ci/memory_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Do roundtrips with very small files (performance reasons) while checking for memory issues
+set -e
+
+############ Check prerequisites ###############
+
+if ! command -v git &> /dev/null
+then
+    echo "Git could not be found. Please install git (debian: sudo apt-get install git)."
+    exit 1
+fi
+
+if [[ "$OSTYPE" == "win32" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+    fileExt=".exe"
+else
+    fileExt=""
+fi
+
+
+git_root_dir="$(git rev-parse --show-toplevel)"
+
+
+############ Run ###############
