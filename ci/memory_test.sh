aeb31cff90461886644aca67c9cdfbb9dc1479c8
Fabian Müntefering <fabian.muentefering@mail.de>
2021-02-28 02:31:00 +0100

    Memory check

--- ci/memory_test.sh
+++ ci/memory_test.sh
@@ -16,11 +16,28 @@ else
     fileExt=""
 fi
 
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+    echo "Skipping memcheck for non-linux OS"
+    return 0
+fi
+
 
 git_root_dir="$(git rev-parse --show-toplevel)"
 
 
 ############ Run ###############
 
-cd $git_root_dir/cmake-build-debug/bin
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --error-exitcode=1 ./genie$fileExt run --help || { echo 'Memcheck failed!' ; exit 1; }
+echo "Single-end fastq"
+wget --no-check-certificate https://cloud.fab-mue.de/s/bC6ZQrCxMkW2P4y/download -O /tmp/ERR174310_tiny_1.fastq.gz || { echo 'Could not download single end fastq!' ; exit 1; }
+gzip -df /tmp/ERR174310_tiny_1.fastq.gz
+valgrind --suppressions=$git_root_dir/ci/omp.supp --gen-suppressions=all --leak-check=full --track-origins=yes --error-exitcode=1 $git_root_dir/cmake-build-release/bin/genie$fileExt run -i /tmp/ERR174310_tiny_1.fastq -o /tmp/single_fastq.mgb -f || { echo 'Memcheck for genie compression of single end fastq failed!' ; exit 1; }
+rm /tmp/single_fastq.mgb
+#rm /tmp/single_fastq.mgrec
+
+
+echo "Paired-end fastq"
+wget --no-check-certificate https://cloud.fab-mue.de/s/QMoZPiX47NwJqcP/download -O /tmp/ERR174310_tiny_2.fastq.gz || { echo 'Could not download paired end fastq!' ; exit 1; }
+gzip -df /tmp/ERR174310_tiny_2.fastq.gz
+valgrind --suppressions=$git_root_dir/ci/omp.supp --gen-suppressions=all --leak-check=full --track-origins=yes --error-exitcode=1 $git_root_dir/cmake-build-release/bin/genie$fileExt run -i /tmp/ERR174310_tiny_1.fastq --input-suppl-file /tmp/ERR174310_tiny_2.fastq -o /tmp/paired_fastq.mgb -f || { echo 'Memcheck for genie compression of paired end fastq failed!' ; exit 1; }
+rm /tmp/paired_fastq.mgb
+#rm /tmp/paired_fastq.mgrec
