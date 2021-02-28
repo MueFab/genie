be5dde72d0447588b1166a8e1cda1baec85c20d9
Fabian Müntefering <fabian.muentefering@mail.de>
2021-02-28 21:46:48 +0100

    Full fastq test

--- ci/memory_test.sh
+++ ci/memory_test.sh
@@ -27,122 +27,109 @@ git_root_dir="$(git rev-parse --show-toplevel)"
 
 ############ Run ###############
 
+compress_roundtrip () {
+
+if [[ "$2" == "" ]]; then
+    second_input_file=""
+    second_output_file=""
+else
+    second_input_file="--input-suppl-file $2"
+    second_output_file="--output-suppl-file $2"
+fi
+
+echo "Genie compress"
+valgrind --suppressions=$git_root_dir/ci/omp.supp \
+    --gen-suppressions=all \
+    --leak-check=full \
+    --track-origins=yes \
+    --error-exitcode=1 \
+    $git_root_dir/cmake-build-release/bin/genie$fileExt run \
+    -i $1 \
+    $second_input_file \
+    -o /tmp/output.mgb -f \
+    $3 \
+    || { echo "Genie compress ($1; $2; $3) failed!" ; exit 1; }
+
+echo "Genie decompress"
+valgrind --suppressions=$git_root_dir/ci/omp.supp \
+    --gen-suppressions=all \
+    --leak-check=full \
+    --track-origins=yes \
+    --error-exitcode=1 \
+    $git_root_dir/cmake-build-release/bin/genie$fileExt run \
+    -o /tmp/output_1.fastq \
+    --output-suppl-file output_2.fastq \
+    -i /tmp/output.mgb -f \
+    || { echo "Genie decompress ($1; $2; $3) failed!" ; exit 1; }
+rm /tmp/output_1.fastq
+rm -f /tmp/output_2.fastq
+rm /tmp/output.mgb
+}
+
+convert_roundtrip() {
+if [[ "$2" == "" ]]; then
+    second_input_file=""
+else
+    second_input_file="--input-suppl-file $2"
+fi
+
+echo "Genie convert"
+valgrind --suppressions=$git_root_dir/ci/omp.supp \
+    --gen-suppressions=all \
+    --leak-check=full \
+    --track-origins=yes \
+    --error-exitcode=1 \
+    $git_root_dir/cmake-build-release/bin/genie$fileExt run \
+    -o /tmp/output.mgrec \
+    -i $1 -f \
+    $second_input_file \
+    $3 \
+    || { echo "Genie convert ($1; $2; $3) failed!" ; exit 1; }
+
+echo "Genie convert back"
+valgrind --suppressions=$git_root_dir/ci/omp.supp \
+    --gen-suppressions=all \
+    --leak-check=full \
+    --track-origins=yes \
+    --error-exitcode=1 \
+    $git_root_dir/cmake-build-release/bin/genie$fileExt run \
+    -i /tmp/output.mgrec \
+    -o /tmp/output_1.fastq \
+    --output-suppl-file /tmp/output_2.fastq \
+    || { echo "Genie convert ($1; $2; $3) failed!" ; exit 1; }
+
+rm /tmp/output_1.fastq
+rm /tmp/output_2.fastq
+rm /tmp/output.mgrec
+}
+
 echo "*** Single-end fastq"
+# Get fastq file no 1
 wget --no-check-certificate \
-    https://cloud.fab-mue.de/s/bC6ZQrCxMkW2P4y/download \
+    https://cloud.fab-mue.de/s/bC6ZQrCxMkW2P4y/download  \
     -O /tmp/ERR174310_tiny_1.fastq.gz \
     || { echo 'Could not download single end fastq!' ; exit 1; }
 gzip -df /tmp/ERR174310_tiny_1.fastq.gz
 
-echo "** fastq -> mgb"
-valgrind --suppressions=$git_root_dir/ci/omp.supp \
-    --gen-suppressions=all \
-    --leak-check=full \
-    --track-origins=yes \
-    --error-exitcode=1 \
-    $git_root_dir/cmake-build-release/bin/genie$fileExt run \
-    -i /tmp/ERR174310_tiny_1.fastq \
-    -o /tmp/single_fastq.mgb -f \
-    || { echo 'Memcheck for genie compression of single end fastq failed!' ; exit 1; }
+compress_roundtrip "/tmp/ERR174310_tiny_1.fastq" "" "--low-latency --qv none --read-ids none"
+compress_roundtrip "/tmp/ERR174310_tiny_1.fastq" "" "--low-latency"
+compress_roundtrip "/tmp/ERR174310_tiny_1.fastq" "" "--qv none --read-ids none"
+compress_roundtrip "/tmp/ERR174310_tiny_1.fastq" "" ""
+convert_roundtrip "/tmp/ERR174310_tiny_1.fastq" "" ""
 
-echo "** mgb -> fastq"
-valgrind --suppressions=$git_root_dir/ci/omp.supp \
-    --gen-suppressions=all \
-    --leak-check=full \
-    --track-origins=yes \
-    --error-exitcode=1 \
-    $git_root_dir/cmake-build-release/bin/genie$fileExt run \
-    -o /tmp/single_fastq.fastq \
-    -i /tmp/single_fastq.mgb -f \
-    || { echo 'Memcheck for genie decompression of single end fastq failed!' ; exit 1; }
-rm /tmp/single_fastq.mgb
-rm /tmp/single_fastq.fastq
-
-echo "** fastq -> mgrec"
-valgrind --suppressions=$git_root_dir/ci/omp.supp \
-    --gen-suppressions=all \
-    --leak-check=full \
-    --track-origins=yes \
-    --error-exitcode=1 \
-    $git_root_dir/cmake-build-release/bin/genie$fileExt run \
-    -i /tmp/ERR174310_tiny_1.fastq \
-    -o /tmp/single_fastq.mgrec -f \
-    || { echo 'Memcheck for genie single end conversion from fastq to mgrec failed!' ; exit 1; }
-
-echo "** mgrec -> fastq"
-valgrind --suppressions=$git_root_dir/ci/omp.supp \
-    --gen-suppressions=all \
-    --leak-check=full \
-    --track-origins=yes \
-    --error-exitcode=1 \
-    $git_root_dir/cmake-build-release/bin/genie$fileExt run \
-    -o /tmp/single_fastq.fastq \
-    -i /tmp/single_fastq.mgrec -f \
-    || { echo 'Memcheck for genie single end conversion from mgrec to fastq failed!' ; exit 1; }
-rm /tmp/single_fastq.mgrec
-rm /tmp/single_fastq.fastq
-
-
-
-echo "Paired-end fastq"
+echo "*** Paired-end fastq"
+# Get fastq file no 2
 wget --no-check-certificate \
     https://cloud.fab-mue.de/s/QMoZPiX47NwJqcP/download \
     -O /tmp/ERR174310_tiny_2.fastq.gz \
     || { echo 'Could not download paired end fastq!' ; exit 1; }
 gzip -df /tmp/ERR174310_tiny_2.fastq.gz
 
-echo "** fastq -> mgb"
-valgrind --suppressions=$git_root_dir/ci/omp.supp \
-    --gen-suppressions=all \
-    --leak-check=full \
-    --track-origins=yes \
-    --error-exitcode=1 \
-    $git_root_dir/cmake-build-release/bin/genie$fileExt run \
-    -i /tmp/ERR174310_tiny_1.fastq \
-    --input-suppl-file /tmp/ERR174310_tiny_2.fastq \
-    -o /tmp/paired_fastq.mgb -f \
-    || { echo 'Memcheck for genie compression of paired end fastq failed!' ; exit 1; }
+compress_roundtrip "/tmp/ERR174310_tiny_1.fastq" "/tmp/ERR174310_tiny_2.fastq" "--low-latency --qv none --read-ids none"
+compress_roundtrip "/tmp/ERR174310_tiny_1.fastq" "/tmp/ERR174310_tiny_2.fastq" "--low-latency"
+compress_roundtrip "/tmp/ERR174310_tiny_1.fastq" "/tmp/ERR174310_tiny_2.fastq" "--qv none --read-ids none"
+compress_roundtrip "/tmp/ERR174310_tiny_1.fastq" "/tmp/ERR174310_tiny_2.fastq" ""
+convert_roundtrip "/tmp/ERR174310_tiny_1.fastq" "/tmp/ERR174310_tiny_2.fastq" ""
 
-echo "** mgb -> fastq"
-valgrind --suppressions=$git_root_dir/ci/omp.supp \
-    --gen-suppressions=all \
-    --leak-check=full \
-    --track-origins=yes \
-    --error-exitcode=1 \
-    $git_root_dir/cmake-build-release/bin/genie$fileExt run \
-    -o /tmp/paired_fastq_1.fastq \
-    --output-suppl-file /tmp/paired_fastq_2.fastq \
-    -i /tmp/paired_fastq.mgb -f \
-    || { echo 'Memcheck for genie decompression of paired end fastq failed!' ; exit 1; }
-rm /tmp/paired_fastq.mgb
-rm /tmp/paired_fastq_1.fastq
-rm /tmp/paired_fastq_2.fastq
-
-echo "** fastq -> mgrec"
-valgrind --suppressions=$git_root_dir/ci/omp.supp \
-    --gen-suppressions=all \
-    --leak-check=full \
-    --track-origins=yes \
-    --error-exitcode=1 \
-    $git_root_dir/cmake-build-release/bin/genie$fileExt run \
-    -i /tmp/ERR174310_tiny_1.fastq \
-    --input-suppl-file /tmp/ERR174310_tiny_2.fastq \
-    -o /tmp/paired_fastq.mgrec -f \
-    || { echo 'Memcheck for genie compression of paired end fastq failed!' ; exit 1; }
-
-echo "** mgrec -> fastq"
-valgrind --suppressions=$git_root_dir/ci/omp.supp \
-    --gen-suppressions=all \
-    --leak-check=full \
-    --track-origins=yes \
-    --error-exitcode=1 \
-    $git_root_dir/cmake-build-release/bin/genie$fileExt run \
-    -o /tmp/paired_fastq_1.fastq \
-    --output-suppl-file /tmp/paired_fastq_2.fastq \
-    -i /tmp/paired_fastq.mgrec -f \
-    || { echo 'Memcheck for genie decompression of paired end fastq failed!' ; exit 1; }
-rm /tmp/paired_fastq.mgrec
-rm /tmp/paired_fastq_1.fastq
-rm /tmp/paired_fastq_2.fastq
 rm /tmp/ERR174310_tiny_1.fastq
 rm /tmp/ERR174310_tiny_2.fastq
