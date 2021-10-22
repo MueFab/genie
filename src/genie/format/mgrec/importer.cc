5cda9894baa4cf58d83b35687f6457d0c2a293c1
Fabian Müntefering <muenteferi@tnt.uni-hannover.de>
2021-10-22 20:34:14 +0200

    Ignore unsupported records in the mgrec importer: - Records with splices, as currently not supported. - Split alignments with > 32767 distance, as not yet supported (needs to be encoded as two different records)

--- src/genie/format/mgrec/importer.cc
+++ src/genie/format/mgrec/importer.cc
@@ -5,6 +5,7 @@
  */
 
 #include "genie/format/mgrec/importer.h"
+#include <string>
 #include <utility>
 #include "genie/util/ordered-section.h"
 #include "genie/util/watch.h"
@@ -21,15 +22,49 @@ Importer::Importer(size_t _blockSize, std::istream& _file_1) : blockSize(_blockS
 
 // ---------------------------------------------------------------------------------------------------------------------
 
+bool isECigarSupported(const std::string& ecigar) {
+    // Splices not supported
+    if ((ecigar.find_first_of('*') != std::string::npos) || (ecigar.find_first_of('/') != std::string::npos) ||
+        (ecigar.find_first_of('%') != std::string::npos)) {
+        return false;
+    }
+    return true;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+bool isRecordSupported(const core::record::Record& rec) {
+    for (const auto& a : rec.getAlignments()) {
+        if (!isECigarSupported(a.getAlignment().getECigar())) {
+            return false;
+        }
+        for (const auto& s : a.getAlignmentSplits()) {
+            if (s->getType() == core::record::AlignmentSplit::Type::SAME_REC) {
+                if (!isECigarSupported(
+                        dynamic_cast<core::record::alignment_split::SameRec&>(*s).getAlignment().getECigar())) {
+                    return false;
+                }
+                // Splits with more than 32767 delta must be encoded in separate records, which is not yet supported
+                if (std::abs(dynamic_cast<core::record::alignment_split::SameRec&>(*s).getDelta()) > 32767) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
 bool Importer::pumpRetrieve(core::Classifier* _classifier) {
     util::Watch watch;
     core::record::Chunk chunk;
-    {
-        for (size_t i = 0; i < blockSize; ++i) {
-            core::record::Record rec(reader);
-            if (!reader.isGood()) {
-                break;
-            }
+    for (size_t i = 0; i < blockSize; ++i) {
+        core::record::Record rec(reader);
+        if (!reader.isGood()) {
+            break;
+        }
+        if (isRecordSupported(rec)) {
             chunk.getData().emplace_back(std::move(rec));
         }
     }
