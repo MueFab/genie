775282dee053970704ce54f49f4d64da3337105c
Jan Hackspiel <janhackspiel@gmail.com>
2022-06-08 17:55:13 +0200

    fixed CMakeLists and added functions stubs

--- src/genie/quality/qvcalq/encoder.cc
+++ src/genie/quality/qvcalq/encoder.cc
@@ -7,6 +7,7 @@
 #include "genie/quality/qvcalq//encoder.h"
 #include <string>
 #include <utility>
+#include "calq/calq_coder.h"
 #include "genie/core/record/alignment_split/same-rec.h"
 #include "genie/util/watch.h"
 #include "iostream"
@@ -16,10 +17,20 @@ namespace genie {
 namespace quality {
 namespace qvcalq {
 
-
 core::QVEncoder::QVCoded Encoder::process(const core::record::Chunk& rec) {
-    std::cout<<"LOL"<<std::endl;
+//    for (const auto& r : rec.getData()) {
+//        auto seg = r.getSegments();
+//    }
+
+    calq::EncodingOptions encodingOptions; // TODO: option available over commandline
+    calq::SideInformation sideInformation = getSideInformation(rec);
+    calq::EncodingBlock input = getEncodingBlock(rec);
+    calq::DecodingBlock output;
+    calq::encode(encodingOptions, sideInformation, input, &output);
 }
+
+calq::SideInformation Encoder::getSideInformation(const core::record::Chunk& record) {}
+calq::EncodingBlock Encoder::getEncodingBlock(const core::record::Chunk& record) {}
 
 // ---------------------------------------------------------------------------------------------------------------------
 
