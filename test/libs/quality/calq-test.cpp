410347a339e8884b6ce8c707589dc7945839829e
Jan Hackspiel <janhackspiel@gmail.com>
2022-08-11 14:42:47 +0200

    added test-stump for calq local_reference

---
+++ test/libs/quality/calq-test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include "genie/quality/calq/local-reference.h"
+
+TEST(CalqTest, local_reference) {
+    auto local_ref = genie::quality::calq::LocalReference(1024);
+    EXPECT_TRUE(false);
+}
