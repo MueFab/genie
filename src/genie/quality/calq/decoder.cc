b512026ffb4a3059460db518332973a75a3e189c
Jan Hackspiel <janhackspiel@gmail.com>
2022-09-14 12:38:27 +0200

    now using calq for unaligned as well

--- src/genie/quality/calq/decoder.cc
+++ src/genie/quality/calq/decoder.cc
@@ -30,7 +30,7 @@ std::vector<std::string> Decoder::decodeAligned(const quality::paramqv1::Quality
     calq::DecodingBlock input;
 
     sideInformation.posOffset = positions[0];
-    sideInformation.qualOffset = 0; // TODO: check
+    sideInformation.qualOffset = 0;  // TODO: check
     fillInput(input, desc, param);
 
     // fill sideInformation
@@ -47,25 +47,20 @@ std::vector<std::string> Decoder::decodeUnaligned(const quality::paramqv1::Quali
                                                   core::AccessUnit::Descriptor& desc) {
     // from qvwriteout
 
+    auto codebook = param_casted.getCodebook(0).getEntries();
+
     std::vector<std::string> qv;
 
     for (const auto& ecigar : ecigar_vec) {
         qv.emplace_back();
-        if (!desc.get(0).end() && !desc.get(0).pull()) {
-            continue;
-        }
+
         core::CigarTokenizer::tokenize(
             ecigar, core::getECigarInfo(),
             [&qv, &desc, &param_casted](uint8_t cigar, const util::StringView& bs, const util::StringView&) -> bool {
-                auto codebook = (uint8_t)param_casted.getNumberCodeBooks() - 1;
-                if (core::getECigarInfo().lut_step_ref[cigar] ||
-                    core::getAlphabetProperties(core::AlphabetID::ACGTN).isIncluded(cigar)) {
-                    codebook = desc.get(1).end() ? 0 : (uint8_t)desc.get(1).pull();
-                }
-
+                (void)cigar;
                 for (size_t i = 0; i < bs.length(); ++i) {
-                    auto index = (uint8_t)desc.get((uint16_t)(codebook + 2)).pull();
-                    qv.back().push_back(param_casted.getCodebook(codebook).getEntries()[index]);
+                    auto index = (uint8_t)desc.get((uint16_t)(2)).pull();
+                    qv.back().push_back(param_casted.getCodebook(0).getEntries()[index]);
                 }
                 return true;
             });
