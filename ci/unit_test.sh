a21ed412a946cdfcd085a7082b68e0282a6a9100
Stefanie Wardenaar <stefaniewardenaar@users.noreply.github.com>
2023-05-10 14:07:05 +0200

    replaced unit tests in unit_test.sh

--- ci/unit_test.sh
+++ ci/unit_test.sh
@@ -18,56 +18,36 @@ fi
 
 git_root_dir="$(git rev-parse --show-toplevel)"
 
-if [[ ! -x $git_root_dir/cmake-build-debug/bin/util-tests$fileExt ]]
+if [[ ! -x $git_root_dir/cmake-build-debug/bin/Debug/annotationParameterSet-tests$fileExt ]]
 then
-    echo "Utils test not found. Please build genie in debug mode first."
+    echo "annotationParameterSet test not found. Please build genie in debug mode first."
     exit 1
 fi
 
-if [[ ! -x $git_root_dir/cmake-build-debug/bin/coding-tests$fileExt ]]
+if [[ ! -x $git_root_dir/cmake-build-debug/bin/Debug/VariantGenotypeRecord-tests$fileExt ]]
 then
-    echo "Coding test not found. Please build genie in debug mode first."
+    echo "VariantGenotypeRecord test not found. Please build genie in debug mode first."
     exit 1
 fi
 
-if [[ ! -x $git_root_dir/cmake-build-debug/bin/gabac-tests$fileExt ]]
+if [[ ! -x $git_root_dir/cmake-build-debug/bin/Debug/VariantSiteRecord-tests$fileExt ]]
 then
-    echo "Gabac test not found. Please build genie in debug mode first."
-    exit 1
-fi
-
-if [[ ! -x $git_root_dir/cmake-build-debug/bin/read-tests$fileExt ]]
-then
-    echo "Read test not found. Please build genie in debug mode first."
-    exit 1
-fi
-
-if [[ ! -x $git_root_dir/cmake-build-debug/bin/quality-tests$fileExt ]]
-then
-    echo "Quality test not found. Please build genie in debug mode first."
+    echo "VariantSiteRecord test not found. Please build genie in debug mode first."
     exit 1
 fi
 
 ############ Run ###############
 
 echo "Running unit tests:"
-echo "Utils..."
-cd $git_root_dir/cmake-build-debug/bin
-./util-tests$fileExt --gtest_output=xml:utils_test.xml --gtest_brief=1 || { echo 'Utils test failed!' ; exit 1; }
-echo "Utils test passed!"
-echo "Coding..."
-cd $git_root_dir/cmake-build-debug/bin
-./coding-tests$fileExt --gtest_output=xml:coding_test.xml --gtest_brief=1 || { echo 'Coding test failed!' ; exit 1; }
-echo "Coding test passed!"
-echo "Gabac..."
-cd $git_root_dir/cmake-build-debug/bin
-./gabac-tests$fileExt --gtest_output=xml:gabac_test.xml --gtest_brief=1 || { echo 'Gabac test failed!' ; exit 1; }
-echo "Gabac test passed!"
-echo "Read..."
-cd $git_root_dir/cmake-build-debug/bin
-./read-tests$fileExt --gtest_output=xml:read_test.xml --gtest_brief=1 || { echo 'Read test failed!' ; exit 1; }
-echo "Read test passed!"
-echo "Quality..."
-cd $git_root_dir/cmake-build-debug/bin
-./quality-tests$fileExt --gtest_output=xml:calq.xml --gtest_brief=1 || { echo 'Quality test failed!' ; exit 1; }
-echo "Quality test passed!"
+echo "annotationParameterSet..."
+cd $git_root_dir/cmake-build-debug/bin/Debug
+./annotationParameterSet-tests$fileExt --gtest_output=xml:annotationParameterSet-tests.xml --gtest_brief=1 || { echo 'annotationParameterSet-tests failed!' ; exit 1; }
+echo "annotationParameterSet-tests passed!"
+echo "VariantGenotypeRecord..."
+cd $git_root_dir/cmake-build-debug/bin/Debug
+./VariantGenotypeRecord-tests$fileExt --gtest_output=xml:VariantGenotypeRecord-tests.xml --gtest_brief=1 || { echo 'VariantGenotypeRecord-tests failed!' ; exit 1; }
+echo "VariantGenotypeRecord-tests passed!"
+echo "VariantSiteRecord..."
+cd $git_root_dir/cmake-build-debug/bin/Debug
+./VariantSiteRecord-tests$fileExt --gtest_output=xml:VariantSiteRecord-tests.xml --gtest_brief=1 || { echo 'VariantSiteRecord-tests failed!' ; exit 1; }
+echo "VariantSiteRecord-tests passed!"
