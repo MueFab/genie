171d08aee9ebad1985c509e03ce66afc433f3f39
Thomas de Bont <thomas.de.bont@philips.com>
2023-10-26 11:31:56 +0200

    Suppress build tests in `xtl` to avoid dependency on `doctest`.

--- cmake/Findxtl.cmake
+++ cmake/Findxtl.cmake
@@ -5,5 +5,7 @@ FetchContent_Declare(
     GIT_REPOSITORY https://www.github.com/xtensor-stack/xtl
     GIT_TAG fea39142693fbbc2ef19d75012bc6b46ef0a5f8c # v0.7.5
 )
- 
+
+set(BUILD_TESTS OFF)
+
 FetchContent_MakeAvailable(xtl)
