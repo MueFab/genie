3828efb1c3d0125ee05e538d83978dbdafb26187
Yeremia Gunawan Adhisanto <adhisant@tnt.uni-hannover.de>
2023-11-23 15:06:05 +0100

    Add test data for likelihood

---
+++ src/genie/likelihood/likelihood_payload.h
@@ -0,0 +1,10 @@
+//
+// Created by Yeremia on 22/11/2023.
+//
+
+#ifndef GENIE_LIKELIHOOD_PAYLOAD_H
+#define GENIE_LIKELIHOOD_PAYLOAD_H
+
+class likelihood_payload {};
+
+#endif  // GENIE_LIKELIHOOD_PAYLOAD_H
