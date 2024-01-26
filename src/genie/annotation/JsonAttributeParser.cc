8128e992f5a335f29c5e4d08baf1ad1c130f2f7f
Stefanie Wardenaar <stefaniewardenaar@users.noreply.github.com>
2024-01-26 16:56:55 +0100

    added Annotation class + tests

---
+++ src/genie/annotation/JsonAttributeParser.cc
@@ -0,0 +1,67 @@
+/**
+ * @file
+ * @copyright This file is part of GENIE. See LICENSE and/or
+ * https://github.com/mitogen/genie for more details.
+ */
+
+#include <algorithm>
+#include <string>
+#include <utility>
+#include <cctype>
+
+#include "genie/annotation/JsonAttributeParser.h"
+#include "genie/util/bitreader.h"
+#include "genie/util/bitwriter.h"
+#include "genie/util/make-unique.h"
+#include "genie/util/runtime-exception.h"
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace genie {
+namespace annotation {
+
+void JsonAttributeParser::ParseInfoFields() {
+    infoFieldsJSON.erase(std::remove(infoFieldsJSON.begin(), infoFieldsJSON.end(), '\n'), infoFieldsJSON.end());
+    infoFieldsJSON.erase(std::remove(infoFieldsJSON.begin(), infoFieldsJSON.end(), ' '), infoFieldsJSON.end());
+    infoFieldsJSON.erase(std::remove(infoFieldsJSON.begin(), infoFieldsJSON.end(), '\t'), infoFieldsJSON.end());
+    infoFieldsJSON.erase(std::remove(infoFieldsJSON.begin(), infoFieldsJSON.end(), '"'), infoFieldsJSON.end());
+
+    std::stringstream infoFieldsList;
+    infoFieldsList << infoFieldsJSON.substr(1, infoFieldsJSON.size() - 2);
+    std::string infoFieldString;
+    while (std::getline(infoFieldsList, infoFieldString, '}')) {
+        infoFieldString = infoFieldString.substr(1, infoFieldString.size() - 1);
+        InfoField infoField;
+        auto start = infoFieldString.find(':');
+        auto end = infoFieldString.find(',');
+        infoField.ID = infoFieldString.substr(start + 1, end - start - 1);
+        start = infoFieldString.find(':', end + 1);
+        end = infoFieldString.find(',', end + 1);
+        infoField.Type = InterpretType(infoFieldString.substr(start + 1, end - start - 1));
+        start = infoFieldString.find(':', end + 1);
+        end = infoFieldString.find(',', end + 1);
+        infoField.Number = InterpretSize(infoFieldString.substr(start + 1, end - start - 1));
+        infoFields.push_back(infoField);
+    }
+}
+
+genie::core::DataType JsonAttributeParser::InterpretType(std::string type) const {
+    if (type == "STRING") return genie::core::DataType::STRING;
+    if (type == "INTEGER") return genie::core::DataType::INT32;
+    if (type == "FLOAT")
+        return genie::core::DataType::FLOAT;
+    else
+        return genie::core::DataType::UINT64;
+}
+
+uint8_t JsonAttributeParser::InterpretSize(std::string size) const {
+    auto str = size.c_str();
+    if (std::isdigit(size[0])) return static_cast<uint8_t>(atoi(str));
+    // size == '.'
+    return 1;
+}
+
+}  // namespace annotation
+}  // namespace genie
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
---
+++ src/genie/annotation/annotation.h
@@ -0,0 +1,239 @@
+/**
+ * @file
+ * @copyright This file is part of GENIE. See LICENSE and/or
+ * https://github.com/mitogen/genie for more details.
+ */
+
+#ifndef GENIE_ANNOTATION_ANNOTATION_H
+#define GENIE_ANNOTATION_ANNOTATION_H
+
+#include <cstdint>
+#include <fstream>
+#include <list>
+#include <map>
+#include <optional>
+#include <sstream>
+#include <tuple>
+#include "genie/annotation/JsonAttributeParser.h"
+#include "genie/core/constants.h"
+#include "genie/variantsite/AccessUnitComposer.h"
+#include "genie/variantsite/ParameterSetComposer.h"
+#include "genie/variantsite/VariantSiteParser.h"
+
+#include "genie/core/record/variant_genotype/record.h"
+#include "genie/genotype/ParameterSetComposer.h"
+#include "genie/genotype/genotype_coder.h"
+#include "genie/likelihood/likelihood_coder.h"
+#include "genie/likelihood/likelihood_payload.h"
+
+#include "genie/core/record/data_unit/record.h"
+// -----------------------------------------------------------------------------
+
+namespace genie {
+namespace annotation {
+// ---------------------------------------------------------------------------------------------------------------------
+
+enum class RecType { SITE_FILE = 0, GENO_FILE };
+
+class Descriptors {};
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+class Annotation {
+ public:
+    void startStream(RecType recType, std::string recordInputFileName, std::string attributeJsonFileName, std::string outputFileName) {
+        // read attributes info from json file
+        std::ifstream AttributeFieldsFile;
+        AttributeFieldsFile.open(attributeJsonFileName, std::ios::in);
+        std::stringstream attributeFields;
+        UTILS_DIE_IF(!AttributeFieldsFile.is_open(), "unable to open json file");
+
+        if (AttributeFieldsFile.is_open()) {
+            attributeFields << AttributeFieldsFile.rdbuf();
+            AttributeFieldsFile.close();
+        }
+        JsonAttributeParser attributeParser(attributeFields);
+        // determine the compressors
+
+        std::ifstream inputfile;
+        inputfile.open(recordInputFileName, std::ios::in | std::ios::binary);
+
+        if (!inputfile.is_open())  // failed
+            return;
+
+        // read file - determine type?
+        std::vector<genie::core::AnnotDesc> descrList;
+        genie::core::record::annotation_parameter_set::Record annotationParameterSet;
+        genie::variant_site::AccessUnitComposer accessUnitcomposer;
+        std::vector<genie::core::record::annotation_access_unit::Record> annotationAccessUnit;
+
+        if (recType == RecType::GENO_FILE) {
+            annotationAccessUnit.resize(1);
+            (void)recordInputFileName;
+            descrList.push_back(genie::core::AnnotDesc::GENOTYPE);
+            descrList.push_back(genie::core::AnnotDesc::LIKELIHOOD);
+            descrList.push_back(genie::core::AnnotDesc::LINKID);
+
+            std::vector<genie::core::record::VariantGenotype> recs;
+            genie::util::BitReader bitreader(inputfile);
+            while (bitreader.isGood()) {
+                recs.emplace_back(bitreader);
+            }
+            recs.pop_back();
+
+            uint32_t BLOCK_SIZE = 200;
+            bool TRANSFORM_MODE = true;
+
+            genie::likelihood::EncodingOptions likelihood_opt = {
+                BLOCK_SIZE,      // block_size
+                TRANSFORM_MODE,  // transform_flag;
+            };
+
+            genie::genotype::EncodingOptions genotype_opt = {
+                BLOCK_SIZE,                                  // block_size;
+                genie::genotype::BinarizationID::BIT_PLANE,  // binarization_ID;
+                genie::genotype::ConcatAxis::DO_NOT_CONCAT,  // concat_axis;
+                false,                                       // transpose_mat;
+                genie::genotype::SortingAlgoID::NO_SORTING,  // sort_row_method;
+                genie::genotype::SortingAlgoID::NO_SORTING,  // sort_row_method;
+                genie::core::AlgoID::JBIG                    // codec_ID;
+            };
+
+            auto genotypeData = genie::genotype::encode_block(genotype_opt, recs);
+            auto likelihoodData = genie::likelihood::encode_block(likelihood_opt, recs);
+            //--------------------------------------------------
+            uint8_t AT_ID = 1;
+            uint8_t AG_class = 0;
+            genie::genotype::GenotypeParameters genotypeParameters =
+                std::get<genie::genotype::GenotypeParameters>(genotypeData);
+            auto datablock = std::get<genie::genotype::EncodingBlock>(genotypeData);
+            genie::genotype::ParameterSetComposer genotypeParameterSet;
+            annotationParameterSet = genotypeParameterSet.Build(
+                AT_ID, datablock.attributeInfo, genotypeParameters,
+                std::get<genie::likelihood::LikelihoodParameters>(likelihoodData), recs.size());
+            std::map<std::string, genie::core::record::annotation_access_unit::TypedData> attributeTDStream;
+
+            for (auto formatdata : datablock.attributeData) {
+                auto& info = datablock.attributeInfo[formatdata.first];
+                std::vector<uint32_t> arrayDims;
+                arrayDims.push_back(std::min(BLOCK_SIZE, static_cast<uint32_t>(recs.size())));
+                arrayDims.push_back(recs.at(0).getNumSamples());
+                arrayDims.push_back(recs.at(0).getFormatCount());
+                attributeTDStream[formatdata.first].set(info.getAttributeType(), static_cast<uint8_t>(arrayDims.size()),
+                                                        arrayDims);
+                attributeTDStream[formatdata.first].convertToTypedData(formatdata.second);
+            }
+
+            std::map<genie::core::AnnotDesc, std::stringstream> descriptorStream;
+            descriptorStream[genie::core::AnnotDesc::GENOTYPE];
+            {
+                genie::genotype::GenotypePayload genotypePayload(datablock, genotypeParameters);
+                genie::core::Writer writer(&descriptorStream[genie::core::AnnotDesc::GENOTYPE]);
+                genotypePayload.write(writer);
+            }
+
+            descriptorStream[genie::core::AnnotDesc::LIKELIHOOD];
+            {
+                genie::likelihood::LikelihoodPayload payload(
+                    std::get<genie::likelihood::LikelihoodParameters>(likelihoodData),
+                    std::get<genie::likelihood::EncodingBlock>(likelihoodData));
+                genie::core::Writer writer(&descriptorStream[genie::core::AnnotDesc::LIKELIHOOD]);
+                payload.write(writer);
+            }
+
+            // add LINK_ID default values
+            for (auto i = 0u; i < BLOCK_SIZE && i < recs.size(); ++i) {
+                char val = static_cast<char>(0xFF);
+                descriptorStream[genie::core::AnnotDesc::LINKID].write(&val, 1);
+            }
+            accessUnitcomposer.setAccessUnit(descriptorStream, attributeTDStream, datablock.attributeInfo,
+                                             annotationParameterSet, annotationAccessUnit.at(0), AG_class, AT_ID, 0);
+
+        } else {
+            uint64_t defaultTileSize = 1000;
+            genie::variant_site::VariantSiteParser parser(inputfile, attributeFields, defaultTileSize);
+            auto infoFields = attributeParser.getInfoFields();
+            std::map<std::string, InfoField> attributeInfo;
+            for (auto infoField : infoFields) attributeInfo[infoField.ID] = infoField;
+            descrList.push_back(genie::core::AnnotDesc::SEQUENCEID);
+            descrList.push_back(genie::core::AnnotDesc::STARTPOS);
+            descrList.push_back(genie::core::AnnotDesc::STRAND);
+            descrList.push_back(genie::core::AnnotDesc::NAME);
+            descrList.push_back(genie::core::AnnotDesc::DESCRIPTION);
+            descrList.push_back(genie::core::AnnotDesc::LINKNAME);
+            descrList.push_back(genie::core::AnnotDesc::LINKID);
+            descrList.push_back(genie::core::AnnotDesc::DEPTH);
+            descrList.push_back(genie::core::AnnotDesc::SEQQUALITY);
+            descrList.push_back(genie::core::AnnotDesc::MAPQUALITY);
+            descrList.push_back(genie::core::AnnotDesc::MAPNUMQUALITY0);
+            descrList.push_back(genie::core::AnnotDesc::REFERENCE);
+            descrList.push_back(genie::core::AnnotDesc::ALTERN);
+            descrList.push_back(genie::core::AnnotDesc::FILTER);
+            genie::variant_site::ParameterSetComposer encodeParameters;
+            annotationParameterSet =
+                encodeParameters.setParameterSet(descrList, parser.getAttributes().getInfo(), defaultTileSize);
+
+            uint8_t AG_class = 1;
+            uint8_t AT_ID = 0;
+            genie::variant_site::AccessUnitComposer accessUnit;
+            annotationAccessUnit.resize(parser.getNrOfTiles());
+            uint64_t rowIndex = 0;
+
+            auto& descrStream = parser.getDescriptors().getTiles();
+            std::map<std::string, genie::core::record::annotation_access_unit::TypedData> attr;
+            for (auto& tile : descrStream) descrList.push_back(tile.first);
+
+            for (uint64_t i = 0; i < parser.getNrOfTiles(); ++i) {
+                std::map<genie::core::AnnotDesc, std::stringstream> desc;
+                for (auto& desctile : descrStream) {
+                    desc[desctile.first] << desctile.second.getTile(i).rdbuf();
+                }
+                for (auto& attrtile : parser.getAttributes().getTiles()) {
+                    attr[attrtile.first] = attrtile.second.getTypedTile(i);
+                }
+
+                accessUnit.setAccessUnit(desc, attr, parser.getAttributes().getInfo(), annotationParameterSet, annotationAccessUnit.at(i), AG_class, AT_ID,
+                    rowIndex);
+                rowIndex += defaultTileSize;
+
+            }
+        }
+        if (inputfile.is_open()) inputfile.close();
+
+        // determine and write the annotation parameter set to output(stream)
+        // parse n rows for complete tile or until end of file
+        // determine and write the annotation access unit to output(stream)
+
+        std::ofstream testfile;
+        std::string filename = outputFileName;
+        testfile.open(filename  + ".bin", std::ios::binary | std::ios::out);
+        genie::core::Writer testwriter(&testfile);
+        std::ofstream txtfile;
+        txtfile.open(filename + ".txt", std::ios::out);
+        genie::core::Writer txtwriter(&txtfile, true);
+
+        genie::core::record::data_unit::Record APS_dataUnit(annotationParameterSet);
+        APS_dataUnit.write(testwriter);
+        APS_dataUnit.write(txtwriter);
+
+        for (auto& aau : annotationAccessUnit) {
+            genie::core::record::data_unit::Record AAU_dataUnit(aau);
+            AAU_dataUnit.write(testwriter);
+            AAU_dataUnit.write(txtwriter);
+        }
+        testfile.close();
+        txtfile.close();
+
+
+    }
+
+ private:
+    std::ifstream recordInput;
+};
+
+}  // namespace annotation
+}  // namespace genie
+
+// -----------------------------------------------------------------------------
+
+#endif  // GENIE_ANNOTATION_ANNOTATION_H
