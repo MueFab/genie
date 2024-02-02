72459e6966729c02497868d1bc6c5b5afa703fa0
Yeremia Gunawan Adhisanto <adhisant@tnt.uni-hannover.de>
2024-02-02 15:04:34 +0100

    Complete ContactRecord without normalization and the correspondign gtest

--- src/genie/core/record/contact/record.cc
+++ src/genie/core/record/contact/record.cc
@@ -21,8 +21,58 @@ namespace record {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-ContactRecord::ContactRecord(util::BitReader &reader) {
+ContactRecord::ContactRecord(util::BitReader &reader){
+    // Sample
+    sample_ID = reader.readBypassBE<uint8_t>();
+    sample_name.resize(reader.readBypassBE<uint8_t>());
+    reader.readBypass(&sample_name[0], sample_name.size());
 
+    // chr1
+    chr1_ID = reader.readBypassBE<uint8_t>();
+    chr1_name.resize(reader.readBypassBE<uint8_t>());
+    reader.readBypass(&chr1_name[0], chr1_name.size());
+
+    // chr2
+    chr2_ID = reader.readBypassBE<uint8_t>();
+    chr2_name.resize(reader.readBypassBE<uint8_t>());
+    reader.readBypass(&chr2_name[0], chr2_name.size());
+
+    // num_counts and num_norm_counts
+    auto num_counts = reader.readBypassBE<uint64_t>();
+    auto num_norm_counts = reader.readBypassBE<uint8_t>();
+
+    //TODO @Yeremia: Implement this!
+    UTILS_DIE_IF(num_norm_counts != 0, "Not yet implemented for num_norm_counts > 0");
+    for (auto i = 0; i < num_norm_counts; i++){
+    }
+
+    start_pos1.resize(num_counts);
+    for (size_t i = 0; i< num_counts; i++){
+        start_pos1[i] = reader.readBypassBE<uint64_t>();
+    }
+
+    end_pos1.resize(num_counts);
+    for (size_t i = 0; i< num_counts; i++){
+        end_pos1[i] = reader.readBypassBE<uint64_t>();
+    }
+
+    start_pos2.resize(num_counts);
+    for (size_t i = 0; i< num_counts; i++){
+        start_pos2[i] = reader.readBypassBE<uint64_t>();
+    }
+
+    end_pos2.resize(num_counts);
+    for (size_t i = 0; i< num_counts; i++){
+        end_pos2[i] = reader.readBypassBE<uint64_t>();
+    }
+
+    counts.resize(num_counts);
+    for (size_t i = 0; i< num_counts; i++){
+        counts[i] = reader.readBypassBE<uint32_t>();
+    }
+
+    auto link_record_flag = reader.readBypassBE<bool>();
+    UTILS_DIE_IF(link_record_flag, "Not yet implemented for link_record!");
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -55,10 +105,32 @@ uint64_t ContactRecord::getNumCounts() const {return counts.size();}
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-uint8_t ContactRecord::getNormCounts() const {return norm_count_names.size();}
+uint8_t ContactRecord::getNumNormCounts() const {return norm_count_names.size();}
 
+// ---------------------------------------------------------------------------------------------------------------------
+
+const std::vector<uint64_t>& ContactRecord::getStartPos1() const {return start_pos1;}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+const std::vector<uint64_t>& ContactRecord::getEndPos1() const {return end_pos1;}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+const std::vector<uint64_t>& ContactRecord::getStartPos2() const {return start_pos2;}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+const std::vector<uint64_t>& ContactRecord::getEndPos2() const {return end_pos2;}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+const std::vector<uint32_t>& ContactRecord::getCounts() const {return counts;}
+
+// ---------------------------------------------------------------------------------------------------------------------
 
 void ContactRecord::write(util::BitWriter &writer) const {}
+
 // ---------------------------------------------------------------------------------------------------------------------
 
 }  // namespace record
