d68998a18f413b76de9e3090967d6387a3f55575
Yeremia Gunawan Adhisanto <adhisant@tnt.uni-hannover.de>
2024-03-25 10:41:23 +0100

    Update Subcontact matrix parameters

--- src/genie/contact/subcontact_matrix_parameters.cc
+++ src/genie/contact/subcontact_matrix_parameters.cc
@@ -15,7 +15,47 @@ namespace contact {
 // ---------------------------------------------------------------------------------------------------------------------
 
 SubcontactMatrixParameters::SubcontactMatrixParameters()
-    : parameter_set_ID(0), chr1_ID(0), chr2_ID(0), tile_parameters(), row_mask_exists_flag(false), col_mask_exists_flag(false) {}
+    : parameter_set_ID(0),
+      chr1_ID(0),
+      chr2_ID(0),
+      tile_parameters(),
+      row_mask_exists_flag(false),
+      col_mask_exists_flag(false) {}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+SubcontactMatrixParameters::SubcontactMatrixParameters(SubcontactMatrixParameters&& other) noexcept
+    : parameter_set_ID(other.parameter_set_ID),
+      chr1_ID(other.chr1_ID),
+      chr2_ID(other.chr2_ID),
+      tile_parameters(std::move(other.tile_parameters)),
+      row_mask_exists_flag(other.row_mask_exists_flag),
+      col_mask_exists_flag(other.col_mask_exists_flag) {}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// Constructor by reference
+SubcontactMatrixParameters::SubcontactMatrixParameters(const SubcontactMatrixParameters& other)
+    : parameter_set_ID(other.parameter_set_ID),
+      chr1_ID(other.chr1_ID),
+      chr2_ID(other.chr2_ID),
+      tile_parameters(other.tile_parameters),
+      row_mask_exists_flag(other.row_mask_exists_flag),
+      col_mask_exists_flag(other.col_mask_exists_flag) {}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+SubcontactMatrixParameters& SubcontactMatrixParameters::operator=(const SubcontactMatrixParameters& other) {
+    if (this != &other) {
+        parameter_set_ID = other.parameter_set_ID;
+        chr1_ID = other.chr1_ID;
+        chr2_ID = other.chr2_ID;
+        tile_parameters = other.tile_parameters;
+        row_mask_exists_flag = other.row_mask_exists_flag;
+        col_mask_exists_flag = other.col_mask_exists_flag;
+    }
+    return *this;
+}
 
 // ---------------------------------------------------------------------------------------------------------------------
 
@@ -88,22 +128,6 @@ void SubcontactMatrixParameters::setChr2ID(uint8_t id) { chr2_ID = id; }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-ChrIDPair SubcontactMatrixParameters::getChrPair() { return {chr1_ID, chr2_ID}; }
-
-// ---------------------------------------------------------------------------------------------------------------------
-
-bool SubcontactMatrixParameters::isSymmetrical() const { return chr1_ID == chr2_ID;}
-
-// ---------------------------------------------------------------------------------------------------------------------
-
-size_t SubcontactMatrixParameters::getNTilesInRow() const { return tile_parameters.size(); }
-
-// ---------------------------------------------------------------------------------------------------------------------
-
-size_t SubcontactMatrixParameters::getNTilesInCol() const { return tile_parameters.front().size(); }
-
-// ---------------------------------------------------------------------------------------------------------------------
-
 const std::vector<std::vector<TileParameter>>& SubcontactMatrixParameters::getTileParameters() const {
     return tile_parameters;
 }
@@ -132,13 +156,95 @@ void SubcontactMatrixParameters::setColMaskExistsFlag(bool flag) { col_mask_exis
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-// TODO (Yeremia): Implement this!
-void SubcontactMatrixParameters::write(core::Writer& writer) const {}
+ChrIDPair SubcontactMatrixParameters::getChrPair() { return {chr1_ID, chr2_ID}; }
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+bool SubcontactMatrixParameters::isSymmetrical() const { return chr1_ID == chr2_ID;}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+size_t SubcontactMatrixParameters::getNTilesInRow() const { return tile_parameters.size(); }
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+size_t SubcontactMatrixParameters::getNTilesInCol() const { return tile_parameters.front().size(); }
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+SubcontactMatrixParameters::SubcontactMatrixParameters(util::BitReader& reader)
+    : parameter_set_ID(reader.readBypassBE<uint8_t>()),
+      chr1_ID(reader.readBypassBE<uint8_t>()),
+      chr2_ID(reader.readBypassBE<uint8_t>()) {
+
+    // Read the tile parameters
+    size_t nrows = reader.readBypassBE<uint32_t>();
+    size_t ncols = reader.readBypassBE<uint32_t>();
+
+    tile_parameters.resize(nrows);
+    for (size_t i = 0; i < nrows; ++i) {
+        tile_parameters[i].resize(ncols);
+        for (size_t j = 0; j < ncols; ++j) {
+
+            if (isSymmetrical() && i > j){
+                continue;
+            }
+            // 3 bits reserved + 3 bits for the mode itself
+            tile_parameters[i][j].diag_tranform_mode = reader.read<DiagonalTransformMode>(6);
+            tile_parameters[i][j].binarization_mode = reader.read<BinarizationMode>(2);
+            reader.flush();
+        }
+    }
+}
 
 // ---------------------------------------------------------------------------------------------------------------------
 
 // TODO (Yeremia): Implement this!
-size_t SubcontactMatrixParameters::getSize(core::Writer& writesize) const { return 0; }
+size_t SubcontactMatrixParameters::getSize() const {
+    size_t size = 0;
+
+    // Add the size of the parameter_set_ID, chr1_ID, chr2_ID, row_mask_exists_flag, and col_mask_exists_flag
+    size += sizeof(parameter_set_ID);
+    size += sizeof(chr1_ID);
+    size += sizeof(chr2_ID);
+
+    for (size_t i = 0; i < getNTilesInRow(); ++i) {
+        for (size_t j = 0; j < getNTilesInCol(); ++j) {
+            if (isSymmetrical() && i > j){
+                continue;
+            }
+
+            size += 1; // reserved + DiagonalTransformMode + BinarizationMode
+        }
+    }
+
+    size += sizeof(row_mask_exists_flag);
+    size += sizeof(col_mask_exists_flag);
+
+    return size;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+void SubcontactMatrixParameters::write(
+    util::BitWriter& writer
+) const {
+    writer.writeBypassBE(parameter_set_ID);
+    writer.writeBypassBE(chr1_ID);
+    writer.writeBypassBE(chr2_ID);
+
+    // Write the tile_parameters
+    for (const auto& row : tile_parameters) {
+        for (const auto& tile : row) {
+            writer.write(static_cast<uint8_t>(tile.diag_tranform_mode), 6);
+            writer.write(static_cast<uint8_t>(tile.binarization_mode), 2);
+        }
+    }
+    writer.flush();
+
+    writer.writeBypassBE(row_mask_exists_flag);
+    writer.writeBypassBE(col_mask_exists_flag);
+}
 
 // ---------------------------------------------------------------------------------------------------------------------
 
--- src/genie/contact/subcontact_matrix_parameters.h
+++ src/genie/contact/subcontact_matrix_parameters.h
@@ -60,31 +60,184 @@ class SubcontactMatrixParameters {
     bool col_mask_exists_flag;
 
  public:
+    // Default constructor
     SubcontactMatrixParameters();
 
+    // Constructor from move
     SubcontactMatrixParameters(uint8_t parameter_set_ID, uint8_t chr1_ID, uint8_t chr2_ID, std::vector<std::vector<TileParameter>>&& tile_parameters,
                                bool row_mask_exists_flag, bool col_mask_exists_flag);
 
+    // Constructor from move
+    SubcontactMatrixParameters(SubcontactMatrixParameters&& other) noexcept;
+
+    // Constructor by reference
+    SubcontactMatrixParameters(const SubcontactMatrixParameters& other);
+
+    // Constructor using operator=
+    SubcontactMatrixParameters& operator=(const SubcontactMatrixParameters& other);
+
+    // Getters
+    /**
+     * @brief Get the parameter set ID.
+     *
+     * This method returns the parameter set ID.
+     *
+     * @return The parameter set ID.
+     */
+    uint8_t getParameterSetID() const;
+
+    /**
+     * @brief Get the first chromosome ID.
+     *
+     * This method returns the first chromosome ID.
+     *
+     * @return The first chromosome ID.
+     */
+    uint8_t getChr1ID() const;
+
+    /**
+     * @brief Get the second chromosome ID.
+     *
+     * This method returns the second chromosome ID.
+     *
+     * @return The second chromosome ID.
+     */
+    uint8_t getChr2ID() const;
+
+    const std::vector<std::vector<TileParameter>>& getTileParameters() const;
+
+    /**
+     * @brief Get the row mask exists flag.
+     *
+     * This method returns the row mask exists flag.
+     *
+     * @return The row mask exists flag.
+     */
+    bool getRowMaskExistsFlag() const;
+
+    /**
+     * @brief Get the column mask exists flag.
+     *
+     * This method returns the column mask exists flag.
+     *
+     * @return The column mask exists flag.
+     */
+    bool getColMaskExistsFlag() const;
+
+    // Setters
+    /**
+     * @brief Set the parameter set ID.
+     *
+     * This method sets the parameter set ID.
+     *
+     * @param id The new parameter set ID.
+     */
+    void setParameterSetID(uint8_t ID);
+
+    /**
+     * @brief Set the first chromosome ID.
+     *
+     * This method sets the first chromosome ID.
+     *
+     * @param id The new first chromosome ID.
+     */
+    void setChr1ID(uint8_t ID);
+
+    /**
+     * @brief Set the second chromosome ID.
+     *
+     * This method sets the second chromosome ID.
+     *
+     * @param id The new second chromosome ID.
+     */
+    void setChr2ID(uint8_t ID);
+
+    /**
+     * @brief Set the tile parameters.
+     *
+     * This method sets the tile parameters.
+     *
+     * @param parameters The new tile parameters.
+     */
+    void setTileParameters(const std::vector<std::vector<TileParameter>>& parameters);
+
+    /**
+     * @brief Set the row mask exists flag.
+     *
+     * This method sets the row mask exists flag.
+     *
+     *  @param flag The new row mask exists flag.
+     */
+    void setRowMaskExistsFlag(bool flag);
+
+    /**
+     * @brief Set the column mask exists flag.
+     *
+     * This method sets the column mask exists flag.
+     *
+     *  @param flag The new column mask exists flag.
+     */
+    void setColMaskExistsFlag(bool flag);
+
+    ChrIDPair getChrPair();
+
+    /**
+     * @brief Check if the subcontact matrix is symmetrical.
+     *
+     * This method checks if the subcontact matrix is symmetrical.
+     * It returns true if the chromosome IDs are the same, and false otherwise.
+     *
+     * @return True if the subcontact matrix is symmetrical, false otherwise.
+     */
+    bool isSymmetrical() const;
+
+    /**
+     * @brief Get the number of tiles in a row.
+     *
+     * This method returns the number of tiles in a row.
+     *
+     * @return The number of tiles in a row.
+     */
+    size_t getNTilesInRow() const;
+
+    /**
+     * @brief Get the number of tiles in a column.
+     *
+     * This method returns the number of tiles in a column.
+     *
+     * @return The number of tiles in a column.
+     */
+    size_t getNTilesInCol() const;
+
+
+    /**
+     * @brief Constructor that reads from a BitReader.
+     *
+     * This constructor reads the parameters from a BitReader.
+     *
+     * @param reader The BitReader to read from.
+     */
     SubcontactMatrixParameters(util::BitReader& reader);
 
-    uint8_t getParameterSetID() const;
-    void setParameterSetID(uint8_t id);
-    uint8_t getChr1ID() const;
-    void setChr1ID(uint8_t id);
-    uint8_t getChr2ID() const;
-    void setChr2ID(uint8_t id);
-    ChrIDPair getChrPair();
-    bool isSymmetrical() const;
-    size_t getNTilesInRow() const;
-    size_t getNTilesInCol() const;
-    const std::vector<std::vector<TileParameter>>& getTileParameters() const;
-    void setTileParameters(const std::vector<std::vector<TileParameter>>& parameters);
-    bool getRowMaskExistsFlag() const;
-    void setRowMaskExistsFlag(bool flag);
-    bool getColMaskExistsFlag() const;
-    void setColMaskExistsFlag(bool flag);
-    void write(core::Writer& writer) const;
-    size_t getSize(core::Writer& writesize) const;
+    /**
+     * @brief Computes the size of the object in bytes.
+     *
+     * This function computes the size of the object in bytes.
+     * It adds the size of each member variable to the total size.
+     *
+     * @return The size of the object in bytes.
+     */
+    size_t getSize() const;
+
+    /**
+     * @brief Writes the object to a BitWriter.
+     *
+     * This function writes the object to a BitWriter.
+     * It writes each member variable to the BitWriter.
+     *
+     * @param writer The BitWriter to write to.
+     */
+    void write(util::BitWriter& writer) const;
 };
 
 // ---------------------------------------------------------------------------------------------------------------------
