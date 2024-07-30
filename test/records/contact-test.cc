5b42d022c336937a92fca6d0f6b5065c760d8c13
Yeremia Gunawan Adhisanto <adhisant@tnt.uni-hannover.de>
2024-07-30 14:33:24 +0200

    Add contact record and update unit test for contact record

--- test/records/contact-test.cc
+++ test/records/contact-test.cc
@@ -233,6 +233,65 @@ TEST(ContactRecord, IntraContactRecord_HR_Raw) {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
+TEST(ContactRecord, IntraContactRecord_HR_All) {
+    std::string gitRootDir = util_tests::exec("git rev-parse --show-toplevel");
+    std::string filename = "GSE63525_GM12878_insitu_primary_30.mcool-all-50000-21_21.cont";
+    std::string filepath = gitRootDir + "/data/records/contact/" + filename;
+
+    std::vector<genie::core::record::ContactRecord> recs;
+
+    {
+        std::ifstream reader(filepath, std::ios::binary);
+        ASSERT_EQ(reader.fail(), false);
+        genie::util::BitReader bitreader(reader);
+
+        while (bitreader.isGood()){
+            recs.emplace_back(bitreader);
+        }
+
+        // TODO (Yeremia): Temporary fix as the number of records exceeded by 1
+        recs.pop_back();
+    }
+
+    auto& rec = recs.front();
+
+    ASSERT_EQ(rec.getSampleID(), 0u);
+    ASSERT_EQ(rec.getSampleName(), "Test01");
+    ASSERT_EQ(rec.getChr1ID(), 21u);
+    ASSERT_EQ(rec.getChr1Name(), "22");
+    ASSERT_EQ(rec.getChr2ID(), 21u);
+    ASSERT_EQ(rec.getChr2Name(), "22");
+    //TODO: Complete all with the correct values
+    //    ASSERT_EQ(rec.getNumEntries(), 9812u);
+    //    ASSERT_EQ(rec.getNumNormCounts(), 0u);
+    //    ASSERT_EQ(rec.getStartPos1().front(), 16000000u);
+    //    ASSERT_EQ(rec.getStartPos1().back(), 51000000u);
+    //    ASSERT_EQ(rec.getEndPos1().front(), 16250000u);
+    //    ASSERT_EQ(rec.getEndPos1().back(), 51250000u);
+    //    ASSERT_EQ(rec.getStartPos2().front(), 16000000u);
+    //    ASSERT_EQ(rec.getStartPos2().back(), 51000000u);
+    //    ASSERT_EQ(rec.getEndPos2().front(), 16250000u);
+    //    ASSERT_EQ(rec.getEndPos2().back(), 51250000u);
+    //    ASSERT_EQ(rec.getCounts().front(), 16u);
+    //    ASSERT_EQ(rec.getCounts().back(), 68156u);
+
+    {
+        std::stringstream ss;
+        genie::util::BitWriter writer(&ss);
+
+        rec.write(writer);
+
+        genie::util::BitReader bitreader(ss);
+        auto recon_rec = genie::core::record::ContactRecord(bitreader);
+
+        ASSERT_TRUE(rec == recon_rec);
+
+        check_row_col_uniqueness(recon_rec);
+    }
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
 TEST(ContactRecord, InterContactRecord_LR_Raw) {
     std::string gitRootDir = util_tests::exec("git rev-parse --show-toplevel");
     std::string filename = "GSE63525_GM12878_insitu_primary_30.hic-raw-250000-21_22.cont";
@@ -292,6 +351,61 @@ TEST(ContactRecord, InterContactRecord_LR_Raw) {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
+TEST(ContactRecord, InterContactRecord_HR_Raw) {
+    std::string gitRootDir = util_tests::exec("git rev-parse --show-toplevel");
+    std::string filename = "GSE63525_GM12878_insitu_primary_30.hic-raw-50000-21_22.cont";
+    std::string filepath = gitRootDir + "/data/records/contact/" + filename;
 
+    std::vector<genie::core::record::ContactRecord> recs;
+
+    {
+        std::ifstream reader(filepath, std::ios::binary);
+        ASSERT_EQ(reader.fail(), false);
+        genie::util::BitReader bitreader(reader);
+
+        while (bitreader.isGood()){
+            recs.emplace_back(bitreader);
+        }
+
+        // TODO (Yeremia): Temporary fix as the number of records exceeded by 1
+        recs.pop_back();
+    }
+
+    auto& rec = recs.front();
+
+    ASSERT_EQ(rec.getSampleID(), 0u);
+    ASSERT_EQ(rec.getSampleName(), "Test01");
+    ASSERT_EQ(rec.getChr1ID(), 21u);
+    ASSERT_EQ(rec.getChr1Name(), "22");
+    ASSERT_EQ(rec.getChr2ID(), 22u);
+    ASSERT_EQ(rec.getChr2Name(), "X");
+    //TODO: Complete all with the correct values
+    //    ASSERT_EQ(rec.getNumEntries(), 59740);
+    //    ASSERT_EQ(rec.getNumNormCounts(), 0u);
+    //    ASSERT_EQ(rec.getStartPos1().front(), 16000000u);
+    //    ASSERT_EQ(rec.getStartPos1().back(), 51000000u);
+    //    ASSERT_EQ(rec.getEndPos1().front(), 16250000u);
+    //    ASSERT_EQ(rec.getEndPos1().back(), 51250000u);
+    //    ASSERT_EQ(rec.getStartPos2().front(), 16000000u);
+    //    ASSERT_EQ(rec.getStartPos2().back(), 51000000u);
+    //    ASSERT_EQ(rec.getEndPos2().front(), 16250000u);
+    //    ASSERT_EQ(rec.getEndPos2().back(), 51250000u);
+    //    ASSERT_EQ(rec.getCounts().front(), 16u);
+    //    ASSERT_EQ(rec.getCounts().back(), 68156u);
+
+    {
+        std::stringstream ss;
+        genie::util::BitWriter writer(&ss);
+
+        rec.write(writer);
+
+        genie::util::BitReader bitreader(ss);
+        auto recon_rec = genie::core::record::ContactRecord(bitreader);
+
+        ASSERT_TRUE(rec == recon_rec);
+
+        check_row_col_uniqueness(recon_rec);
+    }
+}
 
 // ---------------------------------------------------------------------------------------------------------------------
