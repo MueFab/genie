b4c205a27ae24769c19f432cd013d53480144c82
Yeremia Gunawan Adhisanto <adhisant@tnt.uni-hannover.de>
2024-08-01 11:20:43 +0200

    Update genotype record tests

--- test/records/variant_genotype-test.cc
+++ test/records/variant_genotype-test.cc
@@ -8,8 +8,6 @@
 #include <fstream>
 #include "genie/core/record/variant_genotype/record.h"
 #include "genie/util/bitreader.h"
-#include "genie/util/bitwriter.h"
-#include "genie/util/runtime-exception.h"
 #include "helpers.h"
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -83,7 +81,9 @@ TEST(VariantGenotypeRecord, Genotype_gt_only) {  // NOLINT(cert-err58-cpp)
 
 }
 
-TEST(VariantGenotypeRecord, Genotype_fmt_only) {  // NOLINT(cert-err58-cpp)
+// ---------------------------------------------------------------------------------------------------------------------
+
+TEST(VariantGenotypeRecord, Genotype_FORMAT_only) {  // NOLINT(cert-err58-cpp)
     std::string gitRootDir = util_tests::exec("git rev-parse --show-toplevel");
     std::string filepath = gitRootDir + "/data/records/1.3.5.header100.fmt_only.vcf.geno";
 
@@ -111,6 +111,38 @@ TEST(VariantGenotypeRecord, Genotype_fmt_only) {  // NOLINT(cert-err58-cpp)
         ASSERT_EQ(rec.getLinkedRecord(), false);
     }
     
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+TEST(VariantGenotypeRecord, Genotype_FORMAT_CASE2) {  // NOLINT(cert-err58-cpp)
+    std::string gitRootDir = util_tests::exec("git rev-parse --show-toplevel");
+    std::string filepath = gitRootDir + "/data/records/variant/ALL.chrY.phase3_integrated_v2a.20130502.genotypes.bgz.CASE02.geno";
+
+    std::vector<genie::core::record::VariantGenotype> recs;
+
+    {
+        std::ifstream reader(filepath, std::ios::binary);
+        ASSERT_EQ(reader.fail(), false);
+        genie::util::BitReader bitreader(reader);
+
+        while (bitreader.isGood()){
+            recs.emplace_back(bitreader);
+        }
+
+        // TODO (Yeremia): Temporary fix as the number of records exceeded by 1
+        recs.pop_back();
+    }
+
+    ASSERT_EQ(recs.size(), 5);
+
+    for (size_t i = 0; i < recs.size(); i++) {
+        auto& rec = recs[i];
+        ASSERT_EQ(rec.getVariantIndex(), i);
+        ASSERT_EQ(rec.getNumSamples(), 1233u);
+        ASSERT_EQ(rec.getLinkedRecord(), false);
+    }
+
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
