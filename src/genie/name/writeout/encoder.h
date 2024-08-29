570687b515a4aae8d39821a19b0250ae641f3a88
Fabian Müntefering <muenteferi@tnt.uni-hannover.de>
2024-08-29 11:43:45 +0200

    Added stubs for zstd, lzma, bsc, namewriteout

---
+++ src/genie/name/writeout/encoder.h
@@ -0,0 +1,52 @@
+/**
+ * @file
+ * @copyright This file is part of GENIE. See LICENSE and/or
+ * https://github.com/mitogen/genie for more details.
+ */
+
+#ifndef SRC_GENIE_NAME_WRITEOUT_ENCODER_H_
+#define SRC_GENIE_NAME_WRITEOUT_ENCODER_H_
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+#include <limits>
+#include <string>
+#include <tuple>
+#include <vector>
+#include "genie/core/name-encoder.h"
+#include "genie/core/record/record.h"
+#include "genie/name/tokenizer/tokenizer.h"
+#include "genie/util/data-block.h"
+#include "genie/util/watch.h"
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace genie {
+namespace name {
+namespace writeout {
+
+/**
+ * @brief
+ */
+class Encoder : public core::NameEncoder {
+ public:
+    /**
+     * @brief
+     * @param recs
+     * @return
+     */
+    std::tuple<core::AccessUnit::Descriptor, core::stats::PerfStats> process(const core::record::Chunk& recs) override;
+};
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+}  // namespace writeout
+}  // namespace name
+}  // namespace genie
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+#endif  // SRC_GENIE_NAME_WRITEOUT_ENCODER_H_
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
