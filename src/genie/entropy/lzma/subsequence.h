0ea7fbf2f08501ef3ed452fba439a20c96b2b4ae
Fabian Müntefering <fabian.muentefering@posteo.de>
2024-09-02 23:36:45 +0200

    Add LZMA

---
+++ src/genie/entropy/lzma/subsequence.h
@@ -0,0 +1,32 @@
+/**
+* @file
+ * @copyright This file is part of GENIE. See LICENSE and/or
+ * https://github.com/mitogen/genie for more details.
+ */
+
+#ifndef SRC_GENIE_ENTROPY_LZMA_SUBSEQUENCE_H_
+#define SRC_GENIE_ENTROPY_LZMA_SUBSEQUENCE_H_
+#include <cstdint>
+#include "genie/util/bitwriter.h"
+
+namespace genie::entropy::lzma {
+class Subsequence {
+ public:
+    explicit Subsequence(uint8_t _output_symbol_size);
+
+    [[nodiscard]] uint8_t getOutputSymbolSize() const;
+
+    /**
+     * @brief
+     * @param writer
+     */
+    void write(util::BitWriter &writer) const;
+
+    bool operator==(const Subsequence &rhs) const;
+
+ private:
+    uint8_t output_symbol_size;  //!< @brief
+};
+}  // namespace genie::entropy::lzma
+
+#endif  // SRC_GENIE_ENTROPY_LZMA_SUBSEQUENCE_H_
