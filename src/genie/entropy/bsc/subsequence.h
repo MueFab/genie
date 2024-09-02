374969a2911121d422a40393207ca71408791f43
Fabian Müntefering <fabian.muentefering@posteo.de>
2024-09-03 00:00:10 +0200

    Fix formatting

--- src/genie/entropy/bsc/subsequence.h
+++ src/genie/entropy/bsc/subsequence.h
@@ -6,14 +6,30 @@
 
 #ifndef SRC_GENIE_ENTROPY_BSC_SUBSEQUENCE_H_
 #define SRC_GENIE_ENTROPY_BSC_SUBSEQUENCE_H_
+
+// ---------------------------------------------------------------------------------------------------------------------
+
 #include <cstdint>
 #include "genie/util/bitwriter.h"
 
+// ---------------------------------------------------------------------------------------------------------------------
+
 namespace genie::entropy::bsc {
+
+// ---------------------------------------------------------------------------------------------------------------------
+
 class Subsequence {
  public:
+    /**
+     *
+     * @param _output_symbol_size
+     */
     explicit Subsequence(uint8_t _output_symbol_size);
 
+    /**
+     *
+     * @return
+     */
     [[nodiscard]] uint8_t getOutputSymbolSize() const;
 
     /**
@@ -22,11 +38,24 @@ class Subsequence {
      */
     void write(util::BitWriter &writer) const;
 
+    /**
+     * @brief
+     * @param rhs
+     * @return
+     */
     bool operator==(const Subsequence &rhs) const;
 
  private:
     uint8_t output_symbol_size;  //!< @brief
 };
+
+// ---------------------------------------------------------------------------------------------------------------------
+
 }  // namespace genie::entropy::bsc
 
+// ---------------------------------------------------------------------------------------------------------------------
+
 #endif  // SRC_GENIE_ENTROPY_BSC_SUBSEQUENCE_H_
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
