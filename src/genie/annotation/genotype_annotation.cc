510465f9eea02fde0b8cd43b0db40f0b25d69d34
Ronald Linders <r.f.a.linders@philips.com>
2024-09-04 14:01:30 +0200

    Remove minor VS2017 compiler warning

--- src/genie/annotation/genotype_annotation.cc
+++ src/genie/annotation/genotype_annotation.cc
@@ -68,7 +68,7 @@ DataUnits GenotypeAnnotation::parseGenotype(std::ifstream& inputfile) {
 
         // add LINK_ID default values
         for (auto j = 0u; i < genotype_opt.block_size && j < defaultTileSize && j < rowsRead; ++j) {
-            char val = static_cast<char>(0xFF);
+            const char val = '\xFF';
             descriptorStream[genie::core::AnnotDesc::LINKID].write(&val, 1);
         }
 
