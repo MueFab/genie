ab9c4d569d26c888cb2b12a6aca314c0e78c069e
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-02-26 19:12:05 +0100

    Refactored alignment_split and alignment_external

--- src/genie/core/record/alignment_split/same_rec.cc
+++ src/genie/core/record/alignment_split/same_rec.cc
@@ -1,69 +1,68 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
-#include "same-rec.h"
+#include "genie/core/record/alignment_split/same_rec.h"
+
+#include <memory>
 #include <utility>
+
 #include "genie/util/bit_reader.h"
 #include "genie/util/bit_writer.h"
-#include "genie/util/make_unique.h"
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
-namespace record {
-namespace alignment_split {
+namespace genie::core::record::alignment_split {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-SameRec::SameRec() : AlignmentSplit(AlignmentSplit::Type::SAME_REC), delta(0), alignment() {}
+SameRec::SameRec() : AlignmentSplit(AlignmentSplit::Type::kSameRec), delta_(0) {}
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-SameRec::SameRec(int64_t _delta, Alignment _alignment)
-    : AlignmentSplit(AlignmentSplit::Type::SAME_REC), delta(_delta), alignment(std::move(_alignment)) {}
+SameRec::SameRec(const int64_t delta, Alignment alignment)
+    : AlignmentSplit(Type::kSameRec),
+      delta_(delta),
+      alignment_(std::move(alignment)) {}
 
 // ---------------------------------------------------------------------------------------------------------------------
 
 SameRec::SameRec(uint8_t as_depth, util::BitReader &reader)
-    : AlignmentSplit(AlignmentSplit::Type::SAME_REC),
-      delta(reader.ReadAlignedInt<int64_t, 6>()),
-      alignment(as_depth, reader) {}
+    : AlignmentSplit(Type::kSameRec),
+      delta_(reader.ReadAlignedInt<int64_t, 6>()),
+      alignment_(as_depth, reader) {}
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-void SameRec::write(util::BitWriter &writer) const {
-    AlignmentSplit::write(writer);
-    writer.WriteBypassBE<int64_t, 6>(delta);
-    alignment.Write(writer);
+void SameRec::Write(util::BitWriter &writer) const {
+    AlignmentSplit::Write(writer);
+    writer.WriteBypassBE<int64_t, 6>(delta_);
+    alignment_.Write(writer);
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-const Alignment &SameRec::getAlignment() const { return alignment; }
+const Alignment &SameRec::GetAlignment() const { return alignment_; }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-int64_t SameRec::getDelta() const { return delta; }
+int64_t SameRec::getDelta() const { return delta_; }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
 std::unique_ptr<AlignmentSplit> SameRec::clone() const {
-    auto ret = util::make_unique<SameRec>();
-    ret->delta = this->delta;
-    ret->alignment = this->alignment;
+    auto ret = std::make_unique<SameRec>();
+    ret->delta_ = this->delta_;
+    ret->alignment_ = this->alignment_;
     return ret;
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-}  // namespace alignment_split
-}  // namespace record
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core::record::alignment_split
 
 // ---------------------------------------------------------------------------------------------------------------------
 // ---------------------------------------------------------------------------------------------------------------------
