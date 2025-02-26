58de6ee354ed5c5b4c1a717192c35195657e01b0
hae-banko <irvan.fikardi@stud.uni-hannover.de>
2025-02-26 18:23:16 +0100

    Fixed cmake command in azure-pipelines.yml

--- src/genie/core/record/alignment_external.h
+++ src/genie/core/record/alignment_external.h
@@ -1,7 +1,8 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
 #ifndef SRC_GENIE_CORE_RECORD_ALIGNMENT_EXTERNAL_H_
@@ -11,14 +12,13 @@
 
 #include <cstdint>
 #include <memory>
+
 #include "genie/util/bit_reader.h"
 #include "genie/util/bit_writer.h"
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
-namespace record {
+namespace genie::core::record {
 
 /**
  * @brief
@@ -28,13 +28,13 @@ class AlignmentExternal {
     /**
      * @brief
      */
-    enum class Type : uint8_t { NONE, OTHER_REC };
+    enum class Type : uint8_t { kNone, kOtherRec };
 
     /**
      * @brief
-     * @param _moreAlignmentInfoType
+     * @param more_alignment_info_type
      */
-    explicit AlignmentExternal(Type _moreAlignmentInfoType);
+    explicit AlignmentExternal(Type more_alignment_info_type);
 
     /**
      * @brief
@@ -45,30 +45,28 @@ class AlignmentExternal {
      * @brief
      * @param writer
      */
-    virtual void write(util::BitWriter &writer) const;
+    virtual void Write(util::BitWriter &writer) const;
 
     /**
      * @brief
      * @param reader
      * @return
      */
-    static std::unique_ptr<AlignmentExternal> factory(util::BitReader &reader);
+    static std::unique_ptr<AlignmentExternal> Factory(util::BitReader &reader);
 
     /**
      * @brief
      * @return
      */
-    virtual std::unique_ptr<AlignmentExternal> clone() const = 0;
+    [[nodiscard]] virtual std::unique_ptr<AlignmentExternal> Clone() const = 0;
 
  protected:
-    Type moreAlignmentInfoType;  //!< @brief
+    Type more_alignment_info_type_;  //!< @brief
 };
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-}  // namespace record
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core::record
 
 // ---------------------------------------------------------------------------------------------------------------------
 
