52e5c28ffd899f41162bd23509d5720cf3627119
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-03-05 22:54:55 +0100

    Refactored record/data_unit

--- src/genie/core/record/data_unit/record.cc
+++ src/genie/core/record/data_unit/record.cc
@@ -3,7 +3,6 @@
  * @copyright This file is part of GENIE. See LICENSE and/or
  * https://github.com/mitogen/genie for more details.
  */
-
 #include <algorithm>
 #include <filesystem>
 #include <string>
@@ -14,85 +13,102 @@
 #include "genie/util/runtime_exception.h"
 
 #include "genie/core/record/data_unit/record.h"
-// ---------------------------------------------------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
-namespace record {
-namespace data_unit {
+// -------------------------------------------------------------------------------------------------
 
-Record::Record() : data_unit_type(0), data_unit_size(0) {}
+namespace genie::core::record::data_unit {
 
-Record::Record(RawReference& _rawReference) : data_unit_type(0), data_unit_size(0), rawReference(_rawReference) {}
+// -------------------------------------------------------------------------------------------------
 
-Record::Record(ParameterSet& _parameterSet) : data_unit_type(1), parameterSet(_parameterSet) {
-    data_unit_size = this->parameterSet.getSize();
+Record::Record() : data_unit_type_(0), data_unit_size_(0) {}
+
+// -------------------------------------------------------------------------------------------------
+
+Record::Record(RawReference& _rawReference)
+    : data_unit_type_(0), data_unit_size_(0), rawReference_(_rawReference) {}
+
+// -------------------------------------------------------------------------------------------------
+
+Record::Record(ParameterSet& _parameterSet) : data_unit_type_(1), parameterSet_(_parameterSet) {
+  data_unit_size_ = this->parameterSet_.GetSize();
 }
 
-Record::Record(AccessUnit& _accessUnit) : data_unit_type(2), data_unit_size(0), accessUnit(_accessUnit) {}
+// -------------------------------------------------------------------------------------------------
+
+Record::Record(AccessUnit& _accessUnit)
+    : data_unit_type_(2), data_unit_size_(0), access_unit_(_accessUnit) {}
+
+// -------------------------------------------------------------------------------------------------
 
 Record::Record(annotation_parameter_set::Record& _annotationParameterSet)
-    : data_unit_type(3), data_unit_size(0), annotationParameterSet(_annotationParameterSet) {}
+    : data_unit_type_(3), data_unit_size_(0), annotation_parameter_set_(_annotationParameterSet) {}
 
-Record::Record(annotation_access_unit::Record& _annotationAccessUnit) : data_unit_type(4), data_unit_size(0) {
-    annotationAccessUnit = _annotationAccessUnit;
+// -------------------------------------------------------------------------------------------------
+
+Record::Record(annotation_access_unit::Record& _annotationAccessUnit)
+    : data_unit_type_(4), data_unit_size_(0) {
+  annotation_access_unit_ = _annotationAccessUnit;
 }
 
-void Record::write(core::Writer& writer, uint64_t writeSize) const {
-  writer.Write(data_unit_type, 8);
-    switch (data_unit_type) {
-        case 0:
-            break;
-        case 1:
-            break;
-        case 2:
-            break;
-        case 3:
-          writer.WriteReserved(10);
-            writer.Write(writeSize, 22);
-            annotationParameterSet.write(writer);
-            break;
-        case 4:
-          writer.WriteReserved(3);
-            writer.Write(writeSize, 29);
-            annotationAccessUnit.write(writer);
-            break;
-        default:
-            break;
-    }
+// -------------------------------------------------------------------------------------------------
+
+void Record::write(core::Writer& writer, uint64_t write_size) const {
+  writer.Write(data_unit_type_, 8);
+  switch (data_unit_type_) {
+    case 0:
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      writer.WriteReserved(10);
+      writer.Write(write_size, 22);
+      annotation_parameter_set_.write(writer);
+      break;
+    case 4:
+      writer.WriteReserved(3);
+      writer.Write(write_size, 29);
+      annotation_access_unit_.write(writer);
+      break;
+    default:
+      break;
+  }
 }
+
+// -------------------------------------------------------------------------------------------------
 
 uint64_t genie::core::record::data_unit::Record::write(core::Writer& writer) const {
-  writer.Write(data_unit_type, 8);
-    uint64_t writesize = 0;
-    switch (data_unit_type) {
-        case 0:
-            break;
-        case 1:
-            break;
-        case 2:
-            break;
-        case 3:
-          writer.WriteReserved(10);
-            writesize = (annotationParameterSet.getSize() + 40) / 8;
-            writer.Write(writesize, 22);
-            annotationParameterSet.write(writer);
-            break;
-        case 4:
-          writer.WriteReserved(3);
-            writesize = (annotationAccessUnit.getSize() + 40) / 8;
-            writer.Write(writesize, 29);
-            annotationAccessUnit.write(writer);
-            break;
-        default:
-            break;
-    }
-    return writesize;
+  writer.Write(data_unit_type_, 8);
+  uint64_t writesize = 0;
+  switch (data_unit_type_) {
+    case 0:
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      writer.WriteReserved(10);
+      writesize = (annotation_parameter_set_.getSize() + 40) / 8;
+      writer.Write(writesize, 22);
+      annotation_parameter_set_.write(writer);
+      break;
+    case 4:
+      writer.WriteReserved(3);
+      writesize = (annotation_access_unit_.getSize() + 40) / 8;
+      writer.Write(writesize, 29);
+      annotation_access_unit_.write(writer);
+      break;
+    default:
+      break;
+  }
+  return writesize;
 }
-}  // namespace data_unit
-}  // namespace record
-}  // namespace core
-}  // namespace genie
+
+// -------------------------------------------------------------------------------------------------
+
+}  // namespace genie::core::record::data_unit
 
 // ---------------------------------------------------------------------------------------------------------------------
 // ---------------------------------------------------------------------------------------------------------------------
--- src/genie/core/record/data_unit/record.h
+++ src/genie/core/record/data_unit/record.h
@@ -23,66 +23,134 @@
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
-namespace record {
-namespace data_unit {
+namespace genie::core::record::data_unit {
 
+/**
+ * @brief Class representing raw reference data
+ */
 class RawReference {
  public:
-    size_t getSize() const { return 0; }
-};
-class ParameterSet {
- public:
-    size_t getSize() const { return 0; }
-};
-class AccessUnit {
- public:
-    size_t getSize() const { return 0; }
-};
-class AnnotationAccessUnit {
- public:
-    size_t getSize() const { return 0; }
+  /**
+   * @brief Gets the size of the raw reference
+   * @return Size of the raw reference
+   */
+  [[nodiscard]] static size_t GetSize() {
+    return 0;
+  }
 };
 
 /**
- *  @brief
+ * @brief Class representing parameter set data
+ */
+class ParameterSet {
+ public:
+  /**
+   * @brief Gets the size of the parameter set
+   * @return Size of the parameter set
+   */
+  [[nodiscard]] static size_t GetSize() {
+    return 0;
+  }
+};
+
+/**
+ * @brief Class representing annotation access unit data
+ */
+class AccessUnit {
+ public:
+  /**
+   * @brief Gets the size of the access unit
+   * @return Size of the access unit
+   */
+  [[nodiscard]] static size_t GetSize() {
+    return 0;
+  }
+};
+
+/**
+ * @brief Class representing annotation access unit data
+ */
+class AnnotationAccessUnit {
+ public:
+  /**
+   * @brief Gets the size of the annotation access unit
+   * @return Size of the annotation access unit
+   */
+  [[nodiscard]] static size_t GetSize() {
+    return 0;
+  }
+};
+
+/**
+ * @brief Class representing a data unit record
+ *
+ * A record can contain one of several types of data: raw reference,
+ * parameter set, access unit, annotation access unit, or annotation parameter set.
  */
 class Record {
  private:
-    uint8_t data_unit_type;
-    uint64_t data_unit_size; // TODO: Compute on-the-fly (?)
-    RawReference rawReference;
-    ParameterSet parameterSet;
-    AccessUnit accessUnit;
-    genie::core::record::annotation_access_unit::Record annotationAccessUnit;
-    genie::core::record::annotation_parameter_set::Record annotationParameterSet;
-
+  uint8_t data_unit_type_;
+  uint64_t data_unit_size_;  // TODO: Compute on-the-fly (?)
+  RawReference rawReference_;
+  ParameterSet parameterSet_;
+  AccessUnit access_unit_;
+  genie::core::record::annotation_access_unit::Record annotation_access_unit_;
+  genie::core::record::annotation_parameter_set::Record annotation_parameter_set_;
 
  public:
-    /**
-     * @brief
-     */
-    Record();
+  /**
+   * @brief Default constructor
+   */
+  Record();
 
-    explicit Record(RawReference& rawReference);
-    explicit Record(ParameterSet& parameterSet);
-    explicit Record(AccessUnit& accessUnit);
+  /**
+   * @brief Constructs a record with raw reference data
+   * @param raw_reference The raw reference data
+   */
+  explicit Record(RawReference& raw_reference);
 
-    explicit Record(annotation_access_unit::Record& annotationAccessUnit);
-    explicit Record(annotation_parameter_set::Record& annotationParameterSet);
+  /**
+   * @brief Constructs a record with parameter set data
+   * @param parameter_set The parameter set data
+   */
+  explicit Record(ParameterSet& parameter_set);
 
-    uint64_t write(core::Writer& writer) const;
-    void write(core::Writer& writer, uint64_t writeSize) const;
+  /**
+   * @brief Constructs a record with access unit data
+   * @param access_unit The access unit data
+   */
+  explicit Record(AccessUnit& access_unit);
 
+  /**
+   * @brief Constructs a record with annotation access unit data
+   * @param annotation_access_unit The annotation access unit data
+   */
+  explicit Record(annotation_access_unit::Record& annotation_access_unit);
+
+  /**
+   * @brief Constructs a record with annotation parameter set data
+   * @param annotation_parameter_set The annotation parameter set data
+   */
+  explicit Record(annotation_parameter_set::Record& annotation_parameter_set);
+
+  /**
+   * @brief Writes the complete record to a writer
+   * @param writer The writer to write to
+   * @return Number of bytes written
+   */
+  uint64_t write(core::Writer& writer) const;
+
+  /**
+   * @brief Writes a specific amount of the record to a writer
+   * @param writer The writer to write to
+   * @param write_size The number of bytes to write
+   */
+  void write(core::Writer& writer, uint64_t write_size) const;
 };
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-}  // namespace data_unit
-}  // namespace record
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core::record::data_unit
 
 // ---------------------------------------------------------------------------------------------------------------------
 
