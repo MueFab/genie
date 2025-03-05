0179ec3fd10e4d5c1e1258d09853ecac2c22b319
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-03-05 22:51:25 +0100

    Refactored record/variant_genotype

--- src/genie/core/record/variant_genotype/format_field.h
+++ src/genie/core/record/variant_genotype/format_field.h
@@ -1,7 +1,8 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
 #ifndef GENIE_CORE_RECORD_FORMAT_FIELD_FORMAT_FIELD_H
@@ -19,33 +20,63 @@
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
-namespace record {
+namespace genie::core::record {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
+/**
+ * @brief Class representing a format field in a variant genotype record
+ *
+ * Stores information about format data, including name, type, and values for each sample.
+ */
 class FormatField {
  private:
-    std::string format_name;
-    DataType format_type;
-    // sample_count, array_len
-    std::vector<std::vector<DynamicDataType>> format_value;
+  std::string format_name_;      //!< @brief Name of the format field (e.g., GT, DP, GQ)
+  DataType format_type_;         //!< @brief Data type of the format field
+  // sample_count, array_len
+  std::vector<std::vector<DynamicDataType>> format_value_;  //!< @brief Values for each sample and array index
 
  public:
-    uint8_t getFormatLen() const;
-    const std::string& getFormatName() const;
-    DataType getFormatType() const;
-    uint32_t getSampleCount() const;
-    uint8_t getFormatArrayLen() const;
-    const std::vector<std::vector<DynamicDataType>>& getFormatValue() const;
+  /**
+   * @brief Gets the length of the format name
+   * @return The length of the format name
+   */
+  [[nodiscard]] uint8_t GetFormatLen() const;
+
+  /**
+   * @brief Gets the format field name
+   * @return Constant reference to the format name
+   */
+  [[nodiscard]] const std::string& GetFormatName() const;
+
+  /**
+   * @brief Gets the data type of the format field
+   * @return The format field data type
+   */
+  [[nodiscard]] DataType GetFormatType() const;
+
+  /**
+   * @brief Gets the number of samples this format field applies to
+   * @return The sample count
+   */
+  [[nodiscard]] uint32_t GetSampleCount() const;
+
+  /**
+   * @brief Gets the array length of the format field
+   * @return The array length
+   */
+  [[nodiscard]] uint8_t GetFormatArrayLen() const;
+
+  /**
+   * @brief Gets the format field values for all samples
+   * @return Constant reference to the format values
+   */
+  [[nodiscard]] const std::vector<std::vector<DynamicDataType>>& GetFormatValue() const;
 };
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-}  // namespace record
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core::record
 
 // ---------------------------------------------------------------------------------------------------------------------
 
--- src/genie/core/record/variant_genotype/record.h
+++ src/genie/core/record/variant_genotype/record.h
@@ -1,7 +1,8 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
 #ifndef SRC_GENIE_CORE_RECORD_VARIANT_GENOTYPE_RECORD_H_
@@ -23,95 +24,281 @@
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
-namespace record {
+namespace genie::core::record {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
 /**
- *  @brief
+ * @brief Class representing an information field in a variant record
+ *
+ * Stores tag name, type, length and value information for variant annotations.
  */
-class info_field {
+class InfoField {
  private:
-    uint8_t info_tag_len;             //!< @brief
-    std::string info_tag;             //!< @brief
-    uint8_t info_type;                //!< @brief
-    uint8_t info_array_len;           //!< @brief
-    std::vector<uint8_t> info_value;  //!< @brief type??
-};
-
-class format_field {
- private:
-    uint32_t sample_count;
-    std::string format;
-    core::DataType type;
-    std::vector<std::vector<std::vector<uint8_t>>> value;
-    uint8_t arrayLength;
-
- public:
-    format_field(util::BitReader& bitreader, uint32_t _sample_count);
-    format_field() = default;
-    std::string getFormat() const { return format; }
-    core::DataType getType() const { return type; }
-    uint32_t getSampleCount() const { return sample_count; }
-    const std::vector<std::vector<std::vector<uint8_t>>>& getValue() const { return value; }
-    uint8_t getArrayLength() const { return arrayLength; }
-
-    format_field(std::string _format, core::DataType _type, uint8_t _arrayLength)
-        : sample_count(0), format(_format), type(_type), value{}, arrayLength(_arrayLength) {}
-    void setSampleCount(uint32_t _sample_count) { sample_count = _sample_count; }
-    void setValue(std::vector<std::vector<std::vector<uint8_t>>> _value) { value = _value; }
+  uint8_t info_tag_len_;             //!< @brief
+  std::string info_tag_;             //!< @brief
+  uint8_t info_type_;                //!< @brief
+  uint8_t info_array_len_;           //!< @brief
+  std::vector<uint8_t> info_value_;  //!< @brief type??
 };
 
 /**
- *  @brief
+ * @brief Class representing a format field in a variant genotype record
+ *
+ * Stores information about sample data format, including type, values, and array dimensions.
+ */
+class FormatField {
+ private:
+  uint32_t sample_count_{};
+  std::string format_;
+  core::DataType type_;
+  std::vector<std::vector<std::vector<uint8_t>>> value_;
+  uint8_t array_length_{};
+
+ public:
+  /**
+   * @brief Constructs a format field by reading from a bit stream
+   * @param bitreader The bit reader to read from
+   * @param sample_count The number of samples this format field applies to
+   */
+  FormatField(util::BitReader& bitreader, uint32_t sample_count);
+
+  /**
+   * @brief Default constructor for format_field
+   */
+  FormatField() = default;
+
+  /**
+   * @brief Gets the format string identifier
+   * @return The format string
+   */
+  [[nodiscard]] std::string GetFormat() const {
+    return format_;
+  }
+
+  /**
+   * @brief Gets the data type of the format field
+   * @return The data type
+   */
+  [[nodiscard]] core::DataType GetType() const {
+    return type_;
+  }
+
+  /**
+   * @brief Gets the data type of the format field
+   * @return The data type
+   */
+  [[nodiscard]] uint32_t GetSampleCount() const {
+    return sample_count_;
+  }
+
+  /**
+   * @brief Gets the number of samples this format field applies to
+   * @return The sample count
+   */
+  [[nodiscard]] const std::vector<std::vector<std::vector<uint8_t>>>& GetValue() const {
+    return value_;
+  }
+
+  /**
+   * @brief Gets the array length of the format field
+   * @return The array length
+   */
+  [[nodiscard]] uint8_t GetArrayLength() const {
+    return array_length_;
+  }
+
+  /**
+   * @brief Constructs a format field with specified parameters
+   * @param format The format string identifier
+   * @param type The data type of the format field
+   * @param arrayLength The length of the array for this format field
+   */
+  FormatField(std::string format, core::DataType type, uint8_t arrayLength)
+      : sample_count_(0),
+        format_(std::move(std::move(format))),
+        type_(type),
+        value_{},
+        array_length_(arrayLength) {}
+
+  /**
+   * @brief Sets the number of samples
+   * @param _sample_count The sample count to set
+   */
+  void setSampleCount(uint32_t _sample_count) {
+    sample_count_ = _sample_count;
+  }
+
+  /**
+   * @brief Sets the format field values
+   * @param _value The values to set
+   */
+  void setValue(std::vector<std::vector<std::vector<uint8_t>>> _value) {
+    value_ = std::move(_value);
+  }
+};
+
+/**
+ * @brief Class representing genotype information for a specific variant
+ *
+ * Contains information about samples, alleles, phasings, likelihoods and format fields
+ * associated with a specific variant.
  */
 class VariantGenotype {
  private:
-    uint64_t variant_index;      //!< @brief
-    uint32_t sample_index_from;  //!< @brief
-    uint32_t sample_count;       //!< @brief
+  uint64_t variant_index_;      //!< @brief
+  uint32_t sample_index_from_;  //!< @brief
+  uint32_t sample_count_;       //!< @brief
 
-    std::vector<format_field> format;  //!< @brief
+  std::vector<FormatField> format_;  //!< @brief
 
-    std::vector<std::vector<int8_t>> alleles;    //!< @brief
-    std::vector<std::vector<uint8_t>> phasings;  //!< @brief
+  std::vector<std::vector<int8_t>> alleles_;    //!< @brief
+  std::vector<std::vector<uint8_t>> phasings_;  //!< @brief
 
-    std::vector<std::vector<uint32_t>> likelihoods;  //!< @brief
+  std::vector<std::vector<uint32_t>> likelihoods_;  //!< @brief
 
-    std::optional<LinkRecord> link_record;
+  std::optional<LinkRecord> link_record_;
 
  public:
-    explicit VariantGenotype(util::BitReader& bitreader);
-    VariantGenotype(uint64_t variant_index, uint32_t sample_index_from);
-    uint64_t getVariantIndex() const;
-    uint32_t getStartSampleIndex() const;
-    uint32_t getNumSamples() const;
+  /**
+   * @brief Constructs a variant genotype by reading from a bit stream
+   * @param bitreader The bit reader to read from
+   */
+  explicit VariantGenotype(util::BitReader& bitreader);
 
-    uint8_t getFormatCount() const;
+  /**
+   * @brief Constructs a variant genotype with specified parameters
+   * @param variant_index The index of the variant
+   * @param sample_index_from The starting sample index
+   */
+  VariantGenotype(uint64_t variant_index, uint32_t sample_index_from);
 
-    bool isGenotypePresent() const;
-    bool isLikelihoodPresent() const;
-    uint8_t getNumberOfAllelesPerSample() const;
-    const std::vector<std::vector<int8_t>>& getAlleles() const;
-    const std::vector<std::vector<uint8_t>>& getPhasing() const;
-    uint8_t getNumberOfLikelihoods() const;
-    const std::vector<std::vector<uint32_t>>& getLikelihoods() const;
-    bool getLinkedRecord() const;
-    const LinkRecord& getLinkRecord() const;
-    const std::vector<format_field>& getFormats() const;
+  /**
+   * @brief Gets the variant index
+   * @return The variant index
+   */
+  [[nodiscard]] uint64_t GetVariantIndex() const;
 
-    void setLikelihood(std::vector<std::vector<uint32_t>> _likelihoods) { likelihoods = _likelihoods; }
-    void setPhasings(std::vector<std::vector<uint8_t>> _phasings) { phasings = _phasings; };
-    void setAlleles(std::vector<std::vector<int8_t>> _alleles) { alleles = _alleles; }
-    void setNumberOfSamples(uint32_t sampleSize) { sample_count = sampleSize; };
-    void setFormats(std::vector<format_field> _formats) { format = _formats; }
+  /**
+   * @brief Gets the starting sample index
+   * @return The starting sample index
+   */
+  [[nodiscard]] uint32_t GetStartSampleIndex() const;
+
+  /**
+   * @brief Gets the number of samples
+   * @return The sample count
+   */
+  [[nodiscard]] uint32_t GetNumSamples() const;
+
+  /**
+   * @brief Gets the number of format fields
+   * @return The format field count
+   */
+  [[nodiscard]] uint8_t GetFormatCount() const;
+
+  /**
+   * @brief Checks if likelihood information is present
+   * @return True if likelihood information exists, false otherwise
+   */
+  [[nodiscard]] bool IsGenotypePresent() const;
+
+  /**
+   * @brief Checks if likelihood information is present
+   * @return True if likelihood information exists, false otherwise
+   */
+  [[nodiscard]] bool IsLikelihoodPresent() const;
+
+  /**
+   * @brief Gets the number of alleles per sample
+   * @return The number of alleles per sample
+   */
+  [[nodiscard]] uint8_t GetNumberOfAllelesPerSample() const;
+
+  /**
+   * @brief Gets the allele information
+   * @return Constant reference to the alleles
+   */
+  [[nodiscard]] const std::vector<std::vector<int8_t>>& GetAlleles() const;
+
+  /**
+   * @brief Gets the phasing information
+   * @return Constant reference to the phasings
+   */
+  [[nodiscard]] const std::vector<std::vector<uint8_t>>& GetPhasing() const;
+
+  /**
+   * @brief Gets the number of likelihoods
+   * @return The number of likelihoods
+   */
+  [[nodiscard]] uint8_t GetNumberOfLikelihoods() const;
+
+  /**
+   * @brief Gets the likelihood information
+   * @return Constant reference to the likelihoods
+   */
+  [[nodiscard]] const std::vector<std::vector<uint32_t>>& GetLikelihoods() const;
+
+  /**
+   * @brief Gets the linked record
+   * @return Constant reference to the link record
+   */
+  [[nodiscard]] bool GetLinkedRecord() const;
+
+  /**
+   * @brief Gets the linked record
+   * @return Constant reference to the link record
+   */
+  [[nodiscard]] const LinkRecord& GetLinkRecord() const;
+
+  /**
+   * @brief Gets the format fields
+   * @return Constant reference to the format fields
+   */
+  [[nodiscard]] const std::vector<FormatField>& GetFormats() const;
+
+  /**
+   * @brief Sets the likelihood values
+   * @param likelihoods The likelihood values to set
+   */
+  void SetLikelihood(std::vector<std::vector<uint32_t>> likelihoods) {
+    likelihoods_ = std::move(likelihoods);
+  }
+
+  /**
+   * @brief Sets the phasing information
+   * @param phasings The phasing values to set
+   */
+  void SetPhasings(std::vector<std::vector<uint8_t>> phasings) {
+    phasings_ = std::move(phasings);
+  };
+
+  /**
+   * @brief Sets the allele information
+   * @param alleles The allele values to set
+   */
+  void SetAlleles(std::vector<std::vector<int8_t>> alleles) {
+    alleles_ = std::move(alleles);
+  }
+
+  /**
+   * @brief Sets the number of samples
+   * @param sampleSize The sample count to set
+   */
+  void SetNumberOfSamples(uint32_t sampleSize) {
+    sample_count_ = sampleSize;
+  };
+
+  /**
+   * @brief Sets the format fields
+   * @param formats The format fields to set
+   */
+  void SetFormats(std::vector<FormatField> formats) {
+    format_ = std::move(formats);
+  }
 };
 
-}  // namespace record
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core::record
 
 // ---------------------------------------------------------------------------------------------------------------------
 
