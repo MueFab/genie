8a6157268ac82027ed814a81955182db3de5f60c
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-03-05 22:52:05 +0100

    Refactored record/linked_record

--- src/genie/core/record/linked_record/linked_record.cc
+++ src/genie/core/record/linked_record/linked_record.cc
@@ -1,41 +1,43 @@
 /**
-* @file
-* @copyright This file is part of GENIE. See LICENSE and/or
-* https://github.com/mitogen/genie for more details.
+ * @file
+ * @copyright This file is part of GENIE. See LICENSE and/or
+ * https://github.com/mitogen/genie for more details.
  */
 
 #include "linked_record.h"
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
-namespace record {
+namespace genie::core::record {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-LinkRecord::LinkRecord(genie::util::BitReader& bitreader){
-    auto link_name_len = bitreader.ReadAlignedInt<uint8_t>();
-    bitreader.ReadAlignedBytes(&link_name[0], link_name_len);
-    reference_box_ID = bitreader.ReadAlignedInt<uint8_t>();
+LinkRecord::LinkRecord(genie::util::BitReader& bitreader) {
+  auto link_name_len = bitreader.ReadAlignedInt<uint8_t>();
+  bitreader.ReadAlignedBytes(&link_name_[0], link_name_len);
+  reference_box_id_ = bitreader.ReadAlignedInt<uint8_t>();
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-uint8_t LinkRecord::getLinkNameLen() const { return static_cast<uint8_t>(link_name.size()); }
+uint8_t LinkRecord::GetLinkNameLen() const {
+  return static_cast<uint8_t>(link_name_.size());
+}
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-const std::string& LinkRecord::getLinkName() const { return link_name; }
+const std::string& LinkRecord::GetLinkName() const {
+  return link_name_;
+}
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-uint8_t LinkRecord::getReferenceBoxID() const { return reference_box_ID; }
+uint8_t LinkRecord::GetReferenceBoxId() const {
+  return reference_box_id_;
+}
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-}  // namespace record
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core::record
 
 // ---------------------------------------------------------------------------------------------------------------------
