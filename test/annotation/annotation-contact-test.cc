469a7cb3fe3dd916e9b088e075cfa8ca757e19fd
Stefanie Wardenaar <stefaniewardenaar@users.noreply.github.com>
2025-03-19 16:34:54 +0100

    added more cases for Contact Matrix conformance bitstreams all norm_as_weight set to true

--- test/annotation/annotation-contact-test.cc
+++ test/annotation/annotation-contact-test.cc
@@ -15,12 +15,11 @@
 #include "helpers.h"
 
 struct ContactTestDetails {
-  ContactTestDetails(std::string casename, std::string contactin, bool REMOVE_UNALIGNED_REGION,
-                     bool TRANSFORM_MASK, bool ENA_DIAG_TRANSFORM,
-                     bool ENA_BINARIZATION, bool NORM_AS_WEIGHT,
-                     bool MULTIPLICATIVE_NORM, uint32_t TILE_SIZE, uint32_t MULT = 1u )
-
-  {
+  ContactTestDetails(std::string casename, std::string contactin,
+                     bool REMOVE_UNALIGNED_REGION, bool TRANSFORM_MASK,
+                     bool ENA_DIAG_TRANSFORM, bool ENA_BINARIZATION,
+                     bool NORM_AS_WEIGHT, bool MULTIPLICATIVE_NORM,
+                     uint32_t TILE_SIZE, uint32_t MULT = 1u) {
     filename = contactin;
     cm_pars.REMOVE_UNALIGNED_REGION = REMOVE_UNALIGNED_REGION;
     cm_pars.TRANSFORM_MASK = TRANSFORM_MASK;
@@ -28,9 +27,7 @@ struct ContactTestDetails {
     cm_pars.ENA_BINARIZATION = ENA_BINARIZATION;
     cm_pars.NORM_AS_WEIGHT = NORM_AS_WEIGHT;
     cm_pars.MULTIPLICATIVE_NORM = MULTIPLICATIVE_NORM;
-    // genie::core::AlgoID::JBIG,
-    cm_pars.TILE_SIZE = TILE_SIZE;
-    // 1u};
+    cm_pars.CODEC_ID = genie::core::AlgoID::JBIG, cm_pars.TILE_SIZE = TILE_SIZE;
     cm_pars.MULT = MULT;
     outputfile = casename;
   }
@@ -65,10 +62,10 @@ TEST_P(AnnotationContactTests, contactparameterset) {
   std::string filePath = gitRootDir + "/data/records/contact/";
   auto testParams = GetParam();
   std::string inputFilename = filePath + testParams.filename;
- 
+
   ASSERT_TRUE(std::filesystem::exists(inputFilename));
 
-  std::string outputFilename = testParams.outputfile + "-output";
+  std::string outputFilename = filePath + testParams.outputfile + "-output";
   std::filesystem::remove(outputFilename + ".bin");
 
   genie::annotation::Compressor compressors;
@@ -81,9 +78,6 @@ TEST_P(AnnotationContactTests, contactparameterset) {
 
   genie::annotation::ContactMatrixParameters cmParameters = testParams.cm_pars;
 
- // cmParameters.NORM_AS_WEIGHT = false;
- // cmParameters.MULTIPLICATIVE_NORM = false;
-
   genie::annotation::Annotation annotationGenerator;
   annotationGenerator.setTileSize(100, 3000);
   annotationGenerator.setContactOptions(cmParameters);
@@ -94,36 +88,49 @@ TEST_P(AnnotationContactTests, contactparameterset) {
       << outputFilename + ".bin";
 }
 
-// contactin 
-// REMOVE_UNALIGNED_REGION, TRANSFORM_MASK,  ENA_DIAG_TRANSFORM,  ENA_BINARIZATION, NORM_AS_WEIGHT,  MULTIPLICATIVE_NORM,TILE_SIZE
+// contactin
+// REMOVE_UNALIGNED_REGION, TRANSFORM_MASK,  ENA_DIAG_TRANSFORM,
+// ENA_BINARIZATION, NORM_AS_WEIGHT,  MULTIPLICATIVE_NORM,TILE_SIZE
 INSTANTIATE_TEST_SUITE_P(
     testoutputs, AnnotationContactTests,
     ::testing::Values(
-        ContactTestDetails("CASE01",
+        ContactTestDetails(
+            "CASE01",
             "GSE63525_GM12878_insitu_primary_30.hic-raw-250000-21_21.cont",
-            false, false, true, true, false, false, 1000),
+            false, false, true, true, true, false, 1000),
         ContactTestDetails(
             "CASE02",
             "GSE63525_GM12878_insitu_primary_30.hic-raw-250000-21_21.cont",
-            false, false, true, true, false, false, 150),
+            false, false, true, true, true, false, 150),
         ContactTestDetails(
             "CASE03",
             "GSE63525_GM12878_insitu_primary_30.hic-raw-250000-21_22.cont",
-            false, false, true, true, false, false, 1000),
+            false, false, true, true, true, false, 1000),
         ContactTestDetails(
             "CASE04",
             "GSE63525_GM12878_insitu_primary_30.hic-raw-250000-21_22.cont",
-            false, false, true, true, false, false, 150),
+            false, false, true, true, true, false, 150),
         ContactTestDetails(
             "CASE07",
             "GSE63525_GM12878_insitu_primary_30.hic-raw-50000-21_21.cont",
-            false, false, true, true, false, false, 1000, 5u),
+            false, false, true, true, true, false, 1000, 5u),
         ContactTestDetails(
             "CASE08",
             "GSE63525_GM12878_insitu_primary_30.hic-raw-50000-21_21.cont",
-            false, false, true, true, false, false, 150, 5u),
+            false, false, true, true, true, false, 150, 5u),
         ContactTestDetails(
             "CASE09",
             "GSE63525_GM12878_insitu_primary_30.hic-raw-250000-21_21.cont",
-            true, false, true, true, false, false, 1000, 5u)
-                          ));
+            true, false, true, true, true, false, 1000, 5u),
+        ContactTestDetails(
+            "CASE12",
+            "GSE63525_GM12878_insitu_primary_30.hic-raw-250000-21_21.cont",
+            true, false, false, true, true, false, 1000),
+        ContactTestDetails(
+            "CASE13",
+            "GSE63525_GM12878_insitu_primary_30.hic-raw-250000-21_21.cont",
+            true, false, true, true, true, false, 1000),
+        ContactTestDetails(
+            "CASE14",
+            "GSE63525_GM12878_insitu_primary_30.hic-raw-250000-21_21.cont",
+            true, false, true, true, true, false, 1000)));
