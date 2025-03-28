d65d75231404d37b0c60eba2b9214fa4464c9fb7
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-03-28 20:08:18 +0100

    Refactor: Leftover minor refactorings from previous commit.

--- src/genie/format/mgg/reference/location/external/mpeg.cc
+++ src/genie/format/mgg/reference/location/external/mpeg.cc
@@ -20,26 +20,26 @@ namespace external {
 // ---------------------------------------------------------------------------------------------------------------------
 
 MPEG::MPEG(uint8_t _reserved, std::string _uri, ChecksumAlgorithm algo, uint8_t _group_id, uint16_t _dataset_id,
-           std::string _ref_checksum, genie::core::MPEGMinorVersion _version)
+           std::string _ref_checksum, genie::core::MpegMinorVersion _version)
     : External(_reserved, std::move(_uri), algo, RefType::MPEGG_REF),
       version(_version),
       external_dataset_group_id(_group_id),
       external_dataset_id(_dataset_id),
       ref_checksum(std::move(_ref_checksum)) {
-    UTILS_DIE_IF(version == genie::core::MPEGMinorVersion::V1900 &&
+    UTILS_DIE_IF(version == genie::core::MpegMinorVersion::kV1900 &&
                      ref_checksum.size() != checksum_sizes[static_cast<uint8_t>(algo)],
                  "Invalid reference checksum");
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-MPEG::MPEG(genie::util::BitReader& reader, size_t seq_count, genie::core::MPEGMinorVersion _version)
+MPEG::MPEG(genie::util::BitReader& reader, size_t seq_count, genie::core::MpegMinorVersion _version)
     : External(reader),
       version(_version),
       ref_checksum(checksum_sizes[static_cast<uint8_t>(getChecksumAlgorithm())], '\0') {
     external_dataset_group_id = reader.readBypassBE<uint8_t>();
     external_dataset_id = reader.readBypassBE<uint16_t>();
-    if (version == core::MPEGMinorVersion::V1900) {
+    if (version == core::MpegMinorVersion::kV1900) {
         reader.readBypass(ref_checksum);
     } else {
         ref_checksum.clear();
@@ -53,13 +53,13 @@ MPEG::MPEG(genie::util::BitReader& reader, size_t seq_count, genie::core::MPEGMi
 // ---------------------------------------------------------------------------------------------------------------------
 
 MPEG::MPEG(genie::util::BitReader& reader, uint8_t _reserved, std::string _uri, ChecksumAlgorithm algo,
-           size_t seq_count, genie::core::MPEGMinorVersion _version)
+           size_t seq_count, genie::core::MpegMinorVersion _version)
     : External(_reserved, std::move(_uri), algo, RefType::MPEGG_REF),
       version(_version),
       ref_checksum(checksum_sizes[static_cast<uint8_t>(getChecksumAlgorithm())], '\0') {
     external_dataset_group_id = reader.readBypassBE<uint8_t>();
     external_dataset_id = reader.readBypassBE<uint16_t>();
-    if (version == core::MPEGMinorVersion::V1900) {
+    if (version == core::MpegMinorVersion::kV1900) {
         reader.readBypass(ref_checksum);
     } else {
         ref_checksum.clear();
@@ -100,7 +100,7 @@ void MPEG::write(genie::util::BitWriter& writer) {
     External::write(writer);
     writer.writeBypassBE(external_dataset_group_id);
     writer.writeBypassBE(external_dataset_id);
-    if (version == core::MPEGMinorVersion::V1900) {
+    if (version == core::MpegMinorVersion::kV1900) {
         writer.writeBypass(ref_checksum.data(), ref_checksum.length());
     } else {
         for (const auto& s : seq_checksums) {
@@ -114,7 +114,7 @@ void MPEG::write(genie::util::BitWriter& writer) {
 void MPEG::addChecksum(std::string checksum) {
     UTILS_DIE_IF(checksum.size() != checksum_sizes[static_cast<uint8_t>(getChecksumAlgorithm())],
                  "Invalid checksum length");
-    if (version != core::MPEGMinorVersion::V1900) {
+    if (version != core::MpegMinorVersion::kV1900) {
         seq_checksums.emplace_back(std::move(checksum));
     }
 }
@@ -122,7 +122,7 @@ void MPEG::addChecksum(std::string checksum) {
 // ---------------------------------------------------------------------------------------------------------------------
 
 std::unique_ptr<genie::core::meta::RefBase> MPEG::decapsulate() {
-    auto ret = genie::util::make_unique<genie::core::meta::external_ref::MPEG>(
+    auto ret = genie::util::make_unique<genie::core::meta::external_ref::Mpeg>(
         std::move(getURI()), genie::core::meta::ExternalRef::ChecksumAlgorithm(getChecksumAlgorithm()),
         external_dataset_group_id, external_dataset_id, std::move(ref_checksum));
     return ret;
