d589ea4870b40038c066d9be251fd763ae86d707
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-04-02 17:14:13 +0200

    Refactor: cigar_tokenizer* is now up to date with the develop branch

--- src/genie/core/cigar_tokenizer.h
+++ src/genie/core/cigar_tokenizer.h
@@ -1,51 +1,51 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
 #ifndef SRC_GENIE_CORE_CIGAR_TOKENIZER_H_
 #define SRC_GENIE_CORE_CIGAR_TOKENIZER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #include <string>
+
 #include "genie/core/constants.h"
-#include "genie/util/stringview.h"
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
+namespace genie::core {
 
 /**
  * @brief
  */
 class CigarTokenizer {
  public:
-    /**
-     * @brief
-     * @tparam Lambda
-     * @param cigar
-     * @param format
-     * @param lambda
-     */
-    template <typename Lambda>
-    static void tokenize(const std::string& cigar, const CigarFormatInfo& format, const Lambda& lambda);
+  /**
+   * @brief
+   * @tparam Lambda
+   * @param cigar
+   * @param format
+   * @param lambda
+   */
+  template <typename Lambda>
+  static void Tokenize(const std::string& cigar, const CigarFormatInfo& format,
+                       const Lambda& lambda);
 };
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #include "genie/core/cigar_tokenizer.impl.h"
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #endif  // SRC_GENIE_CORE_CIGAR_TOKENIZER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
