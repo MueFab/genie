d9f1fd425f6648b21f347dc60e11db266e55d9b0
Laura-Sophie Renz <sophie,renz@online.de>
2025-04-23 14:38:10 +0200

    changed fastq_gzip_roundtrip to work with gz file input

--- ci/fastq_tools/fastq_gzip_roundtrip.sh
+++ ci/fastq_tools/fastq_gzip_roundtrip.sh
@@ -41,48 +41,41 @@ git_root_dir="$(git rev-parse --show-toplevel)"
 
 compress_roundtrip () {
     genie_encoder_parameters="$1"
-    fastq_cmp_error_parameters="$2"
+    fastq_cmp_error_parameters="${2::-3}"
     fastq_cmp_error_parameters_refdecoder="$fastq_cmp_error_parameters"
     genie_decoder_recombine=""
 
-    echo "-----------------Zip Input files"
-    primary_gz_file="$primary_fastq_file.gz"
-    eval gzip -c "$primary_fastq_file" > $primary_gz_file || { echo "Zipping $primary_fastq_file failed!"; exit 1; }
-
+    primary_fastq_file_unzipped="${primary_fastq_file::-3}"
+    eval gzip -k -d -f $primary_fastq_file
 
     if [[ "$paired_fastq_file" == "" ]]; then
         paired_fastq_parameter=""
-        fastq_cmp_input_parameters="-i $primary_fastq_file -p $working_dir/output_1.fastq"
+        fastq_cmp_input_parameters="-i $primary_fastq_file_unzipped -p $working_dir/output_1.fastq"
     else
-        paired_gz_file="$paired_fastq_file.gz"
-        eval gzip -c "$paired_fastq_file" > "$paired_gz_file" || { echo "Zipping $paired_fastq_file failed!"; exit 1; }
-
-    echo "-----------------Zipping Complete"
-
-        paired_fastq_parameter="--input-suppl-file $paired_gz_file"
-        fastq_cmp_input_parameters="-i $primary_fastq_file -j $paired_fastq_file -p $working_dir/output_1.fastq -q $working_dir/output_2.fastq"
+        paired_fastq_file_unzipped="${paired_fastq_file::-3}"
+        eval gzip -k -d -f $paired_fastq_file
+        paired_fastq_parameter="--input-suppl-file $paired_fastq_file"
+        fastq_cmp_input_parameters="-i $primary_fastq_file_unzipped -j $paired_fastq_file_unzipped -p $working_dir/output_1.fastq -q $working_dir/output_2.fastq"
         if [[ "$genie_encoder_parameters" != *"--low-latency"* ]] && [[ "$genie_encoder_parameters" != *"--read-ids none"* ]]; then
             genie_decoder_recombine="--combine-pairs"
             fastq_cmp_error_parameters_refdecoder="$fastq_cmp_error_parameters_refdecoder --broken_pairing"
         fi
     fi
 
-
     echo "-----------------Genie compress"
     eval $timing_command \
         $git_root_dir/cmake-build-release/bin/genie$exe_file_extension run \
-        -i $primary_gz_file \
+        -i $primary_fastq_file \
         $paired_fastq_parameter \
         -o $working_dir/output.mgb -f \
         -w $working_dir \
         $genie_encoder_parameters \
-        --entropy gabac \
-        || { echo "Genie compress ($primary_gz_file; $paired_gz_file; $genie_encoder_parameters) failed!" ; exit 1; }
+        || { echo "Genie compress ($primary_fastq_file; $paired_fastq_file; $genie_encoder_parameters) failed!" ; exit 1; }
 
     echo "-----------------Compressed:"
-    ls -l $primary_gz_file
-    if [[ "$paired_gz_file" != "" ]]; then
-        ls -l $paired_gz_file
+    ls -l $primary_fastq_file
+    if [[ "$paired_fastq_file" != "" ]]; then
+        ls -l $paired_fastq_file
     fi
     ls -l $working_dir/output.mgb
 
@@ -96,12 +89,14 @@ compress_roundtrip () {
         -w $working_dir \
         -i $working_dir/output.mgb -f \
         $genie_decoder_recombine \
-        || { echo "Genie decompress ($primary_gz_file; $paired_gz_file; $genie_encoder_parameters) failed!" ; exit 1; }
+        || { echo "Genie decompress ($primary_fastq_file; $paired_fastq_file; $genie_encoder_parameters) failed!" ; exit 1; }
 
     echo "-----------------Unzip output"
 
-    eval gzip -d $working_dir/output_1.fastq.gz
-    eval gzip -d $working_dir/output_2.fastq.gz
+    eval gzip -d -f -k $working_dir/output_1.fastq.gz
+    if [[ -f $working_dir/output_2.fastq.gz ]]; then
+      eval gzip -d -f -k $working_dir/output_2.fastq.gz
+    fi
 
     ls -l $working_dir/output_1.fastq
     ls -l $working_dir/output_2.fastq
@@ -119,7 +114,7 @@ compress_roundtrip () {
             $MPEGG_REF_DECODER \
             -i $working_dir/output.mgb \
             -o $working_dir/output.mgrec \
-            || { echo "Reference decoder ($primary_gz_file; $paired_gz_file; $genie_encoder_parameters) failed!" ; exit 1; }
+            || { echo "Reference decoder ($primary_fastq_file; $paired_fastq_file; $genie_encoder_parameters) failed!" ; exit 1; }
         rm $working_dir/output.mgb
 
         echo "-----------------Refdecoder decoded:"
@@ -131,7 +126,7 @@ compress_roundtrip () {
         -i $working_dir/output.mgrec \
         -o $working_dir/output_1.fastq \
         --output-suppl-file $working_dir/output_2.fastq -f \
-        || { echo "Genie transcode ($primary_gz_file; $paired_gz_file; $genie_encoder_parameters) failed!" ; exit 1; }
+        || { echo "Genie transcode ($primary_fastq_file; $paired_fastq_file; $genie_encoder_parameters) failed!" ; exit 1; }
 
         rm $working_dir/output.mgrec
 
