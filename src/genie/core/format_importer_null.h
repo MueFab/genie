a0843ce5fd94ffa08240c220a6594219e959bfc9
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-06-17 17:23:11 +0200

    Refactor: Updated format exporter and importer to match develop branch

--- src/genie/core/format_importer_null.h
+++ src/genie/core/format_importer_null.h
@@ -1,41 +1,40 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
 #ifndef SRC_GENIE_CORE_FORMAT_IMPORTER_NULL_H_
 #define SRC_GENIE_CORE_FORMAT_IMPORTER_NULL_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #include "genie/core/format_importer.h"
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
+namespace genie::core {
 /**
  * @brief
  */
-class NullImporter : public FormatImporter {
+class NullImporter final : public FormatImporter {
  protected:
-    /**
-     * @brief
-     * @param _classifier
-     * @return
-     */
-    bool pumpRetrieve(Classifier* _classifier) override;
+  /**
+   * @brief
+   * @param classifier
+   * @return
+   */
+  bool PumpRetrieve(Classifier* classifier) override;
 };
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #endif  // SRC_GENIE_CORE_FORMAT_IMPORTER_NULL_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
