eba706d88bf1984dabf29f46497e5c04098b5d05
hae-banko <irvan.fikardi@stud.uni-hannover.de>
2025-06-17 19:30:23 +0200

    Refactor: Small formatting changes in core/meta and core/stats

--- src/genie/core/meta/label.h
+++ src/genie/core/meta/label.h
@@ -1,5 +1,5 @@
 /**
-* Copyright 2018-2024 The Genie Authors.
+ * Copyright 2018-2024 The Genie Authors.
  * @file
  * @copyright This file is part of Genie. See LICENSE and/or
  * https://github.com/MueFab/genie for more details.
@@ -23,48 +23,48 @@ namespace genie::core::meta {
  * @brief Label metadata
  */
 class Label {
- /// Name of the label
- std::string label_id_;
+  /// Name of the label
+  std::string label_id_;
 
- /// Regions in the genome the label applies to
- std::vector<Region>regions_;
+  /// Regions in the genome the label applies to
+  std::vector<Region>regions_;
 
-public:
- /**
-  * @brief Construct from raw values
-  * @param id Name of the label
-  */
- explicit Label(std::string id);
+ public:
+  /**
+   * @brief Construct from raw values
+   * @param id Name of the label
+   */
+  explicit Label(std::string id);
 
- /**
-  * @brief Construct from json
-  * @param json Json representation
-  */
- explicit Label(const nlohmann::json& json);
+  /**
+   * @brief Construct from json
+   * @param json Json representation
+   */
+  explicit Label(const nlohmann::json& json);
 
- /**
-  * @brief Convert to json
-  * @return Json representation
-  */
- [[nodiscard]] nlohmann::json ToJson() const;
+  /**
+   * @brief Convert to json
+   * @return Json representation
+   */
+  [[nodiscard]] nlohmann::json ToJson() const;
 
- /**
-  * @brief Apply label to a new region of the genome
-  * @param r New region
-  */
- void AddRegion(Region r);
+  /**
+   * @brief Apply label to a new region of the genome
+   * @param r New region
+   */
+  void AddRegion(Region r);
 
- /**
-  * @brief Return the name of the label
-  * @return label id
-  */
- [[nodiscard]] const std::string& GetId() const;
+  /**
+   * @brief Return the name of the label
+   * @return label id
+   */
+  [[nodiscard]] const std::string& GetId() const;
 
- /**
-  * @brief Return the list of active region on the genome
-  * @return Active regions of this label
-  */
- [[nodiscard]] const std::vector<Region>& GetRegions() const;
+  /**
+   * @brief Return the list of active region on the genome
+   * @return Active regions of this label
+   */
+  [[nodiscard]] const std::vector<Region>& GetRegions() const;
 };
 
 // -----------------------------------------------------------------------------
--- src/genie/core/meta/sequence.h
+++ src/genie/core/meta/sequence.h
@@ -1,5 +1,5 @@
 /**
-* Copyright 2018-2024 The Genie Authors.
+ * Copyright 2018-2024 The Genie Authors.
  * @file
  * @copyright This file is part of Genie. See LICENSE and/or
  * https://github.com/MueFab/genie for more details.
@@ -22,59 +22,59 @@ namespace genie::core::meta {
  * @brief Reference sequence information
  */
 class Sequence {
- /// Sequence name
- std::string name_;
+  /// Sequence name
+  std::string name_;
 
- /// Sequence length
- uint64_t length_;
+  /// Sequence length
+  uint64_t length_;
 
- /// Sequence ID
- uint16_t id_;
+  /// Sequence ID
+  uint16_t id_;
 
-public:
- /**
-  * @brief
-  * @return
-  */
- std::string& GetName();
+ public:
+  /**
+   * @brief
+   * @return
+   */
+  std::string& GetName();
 
- /**
-  * @brief Construct from raw values
-  * @param name Sequence name
-  * @param length  Sequence length
-  * @param id Sequence id
-  */
- explicit Sequence(std::string name, uint64_t length, uint16_t id);
+  /**
+   * @brief Construct from raw values
+   * @param name Sequence name
+   * @param length  Sequence length
+   * @param id Sequence id
+   */
+  explicit Sequence(std::string name, uint64_t length, uint16_t id);
 
- /**
-  * @brief Construct from json
-  * @param json Json representation
-  */
- explicit Sequence(const nlohmann::json& json);
+  /**
+   * @brief Construct from json
+   * @param json Json representation
+   */
+  explicit Sequence(const nlohmann::json& json);
 
- /**
-  * @brief Convert to json
-  * @return Json representation
-  */
- [[nodiscard]] nlohmann::json ToJson() const;
+  /**
+   * @brief Convert to json
+   * @return Json representation
+   */
+  [[nodiscard]] nlohmann::json ToJson() const;
 
- /**
-  * @brief Get Name of Sequence
-  * @return Name
-  */
- [[nodiscard]] const std::string& GetName() const;
+  /**
+   * @brief Get Name of Sequence
+   * @return Name
+   */
+  [[nodiscard]] const std::string& GetName() const;
 
- /**
-  * @brief Get length of sequence
-  * @return Length
-  */
- [[nodiscard]] uint64_t GetLength() const;
+  /**
+   * @brief Get length of sequence
+   * @return Length
+   */
+  [[nodiscard]] uint64_t GetLength() const;
 
- /**
-  * @brief Get Sequence ID
-  * @return ID
-  */
- [[nodiscard]] uint16_t GetId() const;
+  /**
+   * @brief Get Sequence ID
+   * @return ID
+   */
+  [[nodiscard]] uint16_t GetId() const;
 };
 
 // -----------------------------------------------------------------------------
