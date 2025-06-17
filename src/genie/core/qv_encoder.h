f2257c72cbd738f0b59aa1146cc4e9f684c0d7f8
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-06-17 18:09:55 +0200

    Refactor: Updated qv encoder and decoder in core to match develop branch

--- src/genie/core/qv_encoder.h
+++ src/genie/core/qv_encoder.h
@@ -1,56 +1,54 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
 #ifndef SRC_GENIE_CORE_QV_ENCODER_H_
 #define SRC_GENIE_CORE_QV_ENCODER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #include <memory>
 #include <tuple>
 #include <utility>
+
 #include "genie/core/access_unit.h"
 #include "genie/core/record/chunk.h"
-#include "genie/util/drain.h"
-#include "genie/util/source.h"
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
+namespace genie::core {
 
 /**
  * @brief The basic interface for modules encoding quality values
  */
-class QVEncoder {
+class QvEncoder {
  public:
-    using QVCoded =
-        std::tuple<std::unique_ptr<parameter::QualityValues>, AccessUnit::Descriptor, core::stats::PerfStats>;  //!<
+  using qv_coded = std::tuple<std::unique_ptr<parameter::QualityValues>,
+                              AccessUnit::Descriptor, stats::PerfStats>;  //!<
 
-    /**
-     * @brief
-     * @param rec
-     * @return
-     */
-    virtual QVCoded process(const record::Chunk& rec) = 0;
+  /**
+   * @brief
+   * @param rec
+   * @return
+   */
+  virtual qv_coded Process(const record::Chunk& rec) = 0;
 
-    /**
-     * @brief For polymorphic destruction
-     */
-    virtual ~QVEncoder() = default;
+  /**
+   * @brief For polymorphic destruction
+   */
+  virtual ~QvEncoder() = default;
 };
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #endif  // SRC_GENIE_CORE_QV_ENCODER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
