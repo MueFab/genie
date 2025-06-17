8051d3a311bcbe2bbf6aeeae16a2b62bea9ce74c
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-06-17 17:38:48 +0200

    Refactor: Updated global_cfg to match develop branch

--- src/genie/core/global_cfg.h
+++ src/genie/core/global_cfg.h
@@ -1,61 +1,59 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
 #ifndef SRC_GENIE_CORE_GLOBAL_CFG_H_
 #define SRC_GENIE_CORE_GLOBAL_CFG_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #include "genie/util/industrial_park.h"
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
+namespace genie::core {
 
 /**
  * @brief Configuration regarding modules, factories, ...
  */
 class GlobalCfg {
- private:
-    /**
-     * @brief
-     */
-    GlobalCfg() = default;
+  /**
+   * @brief
+   */
+  GlobalCfg() = default;
 
-    /**
-     * @brief
-     * @param cfg
-     */
-    GlobalCfg(GlobalCfg& cfg) = default;
+  /**
+   * @brief
+   * @param cfg
+   */
+  GlobalCfg(GlobalCfg& cfg) = default;
 
-    util::IndustrialPark fpark;  //! @brief Our factories
+  util::IndustrialPark fpark_;  //! @brief Our factories
 
  public:
-    /**
-     * @brief Access the set of all active factories
-     * @return Factories
-     */
-    util::IndustrialPark& GetIndustrialPark();
+  /**
+   * @brief Access the set of all active factories
+   * @return Factories
+   */
+  util::IndustrialPark& GetIndustrialPark();
 
-    /**
-     * @brief Access the global configuration
-     * @return Global genie configuration
-     */
-    static GlobalCfg& GetSingleton();
+  /**
+   * @brief Access the global configuration
+   * @return Global genie configuration
+   */
+  static GlobalCfg& GetSingleton();
 };
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #endif  // SRC_GENIE_CORE_GLOBAL_CFG_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
