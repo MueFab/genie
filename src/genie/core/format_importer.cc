a9677799e7cc69b16f19fc82d8885026bd408fc1
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-06-17 17:23:11 +0200

    Refactor: Updated format exporter and importer to match develop branch

--- src/genie/core/format_importer.cc
+++ src/genie/core/format_importer.cc
@@ -1,66 +1,68 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
-#include <utility>
 #include "genie/core/format_importer.h"
 
-// ---------------------------------------------------------------------------------------------------------------------
+#include <utility>
 
-namespace genie {
-namespace core {
+// -----------------------------------------------------------------------------
 
-// ---------------------------------------------------------------------------------------------------------------------
+namespace genie::core {
 
-void FormatImporter::setClassifier(Classifier* _classifier) { classifier = _classifier; }
+// -----------------------------------------------------------------------------
 
-// ---------------------------------------------------------------------------------------------------------------------
-
-bool FormatImporter::Pump(uint64_t& id, std::mutex& lock) {
-    record::Chunk chunk;
-    util::Section sec{};
-    {
-        std::unique_lock<std::mutex> guard(lock);
-        chunk = classifier->GetChunk();
-        uint32_t segment_count = 0;
-        for (const auto& r : chunk.GetData()) {
-            segment_count += uint32_t(r.GetSegments().size());
-        }
-        if (chunk.GetData().empty()) {
-            segment_count = 1;
-        }
-        if (!chunk.GetData().empty() || !chunk.GetRefToWrite().empty()) {
-            sec = {size_t(id), segment_count, true};
-            id += segment_count;
-        } else {
-            bool dataLeft = pumpRetrieve(classifier);
-            if (!dataLeft && !flushing) {
-                classifier->Flush();
-                flushing = true;
-                return true;
-            }
-            if (!dataLeft && flushing) {
-                flushing = false;
-                return false;
-            }
-        }
-    }
-    if (!chunk.GetData().empty() || !chunk.GetRefToWrite().empty()) {
-        Source<record::Chunk>::FlowOut(std::move(chunk), sec);
-    }
-    return true;
+void FormatImporter::SetClassifier(Classifier* classifier) {
+  classifier_ = classifier;
 }
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+bool FormatImporter::Pump(uint64_t& id, std::mutex& lock) {
+  record::Chunk chunk;
+  util::Section sec{};
+  {
+    std::unique_lock guard(lock);
+    chunk = classifier_->GetChunk();
+    uint32_t segment_count = 0;
+    for (const auto& r : chunk.GetData()) {
+      segment_count += static_cast<uint32_t>(r.GetSegments().size());
+    }
+    if (chunk.GetData().empty()) {
+      segment_count = 1;
+    }
+    if (!chunk.GetData().empty() || !chunk.GetRefToWrite().empty()) {
+      sec = {id, segment_count, true};
+      id += segment_count;
+    } else {
+      const bool data_left = PumpRetrieve(classifier_);
+      if (!data_left && !flushing_) {
+        classifier_->Flush();
+        flushing_ = true;
+        return true;
+      }
+      if (!data_left && flushing_) {
+        flushing_ = false;
+        return false;
+      }
+    }
+  }
+  if (!chunk.GetData().empty() || !chunk.GetRefToWrite().empty()) {
+    FlowOut(std::move(chunk), sec);
+  }
+  return true;
+}
+
+// -----------------------------------------------------------------------------
 
 void FormatImporter::FlushIn(uint64_t& pos) { FlushOut(pos); }
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core
 
-// ---------------------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
