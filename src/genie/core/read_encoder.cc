ff25bec25e5033be034310f1032532a91f592c2c
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-06-17 18:11:50 +0200

    Refactor: Updated read_encoder and read_decoder in core to match develop branch

--- src/genie/core/read_encoder.cc
+++ src/genie/core/read_encoder.cc
@@ -1,71 +1,79 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
+
+#include "genie/core/read_encoder.h"
 
 #include <atomic>
 #include <fstream>
 #include <utility>
-#include "genie/core/read_encoder.h"
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
+namespace genie::core {
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-void ReadEncoder::setQVCoder(util::SideSelector<QVEncoder, QVEncoder::QVCoded, const record::Chunk&>* coder) {
-    qvcoder = coder;
+void ReadEncoder::SetQvCoder(util::SideSelector<QvEncoder, QvEncoder::qv_coded,
+                                                const record::Chunk&>* coder) {
+  qvcoder_ = coder;
 }
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-void ReadEncoder::setNameCoder(NameSelector* coder) { namecoder = coder; }
+void ReadEncoder::SetNameCoder(name_selector* coder) { namecoder_ = coder; }
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-void ReadEncoder::setEntropyCoder(EntropySelector* coder) { entropycoder = coder; }
+void ReadEncoder::SetEntropyCoder(entropy_selector* coder) {
+  entropycoder_ = coder;
+}
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-AccessUnit ReadEncoder::entropyCodeAU(EntropySelector* _entropycoder, AccessUnit&& a, bool write_raw) {
-    AccessUnit au = std::move(a);
-    if (write_raw) {
-        static std::atomic<uint64_t> id(0);
-        auto this_id = id++;
-        for (const auto& d : genie::core::GetDescriptors()) {
-            for (auto& s : au.Get(d.id)) {
-                if (s.IsEmpty()) {
-                    continue;
-                }
-                std::ofstream out_file_stream("rawstream_" + std::to_string(this_id) + "_" +
-                                              std::to_string(static_cast<uint8_t>(d.id)) + "_" +
-                                              std::to_string(static_cast<uint8_t>(s.GetId().second)));
-                out_file_stream.write(static_cast<char*>(s.GetData().GetData()), s.GetData().GetRawSize());
-            }
+AccessUnit ReadEncoder::EntropyCodeAu(entropy_selector* entropycoder,
+                                      AccessUnit&& a, bool write_raw) {
+  AccessUnit au = std::move(a);
+  if (write_raw) {
+    static std::atomic<uint64_t> id(0);
+    auto this_id = id++;
+    for (const auto& d : GetDescriptors()) {
+      for (auto& s : au.Get(d.id)) {
+        if (s.IsEmpty()) {
+          continue;
         }
+        std::ofstream out_file_stream(
+            "rawstream_" + std::to_string(this_id) + "_" +
+            std::to_string(static_cast<uint8_t>(d.id)) + "_" +
+            std::to_string(static_cast<uint8_t>(s.GetId().second)));
+        out_file_stream.write(
+            static_cast<char*>(s.GetData().GetData()),
+            static_cast<std::streamsize>(s.GetData().GetRawSize()));
+      }
     }
-    for (auto& d : au) {
-        auto encoded = _entropycoder->Process(d);
-        au.GetParameters().SetDescriptor(d.GetId(), std::move(std::get<0>(encoded)));
-        au.Set(d.GetId(), std::move(std::get<1>(encoded)));
-        au.GetStats().Add(std::get<2>(encoded));
-    }
-    return au;
+  }
+  for (auto& d : au) {
+    auto encoded = entropycoder->Process(d);
+    au.GetParameters().SetDescriptor(d.GetId(),
+                                     std::move(std::get<0>(encoded)));
+    au.Set(d.GetId(), std::move(std::get<1>(encoded)));
+    au.GetStats().Add(std::get<2>(encoded));
+  }
+  return au;
 }
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-AccessUnit ReadEncoder::entropyCodeAU(AccessUnit&& a) {
-    return entropyCodeAU(entropycoder, std::move(a), writeOutStreams);
+AccessUnit ReadEncoder::EntropyCodeAu(AccessUnit&& a) const {
+  return EntropyCodeAu(entropycoder_, std::move(a), write_out_streams_);
 }
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core
 
-// ---------------------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
