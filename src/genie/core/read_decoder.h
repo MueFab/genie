7e062e8cf283559c364fd456a3c32e0750b04a7f
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-06-17 18:11:50 +0200

    Refactor: Updated read_encoder and read_decoder in core to match develop branch

--- src/genie/core/read_decoder.h
+++ src/genie/core/read_decoder.h
@@ -1,101 +1,104 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
 #ifndef SRC_GENIE_CORE_READ_DECODER_H_
 #define SRC_GENIE_CORE_READ_DECODER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #include <string>
 #include <tuple>
 #include <vector>
+
 #include "genie/core/access_unit.h"
 #include "genie/core/entropy_decoder.h"
 #include "genie/core/module.h"
 #include "genie/core/name_decoder.h"
 #include "genie/core/qv_decoder.h"
 #include "genie/core/record/chunk.h"
-#include "genie/util/selector.h"
 #include "genie/util/side_selector.h"
-#include "genie/util/source.h"
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
+namespace genie::core {
 
 /**
  * @brief The basic interface for modules decoding the plain read data
  */
 class ReadDecoder : public Module<AccessUnit, record::Chunk> {
  public:
-    using QvSelector = util::SideSelector<QVDecoder, std::tuple<std::vector<std::string>, core::stats::PerfStats>,
-                                          const parameter::QualityValues&, const std::vector<std::string>&,
-                                          const std::vector<uint64_t>&, AccessUnit::Descriptor&>;  //!< @brief
-    using NameSelector = util::SideSelector<NameDecoder, std::tuple<std::vector<std::string>, core::stats::PerfStats>,
-                                            AccessUnit::Descriptor&>;  //!< @brief
-    using EntropySelector =
-        util::SideSelector<EntropyDecoder, std::tuple<AccessUnit::Descriptor, core::stats::PerfStats>,
-                           const parameter::DescriptorSubSequenceCfg&, AccessUnit::Descriptor&, bool>;  //!< @brief
+  using qv_selector = util::SideSelector<
+      QvDecoder, std::tuple<std::vector<std::string>, stats::PerfStats>,
+      const parameter::QualityValues&, const std::vector<std::string>&,
+      const std::vector<uint64_t>&, AccessUnit::Descriptor&>;  //!< @brief
+  using name_selector =
+      util::SideSelector<NameDecoder,
+                         std::tuple<std::vector<std::string>, stats::PerfStats>,
+                         AccessUnit::Descriptor&>;  //!< @brief
+  using entropy_selector = util::SideSelector<
+      EntropyDecoder, std::tuple<AccessUnit::Descriptor, stats::PerfStats>,
+      const parameter::DescriptorSubSequenceCfg&, AccessUnit::Descriptor&,
+      bool>;  //!< @brief
 
  protected:
-    QvSelector* qvcoder{};            //!< @brief
-    NameSelector* namecoder{};        //!< @brief
-    EntropySelector* entropycoder{};  //!< @brief
+  qv_selector* qvcoder_{};            //!< @brief
+  name_selector* namecoder_{};        //!< @brief
+  entropy_selector* entropycoder_{};  //!< @brief
 
  public:
-    /**
-     * @brief
-     * @param coder
-     */
-    virtual void setQVCoder(QvSelector* coder);
+  /**
+   * @brief
+   * @param coder
+   */
+  virtual void SetQvCoder(qv_selector* coder);
 
-    /**
-     * @brief
-     * @param coder
-     */
-    virtual void setNameCoder(NameSelector* coder);
+  /**
+   * @brief
+   * @param coder
+   */
+  virtual void SetNameCoder(name_selector* coder);
 
-    /**
-     * @brief
-     * @param coder
-     */
-    virtual void setEntropyCoder(EntropySelector* coder);
+  /**
+   * @brief
+   * @param coder
+   */
+  virtual void SetEntropyCoder(entropy_selector* coder);
 
-    /**
-     * @brief
-     * @param select
-     * @param a
-     * @param mmCoderEnabled
-     * @return
-     */
-    static AccessUnit entropyCodeAU(EntropySelector* select, AccessUnit&& a, bool mmCoderEnabled);
+  /**
+   * @brief
+   * @param select
+   * @param a
+   * @param mm_coder_enabled
+   * @return
+   */
+  static AccessUnit EntropyCodeAu(entropy_selector* select, AccessUnit&& a,
+                                  bool mm_coder_enabled);
 
-    /**
-     * @brief
-     * @param a
-     * @param mmCoderEnabled
-     * @return
-     */
-    AccessUnit entropyCodeAU(AccessUnit&& a, bool mmCoderEnabled);
+  /**
+   * @brief
+   * @param a
+   * @param mm_coder_enabled
+   * @return
+   */
+  AccessUnit EntropyCodeAu(AccessUnit&& a, bool mm_coder_enabled) const;
 
-    /**
-     * @brief For polymorphic destruction
-     */
-    ~ReadDecoder() override = default;
+  /**
+   * @brief For polymorphic destruction
+   */
+  ~ReadDecoder() override = default;
 };
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #endif  // SRC_GENIE_CORE_READ_DECODER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
--- src/genie/core/read_encoder.h
+++ src/genie/core/read_encoder.h
@@ -1,102 +1,105 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
 #ifndef SRC_GENIE_CORE_READ_ENCODER_H_
 #define SRC_GENIE_CORE_READ_ENCODER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #include <tuple>
+
 #include "genie/core/access_unit.h"
 #include "genie/core/entropy_encoder.h"
 #include "genie/core/module.h"
 #include "genie/core/name_encoder.h"
 #include "genie/core/qv_encoder.h"
-#include "genie/util/drain.h"
-#include "genie/util/selector.h"
 #include "genie/util/side_selector.h"
-#include "genie/util/source.h"
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
+namespace genie::core {
 
 /**
  * @brief The basic interface for modules encoding the plain read data
  */
 class ReadEncoder : public Module<record::Chunk, AccessUnit> {
  public:
-    using QvSelector = util::SideSelector<QVEncoder, QVEncoder::QVCoded, const record::Chunk&>;  //!< @brief
-    using NameSelector = util::SideSelector<NameEncoder, std::tuple<AccessUnit::Descriptor, core::stats::PerfStats>,
-                                            const record::Chunk&>;  //!< @brief
-    using EntropySelector =
-        util::SideSelector<EntropyEncoder, EntropyEncoder::entropy_coded, AccessUnit::Descriptor&>;  //!< @brief
+  using qv_selector = util::SideSelector<QvEncoder, QvEncoder::qv_coded,
+                                         const record::Chunk&>;  //!< @brief
+  using name_selector =
+      util::SideSelector<NameEncoder,
+                         std::tuple<AccessUnit::Descriptor, stats::PerfStats>,
+                         const record::Chunk&>;  //!< @brief
+  using entropy_selector =
+      util::SideSelector<EntropyEncoder, EntropyEncoder::entropy_coded,
+                         AccessUnit::Descriptor&>;  //!< @brief
 
  protected:
-    QvSelector* qvcoder{};            //!< @brief
-    NameSelector* namecoder{};        //!< @brief
-    EntropySelector* entropycoder{};  //!< @brief
-    bool writeOutStreams;
+  qv_selector* qvcoder_{};            //!< @brief
+  name_selector* namecoder_{};        //!< @brief
+  entropy_selector* entropycoder_{};  //!< @brief
+  bool write_out_streams_;
 
  public:
-    /**
-     * @brief
-     * @param coder
-     */
-    virtual void setQVCoder(QvSelector* coder);
+  /**
+   * @brief
+   * @param coder
+   */
+  virtual void SetQvCoder(qv_selector* coder);
 
-    /**
-     * @brief
-     * @param coder
-     */
-    virtual void setNameCoder(NameSelector* coder);
+  /**
+   * @brief
+   * @param coder
+   */
+  virtual void SetNameCoder(name_selector* coder);
 
-    /**
-     * @brief
-     * @param coder
-     */
-    virtual void setEntropyCoder(EntropySelector* coder);
+  /**
+   * @brief
+   * @param coder
+   */
+  virtual void SetEntropyCoder(entropy_selector* coder);
 
-    /**
-     * @brief
-     * @param _entropycoder
-     * @param a
-     * @param write_raw
-     * @return
-     */
-    static AccessUnit entropyCodeAU(EntropySelector* _entropycoder, AccessUnit&& a, bool write_raw);
+  /**
+   * @brief
+   * @param entropycoder
+   * @param a
+   * @param write_raw
+   * @return
+   */
+  static AccessUnit EntropyCodeAu(entropy_selector* entropycoder,
+                                  AccessUnit&& a, bool write_raw);
 
-    /**
-     * @brief
-     * @param a
-     * @return
-     */
-    AccessUnit entropyCodeAU(AccessUnit&& a);
+  /**
+   * @brief
+   * @param a
+   * @return
+   */
+  AccessUnit EntropyCodeAu(AccessUnit&& a) const;
 
-    /**
-     * @brief For polymorphic destruction
-     */
-    ~ReadEncoder() override = default;
+  /**
+   * @brief For polymorphic destruction
+   */
+  ~ReadEncoder() override = default;
 
-    /**
-     * @brief
-     * @param _writeOutStreams
-     */
-    explicit ReadEncoder(bool _writeOutStreams) : writeOutStreams(_writeOutStreams) {}
+  /**
+   * @brief
+   * @param write_out_streams
+   */
+  explicit ReadEncoder(const bool write_out_streams)
+      : write_out_streams_(write_out_streams) {}
 };
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #endif  // SRC_GENIE_CORE_READ_ENCODER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
