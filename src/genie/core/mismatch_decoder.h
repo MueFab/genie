a1bf7ef7d032c7bebb66e9bb491728b8a6e8cb9f
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-06-17 18:12:36 +0200

    Refactor: Updated mismatch_decoder in core to match develop branch

--- src/genie/core/mismatch_decoder.h
+++ src/genie/core/mismatch_decoder.h
@@ -1,60 +1,59 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
 #ifndef SRC_GENIE_CORE_MISMATCH_DECODER_H_
 #define SRC_GENIE_CORE_MISMATCH_DECODER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #include <cstdint>
 #include <memory>
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
+namespace genie::core {
 
 /**
  * @brief
  */
 class MismatchDecoder {
  public:
-    /**
-     * @brief
-     */
-    virtual ~MismatchDecoder() = default;
+  /**
+   * @brief
+   */
+  virtual ~MismatchDecoder() = default;
 
-    /**
-     * @brief
-     * @param ref
-     * @return
-     */
-    virtual uint64_t DecodeMismatch(uint64_t ref) = 0;
+  /**
+   * @brief
+   * @param ref
+   * @return
+   */
+  virtual uint64_t DecodeMismatch(uint64_t ref) = 0;
 
-    /**
-     * @brief
-     * @return
-     */
-    virtual bool DataLeft() const = 0;
+  /**
+   * @brief
+   * @return
+   */
+  [[nodiscard]] virtual bool DataLeft() const = 0;
 
-    /**
-     * @brief
-     * @return
-     */
-    virtual std::unique_ptr<MismatchDecoder> Copy() const = 0;
+  /**
+   * @brief
+   * @return
+   */
+  [[nodiscard]] virtual std::unique_ptr<MismatchDecoder> Copy() const = 0;
 };
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #endif  // SRC_GENIE_CORE_MISMATCH_DECODER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
