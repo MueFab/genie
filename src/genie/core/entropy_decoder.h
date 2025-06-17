e5b785667e0b476081b31c380e43a237011b6083
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-06-17 17:19:33 +0200

    Refactor: Updated entropy decoder and encoder to match develop branch

--- src/genie/core/entropy_decoder.h
+++ src/genie/core/entropy_decoder.h
@@ -1,53 +1,55 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
 #ifndef SRC_GENIE_CORE_ENTROPY_DECODER_H_
 #define SRC_GENIE_CORE_ENTROPY_DECODER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #include <tuple>
+
 #include "genie/core/access_unit.h"
 #include "genie/core/module.h"
 #include "genie/core/parameter/descriptor_present/decoder.h"
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
+namespace genie::core {
 
 /**
- * @brief Interface for entropy decoders. They convert access unit payloads to raw access units
+ * @brief Interface for entropy decoders. They convert access unit payloads to
+ * raw access units
  */
 class EntropyDecoder {
  public:
-    /**
-     * @brief
-     */
-    virtual ~EntropyDecoder() = default;
+  /**
+   * @brief
+   */
+  virtual ~EntropyDecoder() = default;
 
-    /**
-     * @brief
-     * @param param
-     * @param desc
-     * @param mmCoderEnabled
-     * @return
-     */
-    virtual std::tuple<core::AccessUnit::Descriptor, core::stats::PerfStats> Process(
-        const parameter::DescriptorSubSequenceCfg& param, core::AccessUnit::Descriptor& desc, bool mmCoderEnabled) = 0;
+  /**
+   * @brief
+   * @param param
+   * @param desc
+   * @param mm_coder_enabled
+   * @return
+   */
+  virtual std::tuple<AccessUnit::Descriptor, stats::PerfStats> Process(
+      const parameter::DescriptorSubSequenceCfg& param,
+      AccessUnit::Descriptor& desc, bool mm_coder_enabled) = 0;
 };
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #endif  // SRC_GENIE_CORE_ENTROPY_DECODER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
