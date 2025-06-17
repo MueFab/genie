a0fc5ca62fcd97023852dbf3fd88b58ce879086c
Irvan F <irvan.fikardi@stud.uni-hannover.de>
2025-06-17 17:44:42 +0200

    Refactor: Updated name encoder and decoder in core to match develop branch

--- src/genie/core/name_decoder.h
+++ src/genie/core/name_decoder.h
@@ -1,50 +1,51 @@
 /**
+ * Copyright 2018-2024 The Genie Authors.
  * @file
- * @copyright This file is part of GENIE. See LICENSE and/or
- * https://github.com/mitogen/genie for more details.
+ * @copyright This file is part of Genie. See LICENSE and/or
+ * https://github.com/MueFab/genie for more details.
  */
 
 #ifndef SRC_GENIE_CORE_NAME_DECODER_H_
 #define SRC_GENIE_CORE_NAME_DECODER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #include <string>
 #include <tuple>
 #include <vector>
+
 #include "genie/core/access_unit.h"
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-namespace genie {
-namespace core {
+namespace genie::core {
 
 /**
  * @brief
  */
 class NameDecoder {
  public:
-    /**
-     * @brief
-     * @param desc
-     * @return
-     */
-    virtual std::tuple<std::vector<std::string>, core::stats::PerfStats> process(AccessUnit::Descriptor& desc) = 0;
+  /**
+   * @brief
+   * @param desc
+   * @return
+   */
+  virtual std::tuple<std::vector<std::string>, stats::PerfStats> Process(
+      AccessUnit::Descriptor& desc) = 0;
 
-    /**
-     * @brief For polymorphic destruction
-     */
-    virtual ~NameDecoder() = default;
+  /**
+   * @brief For polymorphic destruction
+   */
+  virtual ~NameDecoder() = default;
 };
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-}  // namespace core
-}  // namespace genie
+}  // namespace genie::core
 
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 #endif  // SRC_GENIE_CORE_NAME_DECODER_H_
 
-// ---------------------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
